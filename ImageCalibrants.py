6786c120c715b6d333a9eaf8e9e75dc27b416820
Robert Von Dreele <vondreele@anl.gov>
2010-03-24 20:06:02 +0000

    added a few more "standards" & added docs.
    git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@42 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

--- ImageCalibrants.py
+++ ImageCalibrants.py
@@ -1,7 +1,13 @@
+#GSASII powder calibrants file; dictionary of substances commonly used for powder
+#calibrations. Each entry consists of:
+# 'name':(Bravais no,(a,b,c,alpha,beta,gamma))
+#Useful Bravais nos.: F-cubic=0,I-cubic=1,P-cubic=2,R3/m(hex)=3, P6=4
 Calibrants={
 '':(0,(0,0,0,0,0,0)),
 'LaB6  SRM660a':(2,(4.1569162,4.1569162,4.1569162,90,90,90)),
 'LaB6  SRM660': (2,(4.15695,4.15695,4.15695,90,90,90)),
 'Si    SRM640c':(0,(5.4311946,5.4311946,5.4311946,90,90,90)),
 'CeO2  SRM674b':(0,(5.411651,5.411651,5.411651,90,90,90)),
-'Al2O3 SRM676a':(3,(4.759091,4.759091,12.991779,90,90,120))}
+'Al2O3 SRM676a':(3,(4.759091,4.759091,12.991779,90,90,120)),
+'Ni   @ 298K':(0,(3.52475,3.52475,3.52475,90,90,90)),
+'NaCl @ 298K':(0,(5.6402,5.6402,5.6402,90,90,90))}

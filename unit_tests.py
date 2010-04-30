bb438fcc39b853b2045ef6da608501653079b8e5
Brian Toby <toby@anl.gov>
2010-04-30 19:18:26 +0000

    support runing unit_tests.py w/o nosetests
    git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@61 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

--- unit_tests.py
+++ unit_tests.py
@@ -7,3 +7,10 @@ def test_GSASIIspc2():
     GSASIIspc.test2()
 def test_GSASIIspc3():
     GSASIIspc.test3()
+
+if __name__ == '__main__':
+    test_GSASIIspc()
+    test_GSASIIspc1()
+    test_GSASIIspc2()
+    test_GSASIIspc3()
+    print "OK"

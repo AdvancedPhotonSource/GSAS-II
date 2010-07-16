4f7c838f2d3cfa77a7fbd1b1e7b9a9df252d1118
Robert Von Dreele <vondreele@anl.gov>
2010-07-16 15:10:40 +0000

    modified for refactoring of integration
    git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@111 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

--- fsource/histogram2d.for
+++ fsource/histogram2d.for
@@ -1,5 +1,5 @@
-      SUBROUTINE HISTOGRAM2D(N,X,Y,Z,NXBINS,NYBINS,XLIM,YLIM,
-     1  NST,HST,HSTX,HSTY)
+      SUBROUTINE HISTOGRAM2D(N,X,Y,Z,NXBINS,NYBINS,XLIM,YLIM,DX,DY,
+     1  NST,HST)
 
 Cf2py intent(in) N
 Cf2py intent(in) X
@@ -12,37 +12,24 @@ Cf2py intent(in) NXBINS
 Cf2py intent(in) NYBINS
 Cf2py intent(in) XLIM
 Cf2py intent(in) YLIM
+Cf2py intent(in) DX
+Cf2py intent(in) DY
 Cf2py intent(in,out) NST
 Cf2py depend(NXBINS,NYBINS) NST
 Cf2py intent(in,out) HST
 Cf2py depend(NXBINS,NYBINS) HST
-Cf2py intent(in,out) HSTX
-Cf2py depend(NXBINS) HSTX
-Cf2py intent(in,out) HSTY
-Cf2py depend(NYBINS) HSTY
 
       IMPLICIT NONE
       INTEGER*4   N
       REAL*4      X(0:N-1),Y(0:N-1),Z(0:N-1)
       INTEGER*4   NXBINS,NYBINS
       REAL*8      XLIM(0:1),YLIM(0:1)
+      REAL*8      DX,DY
       INTEGER*8   NST(0:NXBINS-1,0:NYBINS-1)
       REAL*4      HST(0:NXBINS-1,0:NYBINS-1)
-      REAL*8      HSTX(0:NXBINS),HSTY(0:NYBINS)
 
       INTEGER*4   I,J,K
-      REAL*8      DX,DY
 
-      DX = (XLIM(1)-XLIM(0))/FLOAT(NXBINS)
-      DY = (YLIM(1)-YLIM(0))/FLOAT(NYBINS)
-
-      DO I=0,NXBINS
-        HSTX(I) = XLIM(0)+FLOAT(I)*DX
-      END DO
-      DO J=0,NYBINS
-        HSTY(J) = YLIM(0)+FLOAT(J)*DY
-      END DO
-      
       DO K=0,N-1
         IF ( ( X(K) .GE. XLIM(0) .AND. X(K) .LE. XLIM(1)) .AND.
      1    (Y(K) .GE. YLIM(0) .AND. Y(K). LE. YLIM(1)) ) THEN

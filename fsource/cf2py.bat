a77d5cd2ac6995c83d8815c882d9233b02825a53
Robert Von Dreele <vondreele@anl.gov>
2011-01-04 22:29:09 +0000

    git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@207 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

--- fsource/cf2py.bat
+++ fsource/cf2py.bat
@@ -1,4 +1,2 @@
-path=%path:MinGW32=MinGW64%
-f2py -c -m --opt='-O1' %1 %1.for %2 --compiler=mingw32
-path=%path:MinGW64=MinGW32%
-move %1.pyd ..\binwin64-2.6
+f2py -c -m %1 %1.for %2 --compiler=mingw32 -Lc:\gsas\lib -lgsas
+mv %1.pyd ..\binwin2.6

6bcdf89a76e5a729151ad52ac82ba7163fb5a2c6
Robert Von Dreele <vondreele@anl.gov>
2011-01-05 18:02:36 +0000

    git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@212 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

---
+++ fsource/cf2py32.bat
@@ -0,0 +1,14 @@
+if [%2] == [] goto ONE
+	cd %2
+	gfortran -c *.for -w -O2 -fno-automatic -finit-local-zero -malign-double -mwindows
+        ar crs  lib%2.a *.o
+	del *.o
+	move  /Y lib%2.a ..
+	cd ..
+	f2py -c -m %1 %1.for --compiler=mingw32 -L./ -l%2
+	goto TWO
+:ONE
+f2py -c -m %1 %1.for --compiler=mingw32 
+:TWO
+move %1.pyd ..\binwin-2.6
+

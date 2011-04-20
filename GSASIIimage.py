removed azm rotation - didn't work fix azimuthal polarization
git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@267 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

GSASIIimage.py
@@ -292,8 +292,6 @@ def GetTthAzmDsp(x,y,data):
     phi = data['rotation']
     LRazim = data['LRazimuth']
     azmthoff = data['azmthOff']
-    azmRot = data['azmthRotate']
-    Full = data['fullIntegrate']
     dx = np.array(x-cent[0],dtype=np.float32)
     dy = np.array(y-cent[1],dtype=np.float32)
     X = np.array(([dx,dy,np.zeros_like(dx)]),dtype=np.float32).T
@@ -302,10 +300,7 @@ def GetTthAzmDsp(x,y,data):
     tth = npatand(np.sqrt(dx**2+dy**2-Z**2)/(dist-Z))
     dsp = wave/(2.*npsind(tth/2.))
     azm = (npatan2d(dx,-dy)+azmthoff+720.)%360.
-    if Full:
-        azm = (azm+azmRot+720.)%360.
-    else:
-        azm = np.where(azm<LRazim[0],azm+360.,azm)
+    azm = np.where(azm<LRazim[0],azm+360.,azm)
     return tth,azm,dsp
     
 def GetTth(x,y,data):
@@ -564,8 +559,6 @@ def ImageIntegrate(image,data,masks):
         LRazm = data['LRazimuth']
     numAzms = data['outAzimuths']
     numChans = data['outChannels']
-    azmRot = data['azmthRotate']
-    Full = data['fullIntegrate']
     Dtth = (LUtth[1]-LUtth[0])/numChans
     Dazm = (LRazm[1]-LRazm[0])/numAzms
     NST = np.zeros(shape=(numAzms,numChans),order='F',dtype=np.float32)
@@ -610,8 +603,6 @@ def ImageIntegrate(image,data,masks):
             H2 = LUtth
         if Dazm:        
             H1 = np.array([azm for azm in np.linspace(LRazm[0],LRazm[1],numAzms+1)])
-            if Full:
-                H1 = H1+azmRot
         else:
             H1 = LRazm
         Nup += 1

GSASIIpeak.py
@@ -165,9 +165,8 @@ def Polarization(Pola,Tth,Azm=0.0):
 #       which (if either) of these is "right"?
 #    return (Pola*npcosd(Azm)**2+(1.-Pola)*npsind(Azm)**2)*npcosd(Tth)**2+ \
 #        Pola*npsind(Azm)**2+(1.-Pola)*npcosd(Azm)**2
-#    return (Pola*npcosd(Azm)**2+npsind(Azm)**2)*npcosd(Tth)**2+   \
-#        Pola*npsind(Azm)**2+npcosd(Azm)**2
-    return Pola*npcosd(Tth)**2+1.0
+    return ((1.0-Pola)*npcosd(Azm)**2+Pola*npsind(Azm)**2)*npcosd(Tth)**2+   \
+        (1.0-Pola)*npsind(Azm)**2+Pola*npcosd(Azm)**2
     
 def Oblique(ObCoeff,Tth):
     if ObCoeff:
@@ -671,9 +670,7 @@ def CalcPDF(data,inst,xydata):
     Tth = xydata['Sample'][1][0]
     dt = (Tth[1]-Tth[0])
     xydata['IofQ'][1][1] /= Absorb(data['Geometry'],Abs,data['Diam'],Tth)
-    pola = Polarization(inst['Polariz.'],Tth,Azm=inst['Azimuth'])
-    auxPlot.append([Tth,pola,'Pola'])      
-    xydata['IofQ'][1][1] /= pola
+    xydata['IofQ'][1][1] /= Polarization(inst['Polariz.'],Tth,Azm=inst['Azimuth'])
     xydata['IofQ'][1][1] *= Oblique(data['ObliqCoeff'],Tth)
     XY = xydata['IofQ'][1]    
     #convert to Q
@@ -705,13 +702,14 @@ def CalcPDF(data,inst,xydata):
     FFSq,SqFF,CF = GetAsfMean(ElList,(xydata['SofQ'][1][0]/(4.0*np.pi))**2)  #these are <f^2>,<f>^2,Cf
     Q = xydata['SofQ'][1][0]
     ruland = Ruland(data['Ruland'],wave,Q,CF)
-    auxPlot.append([Q,ruland,'Ruland'])      
+#    auxPlot.append([Q,ruland,'Ruland'])      
     CF *= ruland
-    auxPlot.append([Q,CF,'Compton*Ruland'])
-    scale =       np.sum((FFSq+CF)[minQ:maxQ])/np.sum(xydata['SofQ'][1][1][minQ:maxQ])
+#    auxPlot.append([Q,CF,'Compton*Ruland'])
+    scale = np.sum((FFSq+CF)[minQ:maxQ])/np.sum(xydata['SofQ'][1][1][minQ:maxQ])
     xydata['SofQ'][1][1] *= scale
     xydata['SofQ'][1][1] -= CF
     xydata['SofQ'][1][1] = xydata['SofQ'][1][1]/SqFF
+    
 
     xydata['FofQ'] = copy.deepcopy(xydata['SofQ'])
     xydata['FofQ'][1][1] = xydata['FofQ'][1][0]*(xydata['SofQ'][1][1]-1.0)

GSASIIplot.py
@@ -1162,9 +1162,6 @@ def PlotImage(self,newPlot=False,event=None,newImage=True):
     def OnImMotion(event):
         Page.canvas.SetToolTipString('')
         sizexy = Data['size']
-        azmRot = 0.
-        if Data['fullIntegrate']:
-            azmRot = Data['azmthRotate']
         if event.xdata and event.ydata:                 #avoid out of frame errors
             Page.canvas.SetCursor(wx.CROSS_CURSOR)
             item = self.itemPicked
@@ -1180,7 +1177,7 @@ def PlotImage(self,newPlot=False,event=None,newImage=True):
                     ypos = event.ydata-ycent
                     tth,azm = G2img.GetTthAzm(event.xdata,event.ydata,Data)
                     if 'line3' in  str(item) or 'line4' in str(item) and not Data['fullIntegrate']:
-                        Page.canvas.SetToolTipString('%6d deg'%(azm-azmRot))
+                        Page.canvas.SetToolTipString('%6d deg'%(azm))
                     elif 'line1' in  str(item) or 'line2' in str(item):
                         Page.canvas.SetToolTipString('%8.3fdeg'%(tth))                           
             else:
@@ -1197,7 +1194,7 @@ def PlotImage(self,newPlot=False,event=None,newImage=True):
                     self.G2plotNB.status.SetFields(['','Polygon mask pick - LB next point, RB close polygon'])
                 else:
                     self.G2plotNB.status.SetFields(\
-                        ['','Detector 2-th =%9.3fdeg, dsp =%9.3fA, Q = %6.5fA-1, azm = %7.2fdeg, I = %6d'%(tth,dsp,Q,azm-azmRot,Int)])
+                        ['','Detector 2-th =%9.3fdeg, dsp =%9.3fA, Q = %6.5fA-1, azm = %7.2fdeg, I = %6d'%(tth,dsp,Q,azm,Int)])
 
     def OnImPlotKeyPress(event):
         try:
@@ -1493,10 +1490,8 @@ def PlotImage(self,newPlot=False,event=None,newImage=True):
             ellO = G2img.GetEllipse(dspO,Data)           #Ditto & more likely for outer ellipse
             if Data['fullIntegrate']:
                 Azm = np.array(range(0,361))
-                AzmRot = Data['azmthRotate']
             else:
                 Azm = np.array(range(LRAzim[0],LRAzim[1]+1))-AzmthOff
-                AzmRot = 0.0
             if ellI:
                 xyI = []
                 for azm in Azm:
@@ -1516,7 +1511,10 @@ def PlotImage(self,newPlot=False,event=None,newImage=True):
                 Plot.plot([arcxI[-1],arcxO[-1]],[arcyI[-1],arcyO[-1]],picker=3)
             if Nazm > 1:
                 for i in range(Nazm):
-                    cake = (LRAzim[0]+i*delAzm+AzmRot+720)%360
+                    if Data['fullIntegrate']:
+                        cake = (LRAzim[0]+i*delAzm+720)%360
+                    else:
+                        cake = LRAzim[0]+i*delAzm
                     ind = np.searchsorted(Azm,cake)
                     Plot.plot([arcxI[ind],arcxO[ind]],[arcyI[ind],arcyO[ind]],color='k',dashes=(5,5))
                     
0f3aa1d90e88843b3516c49765afaee759aa26e0
Robert Von Dreele <vondreele@anl.gov>
2011-05-06 18:59:14 +0000

    git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@284 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

---
+++ fsource/cf2py26.bat
@@ -0,0 +1,16 @@
+set path=%path:Python27=Python26%
+if [%2] == [] goto ONE
+	cd %2
+	g77 -c *.for -w -O2 -fno-automatic -finit-local-zero -malign-double -mwindows
+        ar crs  lib%2.a *.o
+	del *.o
+	move  /Y lib%2.a ..
+	cd ..
+	f2py -c -m %1 %1.for --compiler=mingw32 --fcompiler=gnu -L./ -l%2
+	goto TWO
+:ONE
+f2py -c -m %1 %1.for --compiler=mingw32 --fcompiler=gnu
+:TWO
+move %1.pyd ..\binwin2.6
+set path=%path:Python26=Python27%
+

8e9d4c323f581dfca01774bd3ccfc0e89cf1e1ea
Robert Von Dreele <vondreele@anl.gov>
2011-06-21 20:40:56 +0000

    useful test module
    git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@308 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

---
+++ GSASIItestplot.py
@@ -0,0 +1,39 @@
+import wx
+import wx.aui
+import matplotlib as mpl
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as Canvas
+from matplotlib.backends.backend_wxagg import NavigationToolbar2Wx as Toolbar
+
+class Plot(wx.Panel):
+    def __init__(self, parent, id = -1, dpi = None, **kwargs):
+        wx.Panel.__init__(self, parent, id=id, **kwargs)
+        self.figure = mpl.figure.Figure(dpi=dpi, #figsize=(5,7)
+                                        )
+        self.canvas = Canvas(self, -1, self.figure)
+        self.toolbar = Toolbar(self.canvas)
+        self.toolbar.Realize()
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.canvas,1,wx.EXPAND)
+        sizer.Add(self.toolbar, 0 , wx.LEFT | wx.EXPAND)
+        self.SetSizer(sizer)
+
+class PlotNotebook(wx.Panel):
+    def __init__(self, id = -1):
+        self.app = wx.PySimpleApp()
+        self.frame = wx.Frame(None,-1,'Plotter', size=wx.Size(600,600))
+        wx.Panel.__init__(self, self.frame, id=id)
+        self.nb = wx.aui.AuiNotebook(self)
+        sizer = wx.BoxSizer()
+        sizer.Add(self.nb, 1, wx.EXPAND)
+        self.SetSizer(sizer)
+
+    def StartEventLoop(self):
+        self.frame.Show()
+        self.app.MainLoop()
+
+
+    def add(self,name="plot"):
+       page = Plot(self.nb)
+       self.nb.AddPage(page,name)
+       return page.figure

d58bd0d0d469c4175f9ebd092bd1bfda7b08d159
Robert Von Dreele <vondreele@anl.gov>
2011-08-20 17:11:35 +0000

    git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@352 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

--- fsource/pypowder.for
+++ fsource/pypowder.for
@@ -23,29 +23,3 @@ Cf2py depend(NPTS) PRFUNC
       END DO
       RETURN
       END
-
-      SUBROUTINE PYPSVFCJ2(NPTS,DTT,TTHETA,SIG,GAM,SPH,PRFUNC)
-C DTT in degrees
-C TTHETA in degrees
-C SPH is S/L + H/L
-Cf2py intent(in) NPTS
-Cf2py intent(in) DTT
-cf2py depend(NPTS) DTT
-Cf2py intent(in) TTHETA
-Cf2py intent(in) SIG
-Cf2py intent(in) GAM
-Cf2py intent(in) SPH
-Cf2py intent(out) PRFUNC
-Cf2py depend(NPTS) PRFUNC
-
-      REAL*4 DTT(0:NPTS-1),PRFUNC(0:NPTS-1)
-      SL = SPH/2.0
-      FW = (2.355*SQRT(SIG)+GAM)/100.0
-      FMIN = 10.0*(-FW-SPH*COSD(TTHETA))
-      FMAX = 15.0*FW
-      DO I=0,NPTS-1
-        CALL PSVFCJ2(DTT(I)*100.,TTHETA*100.,SL,SL,SIG,GAM,
-     1    PRFUNC(I))
-      END DO
-      RETURN
-      END

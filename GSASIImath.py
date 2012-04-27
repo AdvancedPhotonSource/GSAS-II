fix "hard" Hessian singular matrix problems - now deletes parameters fix Pawley overlap reflection constraint problem
git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@578 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

GSASIImath.py
@@ -102,8 +102,8 @@ def HessianLSQ(func,x0,Hess,args=(),ftol=1.49012e-8,xtol=1.49012e-8, maxcyc=0):
         chisq0 = np.sum(M**2)
         Yvec,Amat = Hess(x0,*args)
         Adiag = np.sqrt(np.diag(Amat))
-        if 0.0 in Adiag:                #hard singularity in matrix
-            psing = list(np.where(Adiag == 0.)[0])
+        psing = np.where(np.abs(Adiag) < 1.e-14,True,False)
+        if np.any(psing):                #hard singularity in matrix
             return [x0,None,{'num cyc':icycle,'fvec':M,'nfev':nfev,'lamMax':lamMax,'psing':psing}]
         Anorm = np.outer(Adiag,Adiag)
         Yvec /= Adiag
@@ -114,6 +114,7 @@ def HessianLSQ(func,x0,Hess,args=(),ftol=1.49012e-8,xtol=1.49012e-8, maxcyc=0):
             try:
                 Xvec = nl.solve(Amatlam,Yvec)
             except LinAlgError:
+                print 'ouch #1'
                 psing = list(np.where(np.diag(nl.gr(Amatlam)[1]) < 1.e-14)[0])
                 return [x0,None,{'num cyc':icycle,'fvec':M,'nfev':nfev,'lamMax':lamMax,'psing':psing}]
             Xvec /= Adiag
@@ -136,6 +137,7 @@ def HessianLSQ(func,x0,Hess,args=(),ftol=1.49012e-8,xtol=1.49012e-8, maxcyc=0):
         Bmat = nl.inv(Amat)
         return [x0,Bmat,{'num cyc':icycle,'fvec':M,'nfev':nfev,'lamMax':lamMax,'psing':[]}]
     except nl.LinAlgError:
+        print 'ouch #2'
         psing = []
         if maxcyc:
             psing = list(np.where(np.diag(nl.qr(Amat)[1]) < 1.e-14)[0])
@@ -520,7 +522,8 @@ def ValEsd(value,esd=0,nTZ=False):                  #NOT complete - don't use
 
 def FourierMap(data,reflData):
     
-    import scipy.fftpack as fft
+#    import scipy.fftpack as fft
+    import numpy.fft as fft
     generalData = data['General']
     if not generalData['Map']['MapType']:
         print '**** ERROR - Fourier map not defined'
@@ -601,14 +604,12 @@ def findOffset(SGData,rho,Fhkl):
         F = Flist[i]
         hkl = np.unravel_index(Fdict[F],hklShape)
         iabsnt,mulp,Uniq,phi = G2spc.GenHKLf(list(hkl-hklHalf),SGData)
-        Uniq = np.array(Uniq,dtype='i')
-        Uniq = np.concatenate((Uniq,-Uniq))+hklHalf
-        phi = np.concatenate((phi,-phi))
-#        print hkl-hklHalf
-#        for j,H in enumerate(Uniq):
-#            Fh = Fhkl[H[0],H[1],H[2]]
-#            h,k,l = H-hklHalf
-#            print '(%3d,%3d,%3d) %5.2f %9.5f'%(h,k,l,phi[j],np.angle(Fh,deg=True))        
+        Uniq = np.array(Uniq,dtype='i')+hklHalf
+        print hkl-hklHalf
+        for j,H in enumerate(Uniq):
+            Fh = Fhkl[H[0],H[1],H[2]]
+            h,k,l = H-hklHalf
+            print '(%3d,%3d,%3d) %5.2f %9.5f'%(h,k,l,phi[j],np.angle(Fh,deg=True))        
         i += 1
         
         
@@ -645,10 +646,7 @@ def ChargeFlip(data,reflData,pgbar):
                 ff *= G2el.ScatFac(FFtable,SQ)[0]
             E = np.sqrt(ref[8])/ff
             ph = ref[10]
-            if SGData['SGInv']:
-                ph = rn.randint(0,1)*180.
-            else:
-                ph = rn.uniform(0.,360.)
+#            ph = rn.uniform(0.,360.)
             for i,hkl in enumerate(ref[11]):
                 hkl = np.asarray(hkl,dtype='i')
                 dp = 360.*ref[12][i]
@@ -718,10 +716,7 @@ def SearchMap(data,keepDup=False):
         
     def rhoCalc(parms,rX,rY,rZ,res,SGLaue):
         Mag,x0,y0,z0,sig = parms
-        if SGLaue in ['3','3m1','31m','6/m','6/mmm']:
-            return norm*Mag*np.exp(-((x0-rX)**2+(y0-rY)**2+(x0-rX)*(y0-rY)+(z0-rZ)**2)/(2.*sig**2))/(sig*res**3)
-        else:
-            return norm*Mag*np.exp(-((x0-rX)**2+(y0-rY)**2+(z0-rZ)**2)/(2.*sig**2))/(sig*res**3)
+        return norm*Mag*np.exp(-((x0-rX)**2+(y0-rY)**2+(z0-rZ)**2)/(2.*sig**2))/(sig*res**3)
         
     def peakFunc(parms,rX,rY,rZ,rho,res,SGLaue):
         Mag,x0,y0,z0,sig = parms

GSASIIstruct.py
@@ -487,34 +487,42 @@ def GetBLtable(General):
 def GetPawleyConstr(SGLaue,PawleyRef,pawleyVary):
     if SGLaue in ['-1','2/m','mmm']:
         return                      #no Pawley symmetry required constraints
+    eqvDict = {}
     for i,varyI in enumerate(pawleyVary):
+        eqvDict[varyI] = []
         refI = int(varyI.split(':')[-1])
         ih,ik,il = PawleyRef[refI][:3]
-        for varyJ in pawleyVary[0:i]:
+        for varyJ in pawleyVary[i+1:]:
             refJ = int(varyJ.split(':')[-1])
             jh,jk,jl = PawleyRef[refJ][:3]
             if SGLaue in ['4/m','4/mmm']:
                 isum = ih**2+ik**2
                 jsum = jh**2+jk**2
                 if abs(il) == abs(jl) and isum == jsum:
-                    G2mv.StoreEquivalence(varyJ,(varyI,))
+                    eqvDict[varyI].append(varyJ) 
             elif SGLaue in ['3R','3mR']:
                 isum = ih**2+ik**2+il**2
                 jsum = jh**2+jk**2*jl**2
                 isum2 = ih*ik+ih*il+ik*il
                 jsum2 = jh*jk+jh*jl+jk*jl
                 if isum == jsum and isum2 == jsum2:
-                    G2mv.StoreEquivalence(varyJ,(varyI,))
+                    eqvDict[varyI].append(varyJ) 
             elif SGLaue in ['3','3m1','31m','6/m','6/mmm']:
                 isum = ih**2+ik**2+ih*ik
                 jsum = jh**2+jk**2+jh*jk
                 if abs(il) == abs(jl) and isum == jsum:
-                    G2mv.StoreEquivalence(varyJ,(varyI,))
+                    eqvDict[varyI].append(varyJ) 
             elif SGLaue in ['m3','m3m']:
                 isum = ih**2+ik**2+il**2
                 jsum = jh**2+jk**2+jl**2
                 if isum == jsum:
-                    G2mv.StoreEquivalence(varyJ,(varyI,))
+                    eqvDict[varyI].append(varyJ) 
+    for item in pawleyVary:
+        if eqvDict[item]:
+            for item2 in pawleyVary:
+                if item2 in eqvDict[item]:
+                    eqvDict[item2] = []
+            G2mv.StoreEquivalence(item,eqvDict[item])
                     
 def cellVary(pfx,SGData): 
     if SGData['SGLaue'] in ['-1',]:
@@ -2897,7 +2905,7 @@ def Refine(GPXfile,dlg):
     #     print ' *** ERROR - you have not set the refine flags for constraints consistently! ***'
     #     print msg
     #     raise Exception(' *** Refine aborted ***')
-    print G2mv.VarRemapShow(varyList)
+    #print G2mv.VarRemapShow(varyList)
     G2mv.Map2Dict(parmDict,varyList)
     Rvals = {}
     while True:
@@ -2946,9 +2954,11 @@ def Refine(GPXfile,dlg):
         except TypeError:          #result[1] is None on singular matrix
             print '**** Refinement failed - singular matrix ****'
             if 'Hessian' in Controls['deriv type']:
-                for i in result[2]['psing'].reverse():
-                        print 'Removing parameter: ',varyList[i]
-                        del(varyList[i])                    
+                num = len(varyList)-1
+                for i,val in enumerate(np.flipud(result[2]['psing'])):
+                    if val:
+                        print 'Removing parameter: ',varyList[num-i]
+                        del(varyList[num-i])                    
             else:
                 Ipvt = result[2]['ipvt']
                 for i,ipvt in enumerate(Ipvt):
@@ -3128,9 +3138,11 @@ def SeqRefine(GPXfile,dlg):
             except TypeError:          #result[1] is None on singular matrix
                 print '**** Refinement failed - singular matrix ****'
                 if 'Hessian' in Controls['deriv type']:
-                    for i in result[2]['psing'].reverse():
-                            print 'Removing parameter: ',varyList[i]
-                            del(varyList[i])                    
+                    num = len(varyList)-1
+                    for i,val in enumerate(np.flipud(result[2]['psing'])):
+                        if val:
+                            print 'Removing parameter: ',varyList[num-i]
+                            del(varyList[num-i])                    
                 else:
                     Ipvt = result[2]['ipvt']
                     for i,ipvt in enumerate(Ipvt):
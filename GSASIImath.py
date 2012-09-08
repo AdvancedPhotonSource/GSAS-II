fix rotation about view direction eliminate divide by zero error in quaternion stuff
git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@755 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

GSASIImath.py
@@ -1064,7 +1064,10 @@ def AV2Q(A,V):
     '''
     Q = np.zeros(4)
     d = np.sqrt(np.sum(np.array(V)**2))
-    V /= d
+    if d:
+        V /= d
+    else:
+        return [1.,0.,0.,0.]    #identity
     p = A/2.
     Q[0] = np.cos(p)
     s = np.sin(p)
@@ -1077,7 +1080,10 @@ def AVdeg2Q(A,V):
     '''
     Q = np.zeros(4)
     d = np.sqrt(np.sum(np.array(V)**2))
-    V /= d
+    if d:
+        V /= d
+    else:
+        return [1.,0.,0.,0.]    #identity
     p = A/2.
     Q[0] = cosd(p)
     S = sind(p)

GSASIIplot.py
@@ -2671,19 +2671,26 @@ def PlotStructure(G2frame,data):
     def SetRotationZ(newxy):                        
 #first get rotation vector (= view vector) in screen coords. & angle increment        
         View = glGetIntegerv(GL_VIEWPORT)
+        cent = [View[2]/2,View[3]/2]
         oldxy = drawingData['oldxy']
         if not len(oldxy): oldxy = list(newxy)
         dxy = newxy-oldxy
         drawingData['oldxy'] = list(newxy)
         V = drawingData['viewDir']
-        X0 = drawingData['viewPoint'][0]
-        A = (dxy[0]+dxy[1])*.25
-# next transform vector back to xtal coordinates via inverse quaternion
-# & make new quaternion
+        A = [0,0]
+        A[0] = dxy[1]*.25
+        A[1] = dxy[0]*.25
+        if newxy[0] > cent[0]:
+            A[0] *= -1
+        if newxy[1] < cent[1]:
+            A[1] *= -1        
+# next transform vector back to xtal coordinates & make new quaternion
         Q = drawingData['Quaternion']
         V = np.inner(Amat,V)
-        DQ = G2mth.AVdeg2Q(A,V)
-        Q = G2mth.prodQQ(Q,DQ)
+        Qx = G2mth.AVdeg2Q(A[0],V)
+        Qy = G2mth.AVdeg2Q(A[1],V)
+        Q = G2mth.prodQQ(Q,Qx)
+        Q = G2mth.prodQQ(Q,Qy)
         drawingData['Quaternion'] = Q
 
     def RenderBox():
fix derivative errors for crystallite size & eliminate floor on X & Y profile coeff. Stops anomalous behavior if negative.
git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@866 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

GSASIIstruct.py
@@ -1727,8 +1727,8 @@ def GetHistogramData(Histograms,Print=True,pFile=None):
             instDict[insName] = Inst[item][1]
             if Inst[item][2]:
                 insVary.append(insName)
-        instDict[pfx+'X'] = max(instDict[pfx+'X'],0.001)
-        instDict[pfx+'Y'] = max(instDict[pfx+'Y'],0.001)
+#        instDict[pfx+'X'] = max(instDict[pfx+'X'],0.001)
+#        instDict[pfx+'Y'] = max(instDict[pfx+'Y'],0.001)
         instDict[pfx+'SH/L'] = max(instDict[pfx+'SH/L'],0.0005)
         return dataType,instDict,insVary
         
@@ -2786,8 +2786,8 @@ def GetSampleSigGamDerv(refl,wave,G,GB,phfx,calcControls,parmDict):
     #crystallite size derivatives
     if calcControls[phfx+'SizeType'] == 'isotropic':
         Sgam = 1.8*wave/(np.pi*parmDict[phfx+'Size;i']*costh)
-        gamDict[phfx+'Size;i'] = -900.*wave*parmDict[phfx+'Size;mx']/(np.pi*costh)
-        sigDict[phfx+'Size;i'] = -1800.*Sgam*wave*(1.-parmDict[phfx+'Size;mx'])**2/(np.pi*costh*ateln2)
+        gamDict[phfx+'Size;i'] = -1.8*wave*parmDict[phfx+'Size;mx']/(np.pi*costh)
+        sigDict[phfx+'Size;i'] = -3.6*Sgam*wave*(1.-parmDict[phfx+'Size;mx'])**2/(np.pi*costh*ateln2)
     elif calcControls[phfx+'SizeType'] == 'uniaxial':
         H = np.array(refl[:3])
         P = np.array(calcControls[phfx+'SizeAxis'])
@@ -2975,7 +2975,7 @@ def GetFobsSq(Histograms,Phases,parmDict,calcControls):
             hId = Histogram['hId']
             hfx = ':%d:'%(hId)
             Limits = calcControls[hfx+'Limits']
-            shl = max(parmDict[hfx+'SH/L'],0.002)
+            shl = max(parmDict[hfx+'SH/L'],0.0005)
             Ka2 = False
             kRatio = 0.0
             if hfx+'Lam1' in parmDict.keys():

testGSASIIstruct.py
@@ -33,9 +33,11 @@ def test1():
     limits = Histogram['Limits'][1]
     data = Histogram['Data']
     xdata = data[0]
+    xB = np.searchsorted(xdata,limits[0])
+    xF = np.searchsorted(xdata,limits[1])
     fplot = plotter.add('function test').gca()
-    yc,yb = G2st.getPowderProfile(parmDict,xdata,varylist,Histogram,Phases,calcControls,pawleyLookup)
-    fplot.plot(xdata,yc+yb,'r')
+    yc,yb = G2st.getPowderProfile(parmDict,xdata[xB:xF],varylist,Histogram,Phases,calcControls,pawleyLookup)
+    fplot.plot(xdata[xB:xF],yc+yb,'r')
 
 def test2(name,delt):
     if NeedTestData: TestData()
@@ -43,21 +45,23 @@ def test2(name,delt):
     limits = Histogram['Limits'][1]
     data = Histogram['Data']
     xdata = data[0]
+    xB = np.searchsorted(xdata,limits[0])
+    xF = np.searchsorted(xdata,limits[1])
     hplot = plotter.add('derivatives test for '+name).gca()
-    ya = G2st.getPowderProfileDerv(parmDict,xdata,varyList,Histogram,Phases,calcControls,pawleyLookup)[0]
-    hplot.plot(xdata,ya)
+    ya = G2st.getPowderProfileDerv(parmDict,xdata[xB:xF],varyList,Histogram,Phases,calcControls,pawleyLookup)[0]
+    hplot.plot(xdata[xB:xF],ya,'b')
     if 'dA' in name:
         name = ''.join(name.split('d'))
         varyList = [name,]
     parmDict[name] -= delt
-    y0,yb = G2st.getPowderProfile(parmDict,xdata,varyList,Histogram,Phases,calcControls,pawleyLookup)
+    y0,yb = G2st.getPowderProfile(parmDict,xdata[xB:xF],varyList,Histogram,Phases,calcControls,pawleyLookup)
     y0 += yb
     parmDict[name] += 2.*delt
-    y1,yb = G2st.getPowderProfile(parmDict,xdata,varyList,Histogram,Phases,calcControls,pawleyLookup)
+    y1,yb = G2st.getPowderProfile(parmDict,xdata[xB:xF],varyList,Histogram,Phases,calcControls,pawleyLookup)
     y1 += yb 
     yn = (y1-y0)/(2.*delt)
-    hplot.plot(xdata,yn,'r+')
-    hplot.plot(xdata,ya-yn)
+    hplot.plot(xdata[xB:xF],yn,'r+')
+    hplot.plot(xdata[xB:xF],ya-yn,'g')
     
 if __name__ == '__main__':
     if NeedTestData: TestData()
@@ -70,10 +74,9 @@ if __name__ == '__main__':
     for name in parmDict:
         print name,parmDict[name]
     names = [
-        ['0:0:Size:mx',0.001],
-        ['0:0:Mustrain:mx',0.001],
-        ['0:0:Size:i',0.001],
-        ['0:0:Mustrain:i',0.1],
+        ['0:0:Size;i',0.01],
+        ['0:0:Mustrain:0',0.001],
+        ['0:0:Mustrain:1',0.001],
         ]
     for [name,delt] in names:
         test2(name,delt)
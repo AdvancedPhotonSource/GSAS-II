add 'Global' to constraints remove a stray debug print fix U6toUij so it returns a numpy array fixes to Q2AV & Q2AVdeg RB changes - get derivatives in
git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@885 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

GSASIIlattice.py
@@ -239,19 +239,17 @@ def U6toUij(U6):
     returns:
         Uij - numpy [3][3] array of uij
     """
-    U = np.zeros(shape=(3,3))
-    U = [
+    U = np.array([
         [U6[0],  U6[3],  U6[4]], 
         [U6[3],  U6[1],  U6[5]], 
-        [U6[4],  U6[5],  U6[2]]]
+        [U6[4],  U6[5],  U6[2]]])
     return U
 
 def UijtoU6(U):
     """Fill vector [U11,U22,U33,U12,U13,U23] from Uij 
     NB: there is a non numpy version in GSASIIspc: Uij2U
     """
-    U6 = np.zeros(6)
-    U6 = [U[0][0],U[1][1],U[2][2],U[0][1],U[0][2],U[1][2]]
+    U6 = np.array([U[0][0],U[1][1],U[2][2],U[0][1],U[0][2],U[1][2]])
     return U6
 
 def Uij2betaij(Uij,G):

GSASIImath.py
@@ -267,18 +267,15 @@ def UpdateRBUIJ(Bmat,Cart,RBObj):
     ''' Returns atom I/A, Uiso or UIJ for atoms at XYZ as described by RBObj
     '''
     TLStype,TLS = RBObj['ThermalMotion'][:2]
-    Tmat = np.zeros((3,3))
-    Lmat = np.zeros((3,3))
-    Smat = np.zeros((3,3))
+    T = np.zeros(6)
+    L = np.zeros(6)
+    S = np.zeros(8)
     if 'T' in TLStype:
         T = TLS[:6]
-#        Tmat = G2lat.U6toUij(T)
     if 'L' in TLStype:
         L = np.array(TLS[6:12])*(np.pi/180.)**2
-#        Lmat = np.array(G2lat.U6toUij(L))
     if 'S' in TLStype:
         S = np.array(TLS[12:])*(np.pi/180.)
-#        Smat = np.array([[S[6],S[0],S[1]],[S[2],S[7],S[3]],[S[4],S[5],0]])
     g = np.inner(Bmat,Bmat.T)
     gvec = 1./np.sqrt(np.array([g[0][0]**2,g[1][1]**2,g[2][2]**2,
         g[0][0]*g[1][1],g[0][0]*g[2][2],g[1][1]*g[2][2]]))
@@ -286,7 +283,6 @@ def UpdateRBUIJ(Bmat,Cart,RBObj):
     Q = RBObj['Orient'][0]
     for X in Cart:
         X = prodQVQ(Q,X)
-#        Axyz = np.array([[0,X[2],-X[1]], [-X[2],0,X[0]], [X[1],-X[0],0]])
         if 'U' in TLStype:
             Uout.append(['I',TLS[0],0,0,0,0,0,0])
         elif not 'N' in TLStype:
@@ -301,11 +297,10 @@ def UpdateRBUIJ(Bmat,Cart,RBObj):
             U[5] = T[5]+L[3]*X[0]*X[2]+L[4]*X[0]*X[1]-L[5]*X[0]**2-L[0]*X[2]*X[1]+  \
                 S[0]*X[1]-S[1]*X[2]+S[7]*X[0]
             Umat = G2lat.U6toUij(U)
-#            print 'Umat: ',Umat
-#wrong?      Umat1 = Tmat+np.inner(Axyz,Smat)+np.inner(Smat.T,Axyz.T)+np.inner(np.inner(Axyz.T,Lmat),Axyz)
-#            print 'Umat1: ',Umat1
             beta = np.inner(np.inner(Bmat,Umat),Bmat.T)
             Uout.append(['A',0.0,]+list(G2lat.UijtoU6(beta)*gvec))
+        else:
+            Uout.append(['N',])
     return Uout
     
 def GetSHCoeff(pId,parmDict,SHkeys):
@@ -1449,7 +1444,7 @@ def Q2AVdeg(Q):
         q=r+ai+bj+ck
     '''
     A = 2.*acosd(Q[0])
-    V = np.array([0.,0.,1.])
+    V = np.array(Q[1:])
     if nl.norm(Q[1:]):
         V = Q[1:]/nl.norm(Q[1:])
     return A,V
@@ -1459,7 +1454,7 @@ def Q2AV(Q):
         q=r+ai+bj+ck
     '''
     A = 2.*np.arccos(Q[0])
-    V = np.array([0.,0.,1.])
+    V = np.array(Q[1:])
     if nl.norm(Q[1:]):
         V = Q[1:]/nl.norm(Q[1:])
     return A,V

testGSASIIstruct.py
@@ -19,6 +19,8 @@ def TestData():
     Histogram = cPickle.load(file)
     global Phases
     Phases = cPickle.load(file)
+    global RBData
+    RBData = cPickle.load(file)
     global calcControls
     calcControls = cPickle.load(file)
     global pawleyLookup
@@ -37,7 +39,8 @@ def test1():
     xF = np.searchsorted(xdata,limits[1])
     fplot = plotter.add('function test').gca()
     yc,yb = G2st.getPowderProfile(parmDict,xdata[xB:xF],varylist,Histogram,Phases,calcControls,pawleyLookup)
-    fplot.plot(xdata[xB:xF],yc+yb,'r')
+    fplot.plot(xdata[xB:xF],yc+yb,'r',label='calc+bkg')
+    fplot.legend()
 
 def test2(name,delt):
     if NeedTestData: TestData()
@@ -48,8 +51,8 @@ def test2(name,delt):
     xB = np.searchsorted(xdata,limits[0])
     xF = np.searchsorted(xdata,limits[1])
     hplot = plotter.add('derivatives test for '+name).gca()
-    ya = G2st.getPowderProfileDerv(parmDict,xdata[xB:xF],varyList,Histogram,Phases,calcControls,pawleyLookup)[0]
-    hplot.plot(xdata[xB:xF],ya,'b')
+    ya = G2st.getPowderProfileDerv(parmDict,xdata[xB:xF],varyList,Histogram,Phases,RBData,calcControls,pawleyLookup)[0]
+    hplot.plot(xdata[xB:xF],ya,'b',label='analytic deriv')
     if 'dA' in name:
         name = ''.join(name.split('d'))
         varyList = [name,]
@@ -60,8 +63,9 @@ def test2(name,delt):
     y1,yb = G2st.getPowderProfile(parmDict,xdata[xB:xF],varyList,Histogram,Phases,calcControls,pawleyLookup)
     y1 += yb 
     yn = (y1-y0)/(2.*delt)
-    hplot.plot(xdata[xB:xF],yn,'r+')
-    hplot.plot(xdata[xB:xF],ya-yn,'g')
+    hplot.plot(xdata[xB:xF],yn,'r+',label='numeric deriv')
+    hplot.plot(xdata[xB:xF],ya-yn,'g',label='diff')
+    hplot.legend()
     
 if __name__ == '__main__':
     if NeedTestData: TestData()
@@ -74,9 +78,12 @@ if __name__ == '__main__':
     for name in parmDict:
         print name,parmDict[name]
     names = [
-        ['0:0:Size;i',0.01],
-        ['0:0:Mustrain:0',0.001],
-        ['0:0:Mustrain:1',0.001],
+        ['0::AUiso:0',0.001],
+        ['::RBV;0:0',0.001],
+        ['0::RBVT11:0:0',0.1],
+        ['0::RBVL11:0:0',0.1],
+        ['0::RBVPz:0:0',0.0001],
+        ['0::RBVOa:0:0',0.001],
         ]
     for [name,delt] in names:
         test2(name,delt)
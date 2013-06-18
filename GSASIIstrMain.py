fix to Hessian LSQ to stop if lam > 10e5! fix to rigid body quaternion derivatives
git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@954 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

GSASIIstrMain.py
@@ -40,7 +40,7 @@ acosd = lambda x: 180.*np.arccos(x)/np.pi
 atan2d = lambda y,x: 180.*np.arctan2(y,x)/np.pi
     
 ateln2 = 8.0*math.log(2.0)
-DEBUG = False
+DEBUG = True
 
 
 def Refine(GPXfile,dlg):
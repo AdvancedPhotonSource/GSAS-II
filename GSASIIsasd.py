shade out fixed row of Limits table allow user input of min/max particle diameters & change tutorial to reflect change in Model window fix a bug in Size Dist calculation
git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@1260 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

GSASIIsasd.py
@@ -759,7 +759,7 @@ def SizeDistribution(Profile,ProfDict,Limits,Substances,Sample,data):
     Sky = 10**data['Size']['MaxEnt']['Sky']
     BinsBack = np.ones_like(Bins)*Sky*Scale/Contrast
     Back = data['Back']
-    Q,Io,wt,Ic,Ib = Profile
+    Q,Io,wt,Ic,Ib = Profile[:5]
     Qmin = Limits[1][0]
     Qmax = Limits[1][1]
     wtFactor = ProfDict['wtFactor']
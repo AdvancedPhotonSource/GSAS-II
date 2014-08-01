get HKLF data type into RefDict create a SetDefaultDData routine in GSASII.py fix copyflags for sc extinction coeff fix neutron resonant ff for TOF fix error in making Hessian v-cov matrix - now matches the Jabobian one put names in the Es, Ep & Eg sc extinction coeff fix errors in SCExtinction - still problem with derivatives
git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@1453 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

GSASII.py
@@ -98,9 +98,31 @@ atan2d = lambda x,y: 180.*math.atan2(y,x)/math.pi
 def create(parent):
     return GSASII(parent)
 
+def SetDefaultDData(dType,histoName,NShkl=0,NDij=0):
+    if dType in ['SXC','SNC']:
+        return {'Histogram':histoName,'Show':False,'Scale':[1.0,True],
+            'Babinet':{'BabA':[0.0,False],'BabU':[0.0,False]},
+            'Extinction':['Lorentzian','None', {'Tbar':0.1,'Cos2TM':0.955,
+            'Eg':[1.e-10,False],'Es':[1.e-10,False],'Ep':[1.e-10,False]}]}
+    elif dType == 'SNT':
+        return {'Histogram':histoName,'Show':False,'Scale':[1.0,True],
+            'Babinet':{'BabA':[0.0,False],'BabU':[0.0,False]},
+            'Extinction':['Lorentzian','None', {
+            'Eg':[1.e-10,False],'Es':[1.e-10,False],'Ep':[1.e-10,False]}]}
+    elif 'P' in dType:
+        return {'Histogram':histoName,'Show':False,'Scale':[1.0,False],
+            'Pref.Ori.':['MD',1.0,False,[0,0,1],0,{}],
+            'Size':['isotropic',[1.,1.,1.],[False,False,False],[0,0,1],
+                [1.,1.,1.,0.,0.,0.],6*[False,]],
+            'Mustrain':['isotropic',[1000.0,1000.0,1.0],[False,False,False],[0,0,1],
+                NShkl*[0.01,],NShkl*[False,]],
+            'HStrain':[NDij*[0.0,],NDij*[False,]],                          
+            'Extinction':[0.0,False],'Babinet':{'BabA':[0.0,False],'BabU':[0.0,False]}}
+
 class GSASII(wx.Frame):
     '''Define the main GSAS-II frame and its associated menu items
     '''
+    
     def _Add_FileMenuItems(self, parent):
         item = parent.Append(
             help='Open a GSAS-II project file (*.gpx)', id=wx.ID_ANY,
@@ -624,14 +646,6 @@ class GSASII(wx.Frame):
             for i in result:
                 histoName = TextList[i]
                 if histoName in HKLFlist:
-                    UseList[histoName] = {
-                        'Histogram':histoName,'Show':False,'Scale':[1.0,True],
-                        'Babinet':{'BabA':[0.0,False],'BabU':[0.0,False]},
-                        'Extinction':['Lorentzian','None', {'Tbar':0.1,'Cos2TM':0.955,
-                                                            'Eg':[1.e-10,False],
-                                                            'Es':[1.e-10,False],
-                                                            'Ep':[1.e-10,False]},]
-                        }
                     #redo UpdateHKLFdata(histoName) here:
                     Id = G2gd.GetPatternTreeItemId(self,self.root,histoName)
                     refDict,reflData = self.PatternTree.GetItemPyData(Id)
@@ -640,20 +654,13 @@ class GSASII(wx.Frame):
                         H = list(ref[:3])
                         ref[4] = np.sqrt(1./G2lat.calc_rDsq2(H,G))
                         iabsnt,ref[3],Uniq,phi = G2spc.GenHKLf(H,SGData)
+                    UseList[histoName] = SetDefaultDData(refDict['Type'],histoName)
                 elif histoName in PWDRlist:
-                    UseList[histoName] = {
-                        'Histogram':histoName,'Show':False,
-                        'Scale':[1.0,False],'Pref.Ori.':['MD',1.0,False,[0,0,1],0,{}],
-                        'Size':['isotropic',[1.,1.,1.],[False,False,False],[0,0,1],
-                                [1.,1.,1.,0.,0.,0.],6*[False,]],
-                        'Mustrain':['isotropic',[1000.0,1000.0,1.0],[False,False,False],[0,0,1],
-                                    NShkl*[0.01,],NShkl*[False,]],
-                        'HStrain':[NDij*[0.0,],NDij*[False,]],                          
-                        'Extinction':[0.0,False],'Babinet':{'BabA':[0.0,False],'BabU':[0.0,False]}}
                     Id = G2gd.GetPatternTreeItemId(self,self.root,histoName)
                     refList = self.PatternTree.GetItemPyData(
                         G2gd.GetPatternTreeItemId(self,Id,'Reflection Lists'))
                     refList[generalData['Name']] = []
+                    UseList[histoName] = SetDefaultDData('PWDR',histoName,NShkl=NShkl,NDij=NDij)
                 else:
                     raise Exception('Unexpected histogram '+str(histoName))
         wx.EndBusyCursor()
@@ -713,13 +720,9 @@ class GSASII(wx.Frame):
                 dlg.Destroy()
             HistName = 'HKLF '+HistName
             # make new histogram names unique
-            valuesdict = {
-                'wtFactor':1.0,
-                'Dummy':False,
-                'ranId':ran.randint(0,sys.maxint),
-                }
             if len(rd.Banks):
                 for Bank in rd.Banks:
+                    valuesdict = {'wtFactor':1.0,'Dummy':False,'ranId':ran.randint(0,sys.maxint),}
                     HistName = G2obj.MakeUniqueLabel(HistName,HKLFlist)
                     print 'Read structure factor table '+str(HistName)+' from file '+str(self.lastimport)
                     Id = self.PatternTree.AppendItem(parent=self.root,text=HistName)
@@ -731,6 +734,7 @@ class GSASII(wx.Frame):
                     self.PatternTree.SetItemPyData(
                         self.PatternTree.AppendItem(Id,text='Reflection List'),[])  #dummy entry for GUI use
             else:
+                valuesdict = {'wtFactor':1.0,'Dummy':False,'ranId':ran.randint(0,sys.maxint),}
                 HistName = G2obj.MakeUniqueLabel(HistName,HKLFlist)
                 print 'Read structure factor table '+str(HistName)+' from file '+str(self.lastimport)
                 if not rd.RefDict.get('FF'):
@@ -773,20 +777,11 @@ class GSASII(wx.Frame):
             generalData = data['General']
             SGData = generalData['SGData']
             UseList = data['Histograms']
-            NShkl = len(G2spc.MustrainNames(SGData))
-            NDij = len(G2spc.HStrainNames(SGData))
             for histoName in newHistList:
-                UseList[histoName] = {
-                    'Histogram':histoName,'Show':False,'Scale':[1.0,True],
-                    'Babinet':{'BabA':[0.0,False],'BabU':[0.0,False]},
-                    'Extinction':['Lorentzian','None', {'Tbar':0.1,'Cos2TM':0.955,
-                                                        'Eg':[1.e-10,False],
-                                                        'Es':[1.e-10,False],
-                                                        'Ep':[1.e-10,False]},]
-                    }
                 #redo UpdateHKLFdata(histoName) here:
                 Id = G2gd.GetPatternTreeItemId(self,self.root,histoName)
                 refDict,reflData = self.PatternTree.GetItemPyData(Id)
+                UseList[histoName] = SetDefaultDData(refDict['Type'],histoName)
                 G,g = G2lat.cell2Gmat(generalData['Cell'][1:7])
                 for iref,ref in enumerate(reflData['RefList']):
                     H = list(ref[:3])
@@ -1325,15 +1320,7 @@ class GSASII(wx.Frame):
             NShkl = len(G2spc.MustrainNames(SGData))
             NDij = len(G2spc.HStrainNames(SGData))
             for histoName in newHistList:
-                UseList[histoName] = {
-                    'Histogram':histoName,'Show':False,
-                    'Scale':[1.0,False],'Pref.Ori.':['MD',1.0,False,[0,0,1],0,{}],
-                    'Size':['isotropic',[1.,1.,1.],[False,False,False],[0,0,1],
-                            [1.,1.,1.,0.,0.,0.],6*[False,]],
-                    'Mustrain':['isotropic',[1000.0,1000.0,1.0],[False,False,False],[0,0,1],
-                                NShkl*[0.01,],NShkl*[False,]],
-                    'HStrain':[NDij*[0.0,],NDij*[False,]],                          
-                    'Extinction':[0.0,False],'Babinet':{'BabA':[0.0,False],'BabU':[0.0,False]}}
+                UseList[histoName] = SetDefaultDData('PWDR',histoName,NShkl=NShkl,NDij=NDij)
                 Id = G2gd.GetPatternTreeItemId(self,self.root,histoName)
                 refList = self.PatternTree.GetItemPyData(
                     G2gd.GetPatternTreeItemId(self,Id,'Reflection Lists'))
@@ -1495,15 +1482,7 @@ class GSASII(wx.Frame):
             UseList = data['Histograms']
             NShkl = len(G2spc.MustrainNames(SGData))
             NDij = len(G2spc.HStrainNames(SGData))
-            UseList[HistName] = {
-                'Histogram':HistName,'Show':False,
-                'Scale':[1.0,False],'Pref.Ori.':['MD',1.0,False,[0,0,1],0,{}],
-                'Size':['isotropic',[1.,1.,1.],[False,False,False],[0,0,1],
-                        [1.,1.,1.,0.,0.,0.],6*[False,]],
-                'Mustrain':['isotropic',[1000.0,1000.0,1.0],[False,False,False],[0,0,1],
-                            NShkl*[0.01,],NShkl*[False,]],
-                'HStrain':[NDij*[0.0,],NDij*[False,]],                          
-                'Extinction':[0.0,False],'Babinet':{'BabA':[0.0,False],'BabU':[0.0,False]}}
+            UseList[HistName] = SetDefaultDData('PWDR',histoName,NShkl=NShkl,NDij=NDij)
             Id = G2gd.GetPatternTreeItemId(self,self.root,HistName)
             refList = self.PatternTree.GetItemPyData(
                 G2gd.GetPatternTreeItemId(self,Id,'Reflection Lists'))

GSASIIElem.py
@@ -325,24 +325,24 @@ def BlenResCW(Els,BLtables,wave):
             FPP[i] = BL['SL'][1]    #for Li, B, etc.
     return FP,FPP
     
-def BlenResTOF(El,BLtables,wave):
-    FP = np.zeros(len(wave))
-    FPP = np.zeros(len(wave))
-    BL = BLtables[El][1]
-    print BL
-    if 'BW-LS' in BL:
-        Re,Im,E0,gam,A,E1,B,E2 = BL['BW-LS'][1:]
-        Emev = 81.80703/wave**2
-        T0 = Emev-E0
-        T1 = Emev-E1
-        T2 = Emev-E2
-        D0 = T0**2+gam**2
-        D1 = T1**2+gam**2
-        D2 = T2**2+gam**2
-        FP = Re*(T0/D0+A*T1/D1+B*T2/D2)
-        FPP = Im*(1/D0+A/D1+B/D2)
-    else:
-        FPP = np.ones(len(wave))*BL['SL'][1]    #for Li, B, etc.
+def BlenResTOF(Els,BLtables,wave):
+    FP = np.zeros((len(Els),len(wave)))
+    FPP = np.zeros((len(Els),len(wave)))
+    BL = [BLtables[el][1] for el in Els]
+    for i,El in enumerate(Els):
+        if 'BW-LS' in BL[i]:
+            Re,Im,E0,gam,A,E1,B,E2 = BL[i]['BW-LS'][1:]
+            Emev = 81.80703/wave**2
+            T0 = Emev-E0
+            T1 = Emev-E1
+            T2 = Emev-E2
+            D0 = T0**2+gam**2
+            D1 = T1**2+gam**2
+            D2 = T2**2+gam**2
+            FP[i] = Re*(T0/D0+A*T1/D1+B*T2/D2)
+            FPP[i] = Im*(1/D0+A/D1+B/D2)
+        else:
+            FPP[i] = np.ones(len(wave))*BL[i]['SL'][1]    #for Li, B, etc.
     return FP,FPP
     
 def ComptonFac(El,SQ):

GSASIImath.py
@@ -151,18 +151,18 @@ def HessianLSQ(func,x0,Hess,args=(),ftol=1.49012e-8,xtol=1.49012e-8, maxcyc=0,Pr
             if Print: print "converged"
             break
         icycle += 1
-    else:       #after last cycle or if zero cycles
-        M = func(x0,*args)
-        nfev += 1
-        Yvec,Amat = Hess(x0,*args)
-        Adiag = np.sqrt(np.diag(Amat))
-        Anorm = np.outer(Adiag,Adiag)
-        Amatlam = Amat*(One+Lam)/Anorm              #scale Amat to Marquardt array        
+    M = func(x0,*args)
+    nfev += 1
+    Yvec,Amat = Hess(x0,*args)
+    Adiag = np.sqrt(np.diag(Amat))
+    Anorm = np.outer(Adiag,Adiag)
+    Lam = np.eye(Amat.shape[0])*lam
+    Amatlam = Amat/Anorm  #*(One+Lam)              #don't scale Amat to Marquardt array        
     try:
-        Bmat = nl.inv(Amatlam)*(One+Lam)/Anorm      #rescale Bmat to Marquardt array
+        Bmat = nl.inv(Amatlam)/Anorm  #*(One+Lam)      #don't rescale Bmat to Marquardt array
         return [x0,Bmat,{'num cyc':icycle,'fvec':M,'nfev':nfev,'lamMax':lamMax,'psing':[], 'Converged': ifConverged, 'DelChi2':deltaChi2}]
     except nl.LinAlgError:
-        print 'ouch #2 linear algebra error in LS'
+        print 'ouch #2 linear algebra error in making v-cov matrix'
         psing = []
         if maxcyc:
             psing = list(np.where(np.diag(nl.qr(Amat)[1]) < 1.e-14)[0])

GSASIIstrMath.py
@@ -556,7 +556,7 @@ def StructureFactor(refDict,G,hfx,pfx,SGData,calcControls,parmDict):
     BLtables = calcControls['BLtables']
     Tdata,Mdata,Fdata,Xdata,dXdata,IAdata,Uisodata,Uijdata = GetAtomFXU(pfx,calcControls,parmDict)
     FF = np.zeros(len(Tdata))
-    if 'N' in calcControls[hfx+'histType']:
+    if 'NC' in calcControls[hfx+'histType']:
         FP,FPP = G2el.BlenResCW(Tdata,BLtables,parmDict[hfx+'Lam'])
     else:
         FP = np.array([FFtables[El][hfx+'FP'] for El in Tdata])
@@ -571,6 +571,8 @@ def StructureFactor(refDict,G,hfx,pfx,SGData,calcControls,parmDict):
         refDict['FF']['El'] = dat.keys()
         refDict['FF']['FF'] = np.zeros((len(refDict['RefList']),len(dat)))   
     for iref,refl in enumerate(refDict['RefList']):
+        if 'NT' in calcControls[hfx+'histType']:
+            FP,FPP = G2el.BlenResCW(Tdata,BLtables,refl[12])
         fbs = np.array([0,0])
         H = refl[:3]
         SQ = 1./(2.*refl[4])**2
@@ -631,9 +633,9 @@ def StructureFactor2(refDict,G,hfx,pfx,SGData,calcControls,parmDict):
     BLtables = calcControls['BLtables']
     Tdata,Mdata,Fdata,Xdata,dXdata,IAdata,Uisodata,Uijdata = GetAtomFXU(pfx,calcControls,parmDict)
     FF = np.zeros(len(Tdata))
-    if 'N' in calcControls[hfx+'histType']:
+    if 'NC' in calcControls[hfx+'histType']:
         FP,FPP = G2el.BlenResCW(Tdata,BLtables,parmDict[hfx+'Lam'])
-    else:
+    elif 'X' in calcControls[hfx+'histType']:
         FP = np.array([FFtables[El][hfx+'FP'] for El in Tdata])
         FPP = np.array([FFtables[El][hfx+'FPP'] for El in Tdata])
     Uij = np.array(G2lat.U6toUij(Uijdata))
@@ -661,6 +663,10 @@ def StructureFactor2(refDict,G,hfx,pfx,SGData,calcControls,parmDict):
         H = refl.T[:3]
         SQ = 1./(2.*refl.T[4])**2
         SQfactor = 4.0*SQ*twopisq
+        if 'T' in calcControls[hfx+'histType']:
+            FP,FPP = G2el.BlenResTOF(Tdata,BLtables,refl.T[12])
+            FP = np.repeat(FP.T,len(SGT),axis=0)
+            FPP = np.repeat(FPP.T,len(SGT),axis=0)
         Bab = np.repeat(parmDict[phfx+'BabA']*np.exp(-parmDict[phfx+'BabU']*SQfactor),len(SGT))
         Tindx = np.array([refDict['FF']['El'].index(El) for El in Tdata])
         FF = np.repeat(refDict['FF']['FF'][iBeg:iFin].T[Tindx].T,len(SGT),axis=0)
@@ -795,27 +801,34 @@ def SCExtinction(ref,phfx,hfx,pfx,calcControls,parmDict,varyList):
     dervCor = 1.0
     dervDict = {}
     if calcControls[phfx+'EType'] != 'None':
-        cos2T = 1.0-0.5*(parmDict[hfx+'Lam']/ref[4])**2         #cos(2theta)
+        SQ = 1/(4.*ref[4]**2)
+        if 'C' in parmDict[hfx+'Type']:            
+            cos2T = 1.0-2.*SQ*parmDict[hfx+'Lam']**2           #cos(2theta)
+        else:   #'T'
+            cos2T = 1.0-2.*SQ*ref[12]**2                       #cos(2theta)            
         if 'SXC' in parmDict[hfx+'Type']:
             AV = 7.9406e5/parmDict[pfx+'Vol']**2
             PL = np.sqrt(1.0-cos2T**2)/parmDict[hfx+'Lam']
             P12 = (calcControls[phfx+'Cos2TM']+cos2T**4)/(calcControls[phfx+'Cos2TM']+cos2T**2)
+            PLZ = AV*P12*ref[7]*parmDict[hfx+'Lam']**2
         elif 'SNT' in parmDict[hfx+'Type']:
             AV = 1.e7/parmDict[pfx+'Vol']**2
-            PL = 1./(4.*refl[4]**2)
-            P12 = 1.0
+            PL = SQ
+            PLZ = AV*ref[7]*ref[12]**2
         elif 'SNC' in parmDict[hfx+'Type']:
             AV = 1.e7/parmDict[pfx+'Vol']**2
             PL = np.sqrt(1.0-cos2T**2)/parmDict[hfx+'Lam']
-            P12 = 1.0
+            PLZ = AV*ref[7]*parmDict[hfx+'Lam']**2
             
-        PLZ = AV*P12*parmDict[hfx+'Lam']**2*ref[7]
         if 'Primary' in calcControls[phfx+'EType']:
             PLZ *= 1.5
         else:
-            PLZ *= calcControls[phfx+'Tbar']
-                        
+            if 'C' in parmDict[hfx+'Type']:
+                PLZ *= calcControls[phfx+'Tbar']
+            else: #'T'
+                PLZ *= ref[13]
         if 'Primary' in calcControls[phfx+'EType']:
+            PLZ *= 1.5
             PSIG = parmDict[phfx+'Ep']
         elif 'I & II' in calcControls[phfx+'EType']:
             PSIG = parmDict[phfx+'Eg']/np.sqrt(1.+(parmDict[phfx+'Es']*PL/parmDict[phfx+'Eg'])**2)
@@ -843,7 +856,7 @@ def SCExtinction(ref,phfx,hfx,pfx,calcControls,parmDict,varyList):
             extCor = np.sqrt(PF4)
             PF3 = 0.5*(CL+2.*AL*PF/(1.+BL*PF)-AL*PF**2*BL/(1.+BL*PF)**2)/(PF4*extCor)
 
-        dervCor = (1.+PF)*PF3
+        dervCor = (1.+PF)*PF3   #extinction corr for other derivatives
         if 'Primary' in calcControls[phfx+'EType'] and phfx+'Ep' in varyList:
             dervDict[phfx+'Ep'] = -ref[7]*PLZ*PF3
         if 'II' in calcControls[phfx+'EType'] and phfx+'Es' in varyList:
@@ -1892,7 +1905,7 @@ def HessRefine(values,HistoPhases,parmDict,varylist,calcControls,pawleyLookup,dl
             refDict = Histogram['Data']
             time0 = time.time()
             dFdvDict = StructureFactorDerv(refDict,G,hfx,pfx,SGData,calcControls,parmDict)
-            print 'sf-deriv time: %.3f'%(time.time()-time0)
+#            print 'sf-deriv time: %.3f'%(time.time()-time0)
             ApplyRBModelDervs(dFdvDict,parmDict,rigidbodyDict,Phase)
             dMdvh = np.zeros((len(varylist),len(refDict['RefList'])))
             dependentVars = G2mv.GetDependentVars()
@@ -1915,9 +1928,9 @@ def HessRefine(values,HistoPhases,parmDict,varylist,calcControls,pawleyLookup,dl
                                 if var in dFdvDict:
                                     depDerivDict[var][iref] = w*dFdvDict[var][iref]*dervCor*parmDict[phfx+'Scale']
                             if phfx+'Scale' in varylist:
-                                dMdvh[varylist.index(phfx+'Scale')][iref] = w*ref[9]*dervCor
+                                dMdvh[varylist.index(phfx+'Scale')][iref] = w*ref[9]    #*dervCor
                             elif phfx+'Scale' in dependentVars:
-                                depDerivDict[phfx+'Scale'][iref] = w*ref[9]*dervCor
+                                depDerivDict[phfx+'Scale'][iref] = w*ref[9] #*dervCor
                             for item in ['Ep','Es','Eg']:
                                 if phfx+item in varylist:
                                     dMdvh[varylist.index(phfx+item)][iref] = w*dervDict[phfx+item]*parmDict[phfx+'Scale']
@@ -1925,9 +1938,9 @@ def HessRefine(values,HistoPhases,parmDict,varylist,calcControls,pawleyLookup,dl
                                     depDerivDict[phfx+item][iref] = w*dervDict[phfx+item]*parmDict[phfx+'Scale']
                             for item in ['BabA','BabU']:
                                 if phfx+item in varylist:
-                                    dMdvh[varylist.index(phfx+item)][iref] = w*dFdvDict[pfx+item][iref]*parmDict[phfx+'Scale']*dervCor
+                                    dMdvh[varylist.index(phfx+item)][iref] = w*dFdvDict[pfx+item][iref]*parmDict[phfx+'Scale']  #*dervCor
                                 elif phfx+item in dependentVars:
-                                    depDerivDict[phfx+item][iref] = w*dFdvDict[pfx+item][iref]*parmDict[phfx+'Scale']*dervCor
+                                    depDerivDict[phfx+item][iref] = w*dFdvDict[pfx+item][iref]*parmDict[phfx+'Scale']   #*dervCor
             else:
                 for iref,ref in enumerate(refDict['RefList']):
                     if ref[5] > 0.:
@@ -1948,18 +1961,18 @@ def HessRefine(values,HistoPhases,parmDict,varylist,calcControls,pawleyLookup,dl
                             elif phfx+'Scale' in dependentVars:
                                 depDerivDict[phfx+'Scale'][iref] = w*ref[9]*dervCor                           
                             for item in ['Ep','Es','Eg']:
-                                if phfx+item in varylist:
-                                    dMdvh[varylist.index(phfx+item)][iref] = w*dervDict[phfx+item]*parmDict[phfx+'Scale']
-                                elif phfx+item in dependentVars:
-                                    depDerivDict[phfx+item][iref] = w*dervDict[phfx+item]*parmDict[phfx+'Scale']
+                                if phfx+item in varylist and dervDict:
+                                   dMdvh[varylist.index(phfx+item)][iref] = w*dervDict[phfx+item]*parmDict[phfx+'Scale']/dervCor
+                                elif phfx+item in dependentVars and dervDict:
+                                    depDerivDict[phfx+item][iref] = w*dervDict[phfx+item]*parmDict[phfx+'Scale']/dervCor
                             for item in ['BabA','BabU']:
                                 if phfx+item in varylist:
-                                    dMdvh[varylist.index(phfx+item)][iref] = w*dFdvDict[pfx+item][iref]*parmDict[phfx+'Scale']*dervCor
+                                    dMdvh[varylist.index(phfx+item)][iref] = w*dFdvDict[pfx+item][iref]*parmDict[phfx+'Scale']  #*dervCor
                                 elif phfx+item in dependentVars:
-                                    depDerivDict[phfx+item][iref] = w*dFdvDict[pfx+item][iref]*parmDict[phfx+'Scale']*dervCor
+                                    depDerivDict[phfx+item][iref] = w*dFdvDict[pfx+item][iref]*parmDict[phfx+'Scale']   #*dervCor
             # now process derivatives in constraints
             G2mv.Dict2Deriv(varylist,depDerivDict,dMdvh)
-            print 'matrix build time: %.3f'%(time.time()-time0)
+#            print 'matrix build time: %.3f'%(time.time()-time0)
 
             if dlg:
                 dlg.Update(Histogram['Residuals']['wR'],newmsg='Hessian for histogram %d Rw=%8.3f%s'%(hId,Histogram['Residuals']['wR'],'%'))[0]
@@ -2048,7 +2061,7 @@ def errRefine(values,HistoPhases,parmDict,varylist,calcControls,pawleyLookup,dlg
             refDict = Histogram['Data']
             time0 = time.time()
             StructureFactor2(refDict,G,hfx,pfx,SGData,calcControls,parmDict)
-            print 'sf-calc time: %.3f'%(time.time()-time0)
+#            print 'sf-calc time: %.3f'%(time.time()-time0)
             df = np.zeros(len(refDict['RefList']))
             sumwYo = 0
             sumFo = 0

imports/G2sfact.py
@@ -74,6 +74,7 @@ class HKLF_ReaderClass(G2IO.ImportStructFactor):
                 #self.RefDict['FF'].append({}) # now done in OnImportSfact
             self.errors = 'Error after reading reflections (unexpected!)'
             self.RefDict['RefList'] = np.array(self.RefDict['RefList'])
+            self.RefDict['Type'] = 'SXC'
             self.UpdateParameters(Type='SXC',Wave=None) # histogram type
             return True
         except Exception as detail:
@@ -114,6 +115,7 @@ class HKLF2_ReaderClass(G2IO.ImportStructFactor):
                 #self.RefDict['FF'].append({}) # now done in OnImportSfact
             self.errors = 'Error after reading reflections (unexpected!)'
             self.RefDict['RefList'] = np.array(self.RefDict['RefList'])
+            self.RefDict['Type'] = 'SXC'
             self.UpdateParameters(Type='SXC',Wave=None) # histogram type
             return True
         except Exception as detail:
@@ -173,9 +175,11 @@ class NT_HKLF2_ReaderClass(G2IO.ImportStructFactor):
             if len(self.Banks):
                 self.UpdateParameters(Type='SNT',Wave=None) # histogram type
                 for Bank in self.Banks:
-                    Bank['RefDict']['RefList'] = np.array(Bank['RefDict']['RefList'])                    
+                    Bank['RefDict']['RefList'] = np.array(Bank['RefDict']['RefList'])
+                    Bank['RefDict']['Type'] = 'SNT'                    
             else:
                 self.RefDict['RefList'] = np.array(self.RefDict['RefList'])
+                self.RefDict['Type'] = 'SNT'
                 self.errors = 'Error after reading reflections (unexpected!)'
                 self.UpdateParameters(Type='SNT',Wave=None) # histogram type
             return True
@@ -234,9 +238,11 @@ class NT_JANA2K_ReaderClass(G2IO.ImportStructFactor):
             if len(self.Banks):
                 self.UpdateParameters(Type='SNT',Wave=None) # histogram type
                 for Bank in self.Banks:
-                    Bank['RefDict']['RefList'] = np.array(Bank['RefDict']['RefList'])                    
+                    Bank['RefDict']['RefList'] = np.array(Bank['RefDict']['RefList'])
+                    Bank['RefDict']['Type'] = 'SNT'                    
             else:
                 self.RefDict['RefList'] = np.array(self.RefDict['RefList'])
+                self.RefDict['Type'] = 'SNT'
                 self.errors = 'Error after reading reflections (unexpected!)'
                 self.UpdateParameters(Type='SNT',Wave=None) # histogram type
             return True
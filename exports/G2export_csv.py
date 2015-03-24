b445d6f3609d484e2bb31da69dae55a9a4b41fc9
Robert Von Dreele <vondreele@anl.gov>
2015-03-24 14:05:08 +0000

    fix csv exporter for reflection lists
    git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@1760 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

--- exports/G2export_csv.py
+++ exports/G2export_csv.py
@@ -243,13 +243,24 @@ class ExportPowderReflCSV(G2IO.ExportBaseclass):
             self.Write('"'+str(phasenam)+'",'+str(i))
         self.Write('')
         # note addition of a phase # flag at end (i)
-        WriteList(self,("h","k","l","2-theta","F_obs","F_calc","phase","mult","phase #"))
-        fmt = "{:.0f},{:.0f},{:.0f},{:.3f},{:.3f},{:.3f},{:.2f},{:.0f},{:d}"
         for i,phasenam in enumerate(sorted(histblk['Reflection Lists'])):
-            for (
-                h,k,l,mult,dsp,pos,sig,gam,Fobs,Fcalc,phase,Icorr
-                ) in histblk['Reflection Lists'][phasenam]['RefList']:
-                self.Write(fmt.format(h,k,l,pos,Fobs,Fcalc,phase,mult,i))
+            phasDict = histblk['Reflection Lists'][phasenam]
+            if phasDict['Super']:
+                WriteList(self,("h","k","l","m","2-theta","F_obs","F_calc","phase","mult","phase #"))
+                fmt = "{:.0f},{:.0f},{:.0f},{:.0f},{:.3f},{:.3f},{:.3f},{:.2f},{:.0f},{:d}"
+                refList = phasDict['RefList']
+                for (
+                    h,k,l,m,mult,dsp,pos,sig,gam,Fobs,Fcalc,phase,Icorr,x,x,x
+                    ) in refList:
+                    self.Write(fmt.format(h,k,l,m,pos,Fobs,Fcalc,phase,mult,i))                
+            else:
+                WriteList(self,("h","k","l","2-theta","F_obs","F_calc","phase","mult","phase #"))
+                fmt = "{:.0f},{:.0f},{:.0f},{:.3f},{:.3f},{:.3f},{:.2f},{:.0f},{:d}"
+                refList = phasDict['RefList']
+                for (
+                    h,k,l,mult,dsp,pos,sig,gam,Fobs,Fcalc,phase,Icorr,x,x,x
+                    ) in refList:
+                    self.Write(fmt.format(h,k,l,pos,Fobs,Fcalc,phase,mult,i))
         self.CloseFile()
         print(str(hist)+'reflections written to file '+str(self.fullpath))
 

show no. space group extinct reflections in HKLF data - rejected from calcs. calculate a "residual" for texture fitting & show it in ProgressBar correct angle derivatives in texture fitting (wrong sign) trap empty useList in plots make sure U23 is printed in lst output
git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@1800 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

GSASIImath.py
@@ -1593,7 +1593,7 @@ def ValEsd(value,esd=0,nTZ=False):
 ##### Texture fitting stuff
 ################################################################################
 
-def FitTexture(General,Gangls,refData,keyList):
+def FitTexture(General,Gangls,refData,keyList,pgbar):
     import pytexture as ptx
     ptx.pyqlmninit()            #initialize fortran arrays for spherical harmonics
     
@@ -1648,12 +1648,14 @@ def FitTexture(General,Gangls,refData,keyList):
         values corresponding to keys in varylist'''
         parmdict.update(zip(varylist,values))
         
-    def errSpHarm(values,SGData,cell,Gangls,shModel,refData,parmDict,varyList):
+    def errSpHarm(values,SGData,cell,Gangls,shModel,refData,parmDict,varyList,pgbar):
         parmDict.update(zip(varyList,values))
         Mat = np.empty(0)
+        sumObs = 0
         Sangls = [parmDict['Sample '+'omega'],parmDict['Sample '+'chi'],parmDict['Sample '+'phi']]
         for hist in Gangls.keys():
             Refs = refData[hist]
+            sumObs += np.sum(Refs[:,5])
             Refs[:,6] = 1.
             H = Refs[:,:3]
             phi,beta = G2lat.CrsAng(H,cell,SGData)
@@ -1667,10 +1669,13 @@ def FitTexture(General,Gangls,refData,keyList):
                     Refs[:,6] += parmDict[item]*Lnorm*Kcl*Ksl
             mat = Refs[:,5]-Refs[:,6]
             Mat = np.concatenate((Mat,mat))
-        print ' Chi**2: %.3f'%(np.sum(np.abs(Mat)))
+        sumD = np.sum(np.abs(Mat))
+        R = min(100.,100.*sumD/sumObs)
+        pgbar.Update(R,newmsg='Residual = %5.2f'%(R))
+        print ' Residual: %.3f%%'%(R)
         return Mat
         
-    def dervSpHarm(values,SGData,cell,Gangls,shModel,refData,parmDict,varyList):
+    def dervSpHarm(values,SGData,cell,Gangls,shModel,refData,parmDict,varyList,pgbar):
         Mat = np.empty(0)
         Sangls = [parmDict['Sample omega'],parmDict['Sample chi'],parmDict['Sample phi']]
         for hist in Gangls.keys():
@@ -1689,7 +1694,7 @@ def FitTexture(General,Gangls,refData,keyList):
                     for k,itema in enumerate(['Sample omega','Sample chi','Sample phi']):
                         try:
                             l = varyList.index(itema)
-                            mat[l] += parmDict[item]*Lnorm*Kcl*(dKdp*dPdA[k]+dKdg*dGdA[k])
+                            mat[l] -= parmDict[item]*Lnorm*Kcl*(dKdp*dPdA[k]+dKdg*dGdA[k])
                         except ValueError:
                             pass
             if len(Mat):
@@ -1717,7 +1722,7 @@ def FitTexture(General,Gangls,refData,keyList):
         begin = time.time()
         values =  np.array(Dict2Values(parmDict, varyList))
         result = so.leastsq(errSpHarm,values,Dfun=dervSpHarm,full_output=True,
-            args=(SGData,cell,Gangls,Texture['Model'],refData,parmDict,varyList))
+            args=(SGData,cell,Gangls,Texture['Model'],refData,parmDict,varyList,pgbar))
         ncyc = int(result[2]['nfev']/2)
         if ncyc:
             runtime = time.time()-begin    
@@ -1726,7 +1731,6 @@ def FitTexture(General,Gangls,refData,keyList):
             GOF = chisq/(len(result[2]['fvec'])-len(varyList))       #reduced chi^2
             print 'Number of function calls:',result[2]['nfev'],' Number of observations: ',len(result[2]['fvec']),' Number of parameters: ',len(varyList)
             print 'refinement time = %8.3fs, %8.3fs/cycle'%(runtime,runtime/ncyc)
-            print 'chi**2 = %12.6g, reduced chi**2 = %6.2f'%(chisq,GOF)
             try:
                 sig = np.sqrt(np.diag(result[1])*GOF)
                 if np.any(np.isnan(sig)):

GSASIIphsGUI.py
@@ -710,7 +710,7 @@ def UpdatePhaseData(G2frame,Item,data,oldPage):
                 
             def OnRefList(event):
                 dlg = G2G.G2MultiChoiceDialog(G2frame, 'Select reflection sets to use',
-                    'Use data',refsList,filterBox=False)
+                    'Use data',refsList)
                 try:
                     if dlg.ShowModal() == wx.ID_OK:
                         Map['RefList'] = [refsList[i] for i in dlg.GetSelections()]
@@ -781,7 +781,7 @@ def UpdatePhaseData(G2frame,Item,data,oldPage):
             
             def OnRefList(event):
                 dlg = G2G.G2MultiChoiceDialog(G2frame, 'Select reflection sets to use',
-                    'Use data',refsList,filterBox=False)
+                    'Use data',refsList)
                 try:
                     if dlg.ShowModal() == wx.ID_OK:
                         Flip['RefList'] = [refsList[i] for i in dlg.GetSelections()]
@@ -2227,7 +2227,7 @@ def UpdatePhaseData(G2frame,Item,data,oldPage):
 
             def OnRefList(event):
                 dlg = G2G.G2MultiChoiceDialog(G2frame, 'Select reflection sets to use',
-                    'Use data',refsList,filterBox=False)
+                    'Use data',refsList)
                 try:
                     if dlg.ShowModal() == wx.ID_OK:
                         Map['RefList'] = [refsList[i] for i in dlg.GetSelections()]
@@ -3751,7 +3751,8 @@ def UpdatePhaseData(G2frame,Item,data,oldPage):
             if name not in keyList and 'HKLF' in name:
                 TextList.append(name)
             item, cookie = G2frame.PatternTree.GetNextChild(G2frame.root, cookie)                        
-        dlg = wx.MultiChoiceDialog(G2frame, 'Which new data to use?', 'Use data', TextList, wx.CHOICEDLG_STYLE)
+        dlg = G2G.G2MultiChoiceDialog(G2frame, 'Select reflection sets to use',
+                    'Use data',TextList)
         try:
             if dlg.ShowModal() == wx.ID_OK:
                 result = dlg.GetSelections()
@@ -3935,7 +3936,7 @@ def UpdatePhaseData(G2frame,Item,data,oldPage):
         NShkl = len(G2spc.MustrainNames(SGData))
         NDij = len(G2spc.HStrainNames(SGData))
         keyList = UseList.keys()
-        TextList = ['All PWDR']
+        TextList = []
         if G2frame.PatternTree.GetCount():
             item, cookie = G2frame.PatternTree.GetFirstChild(G2frame.root)
             while item:
@@ -3943,7 +3944,8 @@ def UpdatePhaseData(G2frame,Item,data,oldPage):
                 if name not in keyList and 'PWDR' in name:
                     TextList.append(name)
                 item, cookie = G2frame.PatternTree.GetNextChild(G2frame.root, cookie)
-            dlg = wx.MultiChoiceDialog(G2frame, 'Which new data to use?', 'Use data', TextList, wx.CHOICEDLG_STYLE)
+            dlg = G2G.G2MultiChoiceDialog(G2frame, 'Select reflection sets to use',
+                    'Use data',TextList)
             try:
                 if dlg.ShowModal() == wx.ID_OK:
                     result = dlg.GetSelections()
@@ -5922,7 +5924,10 @@ def UpdatePhaseData(G2frame,Item,data,oldPage):
                     refData[name] = np.column_stack((Refs[0],Refs[1],Refs[2],tth,Refs[8+im],Refs[12+im+it],np.zeros_like(Refs[0])))
                 else:   # xray - typical caked 2D image data
                     refData[name] = np.column_stack((Refs[0],Refs[1],Refs[2],Refs[5+im],Refs[8+im],Refs[12+im+it],np.zeros_like(Refs[0])))
-        Error = G2mth.FitTexture(General,Gangls,refData,keyList)
+        pgbar = wx.ProgressDialog('Texture fit','Residual = %5.2f'%(101.0),101.0, 
+            style = wx.PD_ELAPSED_TIME|wx.PD_AUTO_HIDE)
+        Error = G2mth.FitTexture(General,Gangls,refData,keyList,pgbar)
+        pgbar.Destroy()
         if Error:
             wx.MessageBox(Error,caption='Fit Texture Error',style=wx.ICON_EXCLAMATION)
         UpdateTexture()
@@ -5976,7 +5981,7 @@ def UpdatePhaseData(G2frame,Item,data,oldPage):
         elif text == 'Data':
             G2gd.SetDataMenuBar(G2frame,G2frame.dataFrame.DataMenu)
             G2ddG.UpdateDData(G2frame,DData,data)
-            G2plt.PlotSizeStrainPO(G2frame,data,hist='',Start=True)            
+            wx.CallAfter(G2plt.PlotSizeStrainPO,G2frame,data,hist='',Start=True)            
         elif text == 'Atoms':
             G2gd.SetDataMenuBar(G2frame,G2frame.dataFrame.AtomsMenu)
             FillAtomsGrid(Atoms)
@@ -6006,12 +6011,13 @@ def UpdatePhaseData(G2frame,Item,data,oldPage):
         elif text == 'Texture':
             G2gd.SetDataMenuBar(G2frame,G2frame.dataFrame.TextureMenu)
             UpdateTexture()                        
-            G2plt.PlotTexture(G2frame,data,Start=True)            
+            wx.CallAfter(G2plt.PlotTexture,G2frame,data,Start=True)            
         elif text == 'Pawley reflections':
             G2gd.SetDataMenuBar(G2frame,G2frame.dataFrame.PawleyMenu)
             FillPawleyReflectionsGrid()
         else:
             G2gd.SetDataMenuBar(G2frame)
+            
     def FillMenus():
         '''Create the Select tab menus and bind to all menu items
         '''

GSASIIstrMath.py
@@ -2640,6 +2640,7 @@ def errRefine(values,HistoPhases,parmDict,varylist,calcControls,pawleyLookup,dlg
     SumwYo = 0
     Nobs = 0
     Nrej = 0
+    Next = 0
     ApplyRBModels(parmDict,Phases,rigidbodyDict)
     histoList = Histograms.keys()
     histoList.sort()
@@ -2719,6 +2720,7 @@ def errRefine(values,HistoPhases,parmDict,varylist,calcControls,pawleyLookup,dlg
             sumdF2 = 0
             nobs = 0
             nrej = 0
+            next = 0
             if calcControls['F**2']:
                 for i,ref in enumerate(refDict['RefList']):
                     if ref[6+im] > 0:
@@ -2737,8 +2739,11 @@ def errRefine(values,HistoPhases,parmDict,varylist,calcControls,pawleyLookup,dlg
                             df[i] = -w*(ref[5+im]-ref[7+im])
                             sumwYo += (w*ref[5+im])**2      #w*Fo^2
                         else:
-                            ref[3+im] = -abs(ref[3+im])      #mark as rejected
-                            nrej += 1
+                            if ref[3+im]:
+                                ref[3+im] = -abs(ref[3+im])      #mark as rejected
+                                nrej += 1
+                            else:   #sp.gp.extinct
+                                next += 1
             else:
                 for i,ref in enumerate(refDict['RefList']):
                     if ref[5+im] > 0.:
@@ -2758,8 +2763,11 @@ def errRefine(values,HistoPhases,parmDict,varylist,calcControls,pawleyLookup,dlg
                             df[i] = -w*(Fo-Fc)
                             sumwYo += (w*Fo)**2
                         else:
-                            ref[3+im] = -abs(ref[3+im])      #mark as rejected
-                            nrej += 1
+                            if ref[3+im]:
+                                ref[3+im] = -abs(ref[3+im])      #mark as rejected
+                                nrej += 1
+                            else:   #sp.gp.extinct
+                                next += 1
             Histogram['Residuals']['Nobs'] = nobs
             Histogram['Residuals']['sumwYo'] = sumwYo
             SumwYo += sumwYo
@@ -2768,8 +2776,10 @@ def errRefine(values,HistoPhases,parmDict,varylist,calcControls,pawleyLookup,dlg
             Histogram['Residuals'][phfx+'Rf^2'] = 100.*sumdF2/sumFo2
             Histogram['Residuals'][phfx+'Nref'] = nobs
             Histogram['Residuals'][phfx+'Nrej'] = nrej
+            Histogram['Residuals'][phfx+'Next'] = next
             Nobs += nobs
             Nrej += nrej
+            Next += next
             if dlg:
                 dlg.Update(Histogram['Residuals']['wR'],newmsg='For histogram %d Rw=%8.3f%s'%(hId,Histogram['Residuals']['wR'],'%'))[0]
             M = np.concatenate((M,wtFactor*df))
@@ -2777,6 +2787,7 @@ def errRefine(values,HistoPhases,parmDict,varylist,calcControls,pawleyLookup,dlg
     Histograms['sumwYo'] = SumwYo
     Histograms['Nobs'] = Nobs
     Histograms['Nrej'] = Nrej
+    Histograms['Next'] = Next
     Rw = min(100.,np.sqrt(np.sum(M**2)/SumwYo)*100.)
     if dlg:
         GoOn = dlg.Update(Rw,newmsg='%s%8.3f%s'%('All data Rw =',Rw,'%'))[0]
b1517a326e3d168e9b584001e2d002acdd032091
Brian Toby <toby@anl.gov>
2015-10-29 03:04:15 +0000

    try RTFD build again
    git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@2029 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,6 +11,16 @@
 # serve to show the default.
 
 import sys, os
+# dummy packages for misc imports not on readthedocs
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['wx', 'numpy', 'scipy', 'pypowder', 'pyspg', 'pytexture']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the

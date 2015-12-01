explicit plot destroy when GSAS2 closes work on scrolling on phase data tabs - scroll back to place last used fix(?) Block/ZigZag derivatives eliminate Refine & max index buttons for mod vector for single crystal data
git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@2075 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

GSASIImath.py
@@ -935,7 +935,8 @@ def makeWaves(waveTypes,FSSdata,XSSdata,USSdata,Mast):
     USSdata: array 2x6 x atoms X waves (sin,cos terms)
     Mast: array orthogonalization matrix for Uij
     '''
-    glTau,glWt = pwd.pygauleg(0.,1.,32)         #get Gauss-Legendre intervals & weights
+    ngl = 32
+    glTau,glWt = pwd.pygauleg(0.,1.,ngl)         #get Gauss-Legendre intervals & weights
     Ax = np.array(XSSdata[:3]).T   #atoms x waves x sin pos mods
     Bx = np.array(XSSdata[3:]).T   #...cos pos mods
     Af = np.array(FSSdata[0]).T    #sin frac mods x waves x atoms
@@ -944,15 +945,15 @@ def makeWaves(waveTypes,FSSdata,XSSdata,USSdata,Mast):
     Bu = Mast*np.array(G2lat.U6toUij(USSdata[6:])).T   #...cos Uij mods as betaij
     nWaves = [Af.shape[1],Ax.shape[1],Au.shape[1]] 
     if nWaves[0]:
-        tauF = np.arange(1.,nWaves[0]+1-nf)[:,nxs]*glTau  #Fwaves x 32
-        FmodA = Af[:,:,nxs]*np.sin(twopi*tauF[nxs,:,:])   #atoms X Fwaves X 32
+        tauF = np.arange(1.,nWaves[0]+1)[:,nxs]*glTau  #Fwaves x ngl
+        FmodA = Af[:,:,nxs]*np.sin(twopi*tauF[nxs,:,:])   #atoms X Fwaves X ngl
         FmodB = Bf[:,:,nxs]*np.cos(twopi*tauF[nxs,:,:])
-        Fmod = np.sum(FmodA+FmodB,axis=1)             #atoms X 32; sum waves
+        Fmod = np.sum(1.0+FmodA+FmodB,axis=1)             #atoms X ngl; sum waves
     else:
         Fmod = 1.0           
-    XmodZ = np.zeros((Ax.shape[0],Ax.shape[1],3,32))
-    XmodA = np.zeros((Ax.shape[0],Ax.shape[1],3,32))
-    XmodB = np.zeros((Ax.shape[0],Ax.shape[1],3,32))
+    XmodZ = np.zeros((Ax.shape[0],Ax.shape[1],3,ngl))
+    XmodA = np.zeros((Ax.shape[0],Ax.shape[1],3,ngl))
+    XmodB = np.zeros((Ax.shape[0],Ax.shape[1],3,ngl))
     for iatm in range(Ax.shape[0]):
         nx = 0
         if 'ZigZag' in waveTypes[iatm]:
@@ -965,63 +966,63 @@ def makeWaves(waveTypes,FSSdata,XSSdata,USSdata,Mast):
             Tmm = Ax[iatm][0][:2]                        
             XYZmax = np.array([Ax[iatm][0][2],Bx[iatm][0][0],Bx[iatm][0][1]])
             XmodZ[iatm][0] += posBlock(glTau,Tmm,XYZmax).T
-        tauX = np.arange(1.,nWaves[1]+1-nx)[:,nxs]*glTau  #Xwaves x 32
+        tauX = np.arange(1.,nWaves[1]+1-nx)[:,nxs]*glTau  #Xwaves x ngl
         if nx:    
-            XmodA[iatm][:-nx] = Ax[iatm,nx:,:,nxs]*np.sin(twopi*tauX[nxs,:,nxs,:]) #atoms X waves X 3 X 32
+            XmodA[iatm][:-nx] = Ax[iatm,nx:,:,nxs]*np.sin(twopi*tauX[nxs,:,nxs,:]) #atoms X waves X 3 X ngl
             XmodB[iatm][:-nx] = Bx[iatm,nx:,:,nxs]*np.cos(twopi*tauX[nxs,:,nxs,:]) #ditto
         else:
-            XmodA[iatm] = Ax[iatm,:,:,nxs]*np.sin(twopi*tauX[nxs,:,nxs,:]) #atoms X waves X 3 X 32
+            XmodA[iatm] = Ax[iatm,:,:,nxs]*np.sin(twopi*tauX[nxs,:,nxs,:]) #atoms X waves X 3 X ngl
             XmodB[iatm] = Bx[iatm,:,:,nxs]*np.cos(twopi*tauX[nxs,:,nxs,:]) #ditto
-    Xmod = np.sum(XmodA+XmodB+XmodZ,axis=1)                #atoms X 3 X 32; sum waves
+    Xmod = np.sum(XmodA+XmodB+XmodZ,axis=1)                #atoms X 3 X ngl; sum waves
     Xmod = np.swapaxes(Xmod,1,2)
     if nWaves[2]:
-        tauU = np.arange(1.,nWaves[2]+1)[:,nxs]*glTau     #Uwaves x 32
-        UmodA = Au[:,:,:,:,nxs]*np.sin(twopi*tauU[nxs,:,nxs,nxs,:]) #atoms x waves x 3x3 x 32
+        tauU = np.arange(1.,nWaves[2]+1)[:,nxs]*glTau     #Uwaves x ngl
+        UmodA = Au[:,:,:,:,nxs]*np.sin(twopi*tauU[nxs,:,nxs,nxs,:]) #atoms x waves x 3x3 x ngl
         UmodB = Bu[:,:,:,:,nxs]*np.cos(twopi*tauU[nxs,:,nxs,nxs,:]) #ditto
-        Umod = np.swapaxes(np.sum(UmodA+UmodB,axis=1),1,3)      #atoms x 3x3 x 32; sum waves
+        Umod = np.swapaxes(np.sum(UmodA+UmodB,axis=1),1,3)      #atoms x 3x3 x ngl; sum waves
     else:
         Umod = 1.0
 #    GSASIIpath.IPyBreak()
-    return nWaves,Fmod,Xmod,Umod,glTau,glWt
+    return ngl,nWaves,Fmod,Xmod,Umod,glTau,glWt
         
 def Modulation(H,HP,nWaves,Fmod,Xmod,Umod,glTau,glWt):
     '''
     H: array nRefBlk x ops X hklt
     HP: array nRefBlk x ops X hklt proj to hkl
     Fmod: array 2 x atoms x waves    (sin,cos terms)
-    Xmod: array atoms X 3 X 32
-    Umod: array atoms x 3x3 x 32
+    Xmod: array atoms X 3 X ngl
+    Umod: array atoms x 3x3 x ngl
     glTau,glWt: arrays Gauss-Lorentzian pos & wts
     '''
     
-    nxs = np.newaxis    
+    nxs = np.newaxis
     if nWaves[2]:
         if len(HP.shape) > 2:
-            HbH = np.exp(-np.sum(HP[:,:,nxs,nxs,:]*np.inner(HP,Umod),axis=-1)) # refBlk x ops x atoms x 32 add Overhauser corr.?
+            HbH = np.exp(-np.sum(HP[:,:,nxs,nxs,:]*np.inner(HP,Umod),axis=-1)) # refBlk x ops x atoms x ngl add Overhauser corr.?
         else:
-            HbH = np.exp(-np.sum(HP[:,nxs,nxs,:]*np.inner(HP,Umod),axis=-1)) # refBlk x ops x atoms x 32 add Overhauser corr.?
+            HbH = np.exp(-np.sum(HP[:,nxs,nxs,:]*np.inner(HP,Umod),axis=-1)) # refBlk x ops x atoms x ngl add Overhauser corr.?
     else:
         HbH = 1.0
-    HdotX = np.inner(HP,Xmod)                   #refBlk x ops x atoms X 32
+    HdotX = np.inner(HP,Xmod)                   #refBlk x ops x atoms X ngl
     if len(H.shape) > 2:
-        D = H[:,:,3:]*glTau[nxs,nxs,:]              #m*e*tau; refBlk x ops X 32
+        D = H[:,:,3:]*glTau[nxs,nxs,:]              #m*e*tau; refBlk x ops X ngl
         HdotXD = twopi*(HdotX+D[:,:,nxs,:])
     else:
-        D = H[:,3:]*glTau[nxs,:]              #m*e*tau; refBlk x ops X 32
+        D = H[:,3:]*glTau[nxs,:]              #m*e*tau; refBlk x ops X ngl
         HdotXD = twopi*(HdotX+D[:,nxs,:])
     cosHA = np.sum(Fmod*HbH*np.cos(HdotXD)*glWt,axis=-1)       #real part; refBlk X ops x atoms; sum for G-L integration
     sinHA = np.sum(Fmod*HbH*np.sin(HdotXD)*glWt,axis=-1)       #imag part; ditto
     return np.array([cosHA,sinHA])             # 2 x refBlk x SGops x atoms
     
-def makeWavesDerv(waveTypes,FSSdata,XSSdata,USSdata,Mast):
+def makeWavesDerv(ngl,waveTypes,FSSdata,XSSdata,USSdata,Mast):
     '''
     FSSdata: array 2 x atoms x waves    (sin,cos terms)
     XSSdata: array 2x3 x atoms X waves (sin,cos terms)
     USSdata: array 2x6 x atoms X waves (sin,cos terms)
     Mast: array orthogonalization matrix for Uij
     '''
-    glTau,glWt = pwd.pygauleg(0.,1.,32)         #get Gauss-Legendre intervals & weights
-    dT = 1/32.
+    glTau,glWt = pwd.pygauleg(0.,1.,ngl)         #get Gauss-Legendre intervals & weights
+    dT = 1./ngl
     dX = 0.0001
     waveShapes = [FSSdata.T.shape,XSSdata.T.shape,USSdata.T.shape]
     Af = np.array(FSSdata[0]).T    #sin frac mods x waves x atoms
@@ -1031,11 +1032,10 @@ def makeWavesDerv(waveTypes,FSSdata,XSSdata,USSdata,Mast):
     Au = Mast*np.array(G2lat.U6toUij(USSdata[:6])).T   #atoms x waves x sin Uij mods
     Bu = Mast*np.array(G2lat.U6toUij(USSdata[6:])).T   #...cos Uij mods
     nWaves = [Af.shape[1],Ax.shape[1],Au.shape[1]] 
-    nx = 0
-    StauX = np.zeros((Ax.shape[0],Ax.shape[1],3,32))    #atoms x waves x 3 x 32
-    CtauX = np.zeros((Ax.shape[0],Ax.shape[1],3,32))
-    ZtauXx = np.zeros((Ax.shape[0],3,32))               #atoms x XYZmax x 32
-    ZtauXt = np.zeros((Ax.shape[0],2,32))               #atoms x Tminmax x 32
+    StauX = np.zeros((Ax.shape[0],Ax.shape[1],3,ngl))    #atoms x waves x 3 x ngl
+    CtauX = np.zeros((Ax.shape[0],Ax.shape[1],3,ngl))
+    ZtauXt = np.zeros((Ax.shape[0],2,ngl))               #atoms x Tminmax x ngl
+    ZtauXx = np.zeros((Ax.shape[0],3,ngl))               #atoms x XYZmax x ngl
     for iatm in range(Ax.shape[0]):
         nx = 0
         if 'ZigZag' in waveTypes[iatm]:
@@ -1074,28 +1074,28 @@ def makeWavesDerv(waveTypes,FSSdata,XSSdata,USSdata,Mast):
                 Zm = posBlock(glTau,Tmm,XYZmax).T
                 XYZmax[i] += dX
                 ZtauXx[iatm][i] = (Zp-Zm)[i]/(2.*dX)
-        tauX = np.arange(1.,nWaves[1]+1-nx)[:,nxs]*glTau  #Xwaves x 32
+        tauX = np.arange(1.,nWaves[1]+1-nx)[:,nxs]*glTau  #Xwaves x ngl
         if nx:    
-            StauX[iatm][:-nx] = np.ones_like(Ax)[iatm,nx:,:,nxs]*np.sin(twopi*tauX)[nxs,:,nxs,:]   #atoms X waves X 3(xyz) X 32
+            StauX[iatm][:-nx] = np.ones_like(Ax)[iatm,nx:,:,nxs]*np.sin(twopi*tauX)[nxs,:,nxs,:]   #atoms X waves X 3(xyz) X ngl
             CtauX[iatm][:-nx] = np.ones_like(Bx)[iatm,nx:,:,nxs]*np.cos(twopi*tauX)[nxs,:,nxs,:]   #ditto
         else:
-            StauX[iatm] = np.ones_like(Ax)[iatm,:,:,nxs]*np.sin(twopi*tauX)[nxs,:,nxs,:]   #atoms X waves X 3(xyz) X 32
+            StauX[iatm] = np.ones_like(Ax)[iatm,:,:,nxs]*np.sin(twopi*tauX)[nxs,:,nxs,:]   #atoms X waves X 3(xyz) X ngl
             CtauX[iatm] = np.ones_like(Bx)[iatm,:,:,nxs]*np.cos(twopi*tauX)[nxs,:,nxs,:]   #ditto
 #    GSASIIpath.IPyBreak()
     if nWaves[0]:
-        tauF = np.arange(1.,nWaves[0]+1-nf)[:,nxs]*glTau  #Fwaves x 32
+        tauF = np.arange(1.,nWaves[0]+1-nf)[:,nxs]*glTau  #Fwaves x ngl
         StauF = np.ones_like(Af)[:,:,nxs]*np.sin(twopi*tauF)[nxs,:,:] #also dFmod/dAf
         CtauF = np.ones_like(Bf)[:,:,nxs]*np.cos(twopi*tauF)[nxs,:,:] #also dFmod/dBf
     else:
         StauF = 1.0
         CtauF = 1.0
     if nWaves[2]:
-        tauU = np.arange(1.,nWaves[2]+1)[:,nxs]*glTau     #Uwaves x 32
+        tauU = np.arange(1.,nWaves[2]+1)[:,nxs]*glTau     #Uwaves x ngl
         StauU = np.ones_like(Au)[:,:,:,:,nxs]*np.sin(twopi*tauU)[nxs,:,nxs,nxs,:]   #also dUmodA/dAu
         CtauU = np.ones_like(Bu)[:,:,:,:,nxs]*np.cos(twopi*tauU)[nxs,:,nxs,nxs,:]   #also dUmodB/dBu
-        UmodA = Au[:,:,:,:,nxs]*StauU #atoms x waves x 3x3 x 32
+        UmodA = Au[:,:,:,:,nxs]*StauU #atoms x waves x 3x3 x ngl
         UmodB = Bu[:,:,:,:,nxs]*CtauU #ditto
-#derivs need to be ops x atoms x waves x 6uij; ops x atoms x waves x 32 x 6uij before sum
+#derivs need to be ops x atoms x waves x 6uij; ops x atoms x waves x ngl x 6uij before sum
         StauU = np.rollaxis(np.rollaxis(np.swapaxes(StauU,2,4),-1),-1)
         CtauU = np.rollaxis(np.rollaxis(np.swapaxes(CtauU,2,4),-1),-1)
     else:
@@ -1122,17 +1122,17 @@ def ModulationDerv(H,HP,Hij,nWaves,waveShapes,Fmod,Xmod,UmodAB,SCtauF,SCtauX,SCt
     dGdMuC = np.zeros(Mu)
     dGdMuS = np.zeros(Mu)
     
-    D = twopi*H[:,3][:,nxs]*glTau[nxs,:]              #m*e*tau; ops X 32
-    HdotX = twopi*np.inner(HP,Xmod)        #ops x atoms X 32
+    D = twopi*H[:,3][:,nxs]*glTau[nxs,:]              #m*e*tau; ops X ngl
+    HdotX = twopi*np.inner(HP,Xmod)        #ops x atoms X ngl
     HdotXD = HdotX+D[:,nxs,:]
     if nWaves[2]:
-        Umod = np.swapaxes((UmodAB),2,4)      #atoms x waves x 32 x 3x3 (symmetric so I can do this!) 
-        HuH = np.sum(HP[:,nxs,nxs,nxs]*np.inner(HP,Umod),axis=-1)    #ops x atoms x waves x 32
-        HuH = np.sum(HP[:,nxs,nxs,nxs]*np.inner(HP,Umod),axis=-1)    #ops x atoms x waves x 32
-        HbH = np.exp(-np.sum(HuH,axis=-2)) # ops x atoms x 32; sum waves - OK vs Modulation version
-        part1 = -np.exp(-HuH)*Fmod    #ops x atoms x waves x 32
-        dUdAu = Hij[:,nxs,nxs,nxs,:]*np.rollaxis(G2lat.UijtoU6(SCtauU[0]),0,4)[nxs,:,:,:,:]    #ops x atoms x waves x 32 x 6sinUij
-        dUdBu = Hij[:,nxs,nxs,nxs,:]*np.rollaxis(G2lat.UijtoU6(SCtauU[1]),0,4)[nxs,:,:,:,:]    #ops x atoms x waves x 32 x 6cosUij
+        Umod = np.swapaxes((UmodAB),2,4)      #atoms x waves x ngl x 3x3 (symmetric so I can do this!) 
+        HuH = np.sum(HP[:,nxs,nxs,nxs]*np.inner(HP,Umod),axis=-1)    #ops x atoms x waves x ngl
+        HuH = np.sum(HP[:,nxs,nxs,nxs]*np.inner(HP,Umod),axis=-1)    #ops x atoms x waves x ngl
+        HbH = np.exp(-np.sum(HuH,axis=-2)) # ops x atoms x ngl; sum waves - OK vs Modulation version
+        part1 = -np.exp(-HuH)*Fmod    #ops x atoms x waves x ngl
+        dUdAu = Hij[:,nxs,nxs,nxs,:]*np.rollaxis(G2lat.UijtoU6(SCtauU[0]),0,4)[nxs,:,:,:,:]    #ops x atoms x waves x ngl x 6sinUij
+        dUdBu = Hij[:,nxs,nxs,nxs,:]*np.rollaxis(G2lat.UijtoU6(SCtauU[1]),0,4)[nxs,:,:,:,:]    #ops x atoms x waves x ngl x 6cosUij
         dGdMuCa = np.sum(part1[:,:,:,:,nxs]*dUdAu*np.cos(HdotXD)[:,:,nxs,:,nxs]*glWt[nxs,nxs,nxs,:,nxs],axis=-2)   #ops x atoms x waves x 6uij; G-L sum
         dGdMuCb = np.sum(part1[:,:,:,:,nxs]*dUdBu*np.cos(HdotXD)[:,:,nxs,:,nxs]*glWt[nxs,nxs,nxs,:,nxs],axis=-2)
         dGdMuC = np.concatenate((dGdMuCa,dGdMuCb),axis=-1)   #ops x atoms x waves x 12uij
@@ -1141,7 +1141,7 @@ def ModulationDerv(H,HP,Hij,nWaves,waveShapes,Fmod,Xmod,UmodAB,SCtauF,SCtauX,SCt
         dGdMuS = np.concatenate((dGdMuSa,dGdMuSb),axis=-1)   #ops x atoms x waves x 12uij
     else:
         HbH = np.ones_like(HdotX)
-    dHdXA = twopi*HP[:,nxs,nxs,nxs,:]*np.swapaxes(SCtauX[0],-1,-2)[nxs,:,:,:,:]    #ops x atoms x sine waves x 32 x xyz
+    dHdXA = twopi*HP[:,nxs,nxs,nxs,:]*np.swapaxes(SCtauX[0],-1,-2)[nxs,:,:,:,:]    #ops x atoms x sine waves x ngl x xyz
     dHdXB = twopi*HP[:,nxs,nxs,nxs,:]*np.swapaxes(SCtauX[1],-1,-2)[nxs,:,:,:,:]    #ditto - cos waves
     dGdMxCa = -np.sum((Fmod*HbH)[:,:,nxs,:,nxs]*(dHdXA*np.sin(HdotXD)[:,:,nxs,:,nxs])*glWt[nxs,nxs,nxs,:,nxs],axis=-2)
     dGdMxCb = -np.sum((Fmod*HbH)[:,:,nxs,:,nxs]*(dHdXB*np.sin(HdotXD)[:,:,nxs,:,nxs])*glWt[nxs,nxs,nxs,:,nxs],axis=-2)
@@ -1151,8 +1151,8 @@ def ModulationDerv(H,HP,Hij,nWaves,waveShapes,Fmod,Xmod,UmodAB,SCtauF,SCtauX,SCt
     dGdMxSb = np.sum((Fmod*HbH)[:,:,nxs,:,nxs]*(dHdXB*np.cos(HdotXD)[:,:,nxs,:,nxs])*glWt[nxs,nxs,nxs,:,nxs],axis=-2)    
     dGdMxS = np.concatenate((dGdMxSa,dGdMxSb),axis=-1)
 # ZigZag/Block waves
-    dHdXZt = twopi*np.ones(HP.shape[0])[:,nxs,nxs,nxs]*np.swapaxes(SCtauX[2],-1,-2)[nxs,:,:,:]          #??ops x atoms x 32 x 3(ZigZag/Block Tminmax)
-    dHdXZx = twopi*HP[:,nxs,nxs,:]*np.swapaxes(SCtauX[3],-1,-2)[nxs,:,:,:]          #ops x atoms x 32 x 3(ZigZag/Block XYZmax)
+    dHdXZt = twopi*np.ones(HP.shape[0])[:,nxs,nxs,nxs]*np.swapaxes(SCtauX[2],-1,-2)[nxs,:,:,:]          #??ops x atoms x ngl x 3(ZigZag/Block Tminmax)
+    dHdXZx = twopi*HP[:,nxs,nxs,:]*np.swapaxes(SCtauX[3],-1,-2)[nxs,:,:,:]          #ops x atoms x ngl x 3(ZigZag/Block XYZmax)
     dGdMzCt = -np.sum((Fmod*HbH)[:,:,:,nxs]*(dHdXZt*np.sin(HdotXD)[:,:,:,nxs])*glWt[nxs,nxs,:,nxs],axis=-2)
     dGdMzCx = -np.sum((Fmod*HbH)[:,:,:,nxs]*(dHdXZx*np.sin(HdotXD)[:,:,:,nxs])*glWt[nxs,nxs,:,nxs],axis=-2)
     dGdMzC = np.concatenate((dGdMzCt,dGdMzCx),axis=-1)

GSASIIphsGUI.py
@@ -67,15 +67,15 @@ asind = lambda x: 180.*np.arcsin(x)/np.pi
 acosd = lambda x: 180.*np.arccos(x)/np.pi
 atan2d = lambda x,y: 180.*np.arctan2(y,x)/np.pi
 
-def SetPhaseWindow(mainFrame,phasePage,mainSizer):
+def SetPhaseWindow(mainFrame,phasePage,mainSizer,Scroll=0):
     phasePage.SetSizer(mainSizer)
     Size = mainSizer.GetMinSize()
     Size[0] += 40
     Size[1] = min(Size[1]+ 150,500) 
-    phasePage.SetSize(Size)
     phasePage.SetScrollbars(10,10,Size[0]/10-4,Size[1]/10-1)
+    phasePage.SetSize(Size)
+    phasePage.Scroll(0,Scroll)
     mainFrame.setSizePosLeft(Size)
-    mainFrame.SendSizeEvent()
     
 #def FindBondsToo():                         #works but slow for large structures - keep as reference
 #    cx,ct,cs,ci = data['Drawing']['atomPtrs']
@@ -318,7 +318,7 @@ def UpdatePhaseData(G2frame,Item,data,oldPage):
 ##### General phase routines
 ################################################################################
 
-    def UpdateGeneral():
+    def UpdateGeneral(Scroll=0):
         '''Draw the controls for the General phase data subpage
         '''
         
@@ -714,7 +714,7 @@ def UpdatePhaseData(G2frame,Item,data,oldPage):
             
         def ModulatedSizer(name):
             
-            def OnSuperGp(event):
+            def OnSuperGp(event):   #for HKLF needs to reject SSgps not agreeing with modVec!
                 SSymbol = superGp.GetValue()
                 E,SSGData = G2spc.SSpcGroup(generalData['SGData'],SSymbol)
                 if SSGData:
@@ -770,12 +770,13 @@ def UpdatePhaseData(G2frame,Item,data,oldPage):
                 superGp = wx.TextCtrl(General,value=generalData['SuperSg'],style=wx.TE_PROCESS_ENTER)
                 superGp.Bind(wx.EVT_TEXT_ENTER,OnSuperGp)                        
             modSizer.Add(superGp,0,WACV)
-            modSizer.Add(wx.StaticText(General,label=' Max index: '),0,WACV)
-            indChoice = ['1','2','3','4','5','6','7']
-            Max = wx.ComboBox(General,-1,value='%d'%(generalData['SuperVec'][2]),choices=indChoice,
-                style=wx.CB_READONLY|wx.CB_DROPDOWN)
-            Max.Bind(wx.EVT_COMBOBOX,OnMax)        
-            modSizer.Add(Max,0,WACV)
+            if PWDR:
+                modSizer.Add(wx.StaticText(General,label=' Max index: '),0,WACV)
+                indChoice = ['1','2','3','4','5','6','7']
+                Max = wx.ComboBox(General,-1,value='%d'%(generalData['SuperVec'][2]),choices=indChoice,
+                    style=wx.CB_READONLY|wx.CB_DROPDOWN)
+                Max.Bind(wx.EVT_COMBOBOX,OnMax)        
+                modSizer.Add(Max,0,WACV)
             ssSizer.Add(modSizer,0,WACV)
             vecSizer = wx.FlexGridSizer(1,5,5,5)
             vecSizer.Add(wx.StaticText(General,label=' Modulation vector: '),0,WACV)
@@ -795,10 +796,11 @@ def UpdatePhaseData(G2frame,Item,data,oldPage):
                         size=wx.Size(50,20),style=wx.TE_READONLY)
                     modVal.SetBackgroundColour(VERY_LIGHT_GREY)
                     vecSizer.Add(modVal,0,WACV)
-            Ref = wx.CheckBox(General,label='Refine?')
-            Ref.SetValue(generalData['SuperVec'][1])
-            Ref.Bind(wx.EVT_CHECKBOX, OnVecRef)
-            vecSizer.Add(Ref,0,WACV)
+            if PWDR:
+                Ref = wx.CheckBox(General,label='Refine?')
+                Ref.SetValue(generalData['SuperVec'][1])
+                Ref.Bind(wx.EVT_CHECKBOX, OnVecRef)
+                vecSizer.Add(Ref,0,WACV)
             ssSizer.Add(vecSizer)
             return ssSizer
             
@@ -817,7 +819,7 @@ def UpdatePhaseData(G2frame,Item,data,oldPage):
                         return
                 finally:
                     dlg.Destroy()
-                wx.CallAfter(UpdateGeneral)                
+                wx.CallAfter(UpdateGeneral,General.GetScrollPos(wx.VERTICAL))                
                 
             def OnResVal(event):
                 try:
@@ -888,7 +890,7 @@ def UpdatePhaseData(G2frame,Item,data,oldPage):
                         return
                 finally:
                     dlg.Destroy()
-                wx.CallAfter(UpdateGeneral)                
+                wx.CallAfter(UpdateGeneral,General.GetScrollPos(wx.VERTICAL))                
                 
             def OnNormElem(event):
                 PE = G2elemGUI.PickElement(G2frame,ifNone=True)
@@ -980,7 +982,7 @@ def UpdatePhaseData(G2frame,Item,data,oldPage):
                                
             def OnAlist(event):
                 MCSAdata['Algorithm'] = Alist.GetValue()
-                wx.CallAfter(UpdateGeneral)
+                wx.CallAfter(UpdateGeneral,General.GetScrollPos(wx.VERTICAL))
                 
             def OnSlope(event):
                 try:
@@ -1170,7 +1172,7 @@ def UpdatePhaseData(G2frame,Item,data,oldPage):
             G2G.HorizontalLine(mainSizer,General)
             mainSizer.Add(MCSASizer())
         G2frame.dataFrame.SetStatusText('')
-        SetPhaseWindow(G2frame.dataFrame,General,mainSizer)
+        SetPhaseWindow(G2frame.dataFrame,General,mainSizer,Scroll)
 
 ################################################################################
 #####  Atom routines
@@ -2335,7 +2337,7 @@ def UpdatePhaseData(G2frame,Item,data,oldPage):
 #### Wave Data page
 ################################################################################
 
-    def UpdateWavesData():
+    def UpdateWavesData(Scroll=0):
         
         generalData = data['General']
         cx,ct,cs,cia = generalData['AtomPtrs']
@@ -2345,7 +2347,7 @@ def UpdatePhaseData(G2frame,Item,data,oldPage):
             def OnWaveType(event):
                 atom[-1][SS]['waveType']=waveType.GetValue()
                 atom[-1][SS]['Spos'] = []
-                UpdateWavesData()                
+                UpdateWavesData(G2frame.waveData.GetScrollPos(wx.VERTICAL))                
                 
             def OnShowWave(event):
                 Obj = event.GetEventObject()
@@ -2379,7 +2381,7 @@ def UpdatePhaseData(G2frame,Item,data,oldPage):
                 if not len(atomData[iatm][-1][SS][item]) and waveType in ['ZigZag','Block'] and Stype == 'Spos':
                     nt = numVals[waveType]
                 atomData[iatm][-1][SS][item].append([[0.0 for i in range(nt)],False])
-                UpdateWavesData()
+                UpdateWavesData(G2frame.waveData.GetScrollPos(wx.VERTICAL))
                 
             def OnWaveVal(event):
                 Obj = event.GetEventObject()
@@ -2405,7 +2407,7 @@ def UpdatePhaseData(G2frame,Item,data,oldPage):
                 Obj = event.GetEventObject()
                 iatm,item,iwave = Indx[Obj.GetId()]
                 del atomData[iatm][-1][SS][item][iwave]
-                UpdateWavesData()                
+                UpdateWavesData(G2frame.waveData.GetScrollPos(wx.VERTICAL))                
             
             waveSizer = wx.BoxSizer(wx.VERTICAL)
             waveHead = wx.BoxSizer(wx.HORIZONTAL)
@@ -2508,26 +2510,9 @@ def UpdatePhaseData(G2frame,Item,data,oldPage):
                         if generalData['Type'] != 'magnetic' and Stype == 'Smag':
                             break
                         mainSizer.Add(WaveSizer(atm[-1][SS]['waveType'],atm[-1][SS][Stype],Stype,typeNames[Stype],Labels[Stype]))                        
-        SetPhaseWindow(G2frame.dataFrame,waveData,mainSizer)
-                       
-    def On4DMapCompute(event):
-        generalData = data['General']
-        mapData = generalData['4DmapData']
-        reflNames = mapData['RefList']
-        generalData['Map']['Flip'] = False
-        if not len(reflNames):
-            G2frame.ErrorDialog('Fourier map error','No reflections defined for Fourier map')
-            return
-        phaseName = generalData['Name']
-        ReflData = GetReflData(G2frame,phaseName,reflNames)
-        if ReflData == None: return
-        G2mth.Fourier4DMap(data,ReflData)
-        data['Drawing']['contourLevel'] = 1.
-        data['Drawing']['mapSize'] = 10.
-        mapSig = np.std(mapData['rho'])
-        print '4D '+mapData['MapType']+' computed: rhomax = %.3f rhomin = %.3f sigma = %.3f'%(np.max(mapData['rho']),np.min(mapData['rho']),mapSig)
-        wx.CallAfter(UpdateWavesData)
-            
+        
+        SetPhaseWindow(G2frame.dataFrame,G2frame.waveData,mainSizer,Scroll)
+
 ################################################################################
 #### Structure drawing GUI stuff                
 ################################################################################
@@ -4084,7 +4069,6 @@ def UpdatePhaseData(G2frame,Item,data,oldPage):
             H = list(ref[:3])
             ref[4] = np.sqrt(1./G2lat.calc_rDsq2(H,G))
             iabsnt,ref[3],Uniq,phi = G2spc.GenHKLf(H,SGData)
-        #G2frame.PatternTree.SetItemPyData(Id,[refDict,reflData]) #removed by BHT -- not needed!
         
     def OnDataCopy(event):
         UseList = data['Histograms']

GSASIIstrMath.py
@@ -993,7 +993,7 @@ def SStructureFactor(refDict,im,G,hfx,pfx,SGData,SSGData,calcControls,parmDict):
         TwinInv = list(np.where(calcControls[phfx+'TwinInv'],-1,1))
     Tdata,Mdata,Fdata,Xdata,dXdata,IAdata,Uisodata,Uijdata = GetAtomFXU(pfx,calcControls,parmDict)
     waveTypes,FSSdata,XSSdata,USSdata,MSSdata = GetAtomSSFXU(pfx,calcControls,parmDict)
-    nWaves,Fmod,Xmod,Umod,glTau,glWt = G2mth.makeWaves(waveTypes,FSSdata,XSSdata,USSdata,Mast)
+    ngl,nWaves,Fmod,Xmod,Umod,glTau,glWt = G2mth.makeWaves(waveTypes,FSSdata,XSSdata,USSdata,Mast)
     modQ = np.array([parmDict[pfx+'mV0'],parmDict[pfx+'mV1'],parmDict[pfx+'mV2']])
     FF = np.zeros(len(Tdata))
     if 'NC' in calcControls[hfx+'histType']:
@@ -1120,8 +1120,8 @@ def SStructureFactorDerv(refDict,im,G,hfx,pfx,SGData,SSGData,calcControls,parmDi
     Tdata,Mdata,Fdata,Xdata,dXdata,IAdata,Uisodata,Uijdata = GetAtomFXU(pfx,calcControls,parmDict)
     mSize = len(Mdata)  #no. atoms
     waveTypes,FSSdata,XSSdata,USSdata,MSSdata = GetAtomSSFXU(pfx,calcControls,parmDict)
-    nWaves,Fmod,Xmod,Umod,glTau,glWt = G2mth.makeWaves(waveTypes,FSSdata,XSSdata,USSdata,Mast)
-    waveShapes,SCtauF,SCtauX,SCtauU,UmodAB = G2mth.makeWavesDerv(waveTypes,FSSdata,XSSdata,USSdata,Mast)
+    ngl,nWaves,Fmod,Xmod,Umod,glTau,glWt = G2mth.makeWaves(waveTypes,FSSdata,XSSdata,USSdata,Mast)
+    waveShapes,SCtauF,SCtauX,SCtauU,UmodAB = G2mth.makeWavesDerv(ngl,waveTypes,FSSdata,XSSdata,USSdata,Mast)
     modQ = np.array([parmDict[pfx+'mV0'],parmDict[pfx+'mV1'],parmDict[pfx+'mV2']])
     FF = np.zeros(len(Tdata))
     if 'NC' in calcControls[hfx+'histType']:
@@ -1147,12 +1147,10 @@ def SStructureFactorDerv(refDict,im,G,hfx,pfx,SGData,SSGData,calcControls,parmDi
         dFdbab = np.zeros((nRef,nTwin,2))
         dFdfl = np.zeros((nRef,nTwin))
         dFdtw = np.zeros((nRef,nTwin))
-        dFdGfA = np.zeros((nRef,nTwin,mSize,FSSdata.shape[1]))
-        dFdGfB = np.zeros((nRef,nTwin,mSize,FSSdata.shape[1]))
-        dFdGxA = np.zeros((nRef,nTwin,mSize,XSSdata.shape[1],3))
-        dFdGxB = np.zeros((nRef,nTwin,mSize,XSSdata.shape[1],3))
-        dFdGuA = np.zeros((nRef,nTwin,mSize,USSdata.shape[1],6))
-        dFdGuB = np.zeros((nRef,nTwin,mSize,USSdata.shape[1],6))
+        dFdGf = np.zeros((nRef,nTwin,mSize,FSSdata.shape[1]))
+        dFdGx = np.zeros((nRef,nTwin,mSize,XSSdata.shape[1],3))
+        dFdGz = np.zeros((nRef,nTwin,mSize,5))
+        dFdGu = np.zeros((nRef,nTwin,mSize,USSdata.shape[1],6))
     else:
         dFdfr = np.zeros((nRef,mSize))
         dFdx = np.zeros((nRef,mSize,3))
@@ -1241,8 +1239,8 @@ def SStructureFactorDerv(refDict,im,G,hfx,pfx,SGData,SSGData,calcControls,parmDi
             dfbdGf = np.sum(fb[:,it,:,:,nxs,nxs]*dGdf[0][nxs,nxs,:,:,:,:]+fa[:,it,:,:,nxs,nxs]*dGdf[1][nxs,nxs,:,:,:,:],axis=1)
             dfadGx = np.sum(fa[:,it,:,:,nxs,nxs]*dGdx[0][nxs,nxs,:,:,:,:]-fb[:,it,:,:,nxs,nxs]*dGdx[1][nxs,nxs,:,:,:,:],axis=1)
             dfbdGx = np.sum(fb[:,it,:,:,nxs,nxs]*dGdx[0][nxs,nxs,:,:,:,:]+fa[:,it,:,:,nxs,nxs]*dGdx[1][nxs,nxs,:,:,:,:],axis=1)
-            dfadGz = np.sum(fa[:,it,:,0,nxs,nxs]*dGdx[0][nxs,nxs,:,:,:]-fb[:,it,:,0,nxs,nxs]*dGdx[1][nxs,nxs,:,:,:],axis=1)
-            dfbdGz = np.sum(fb[:,it,:,0,nxs,nxs]*dGdx[0][nxs,nxs,:,:,:]+fa[:,it,:,0,nxs,nxs]*dGdx[1][nxs,nxs,:,:,:],axis=1)
+            dfadGz = np.sum(fa[:,it,:,0,nxs,nxs]*dGdz[0][nxs,nxs,:,:,:]-fb[:,it,:,0,nxs,nxs]*dGdz[1][nxs,nxs,:,:,:],axis=1)
+            dfbdGz = np.sum(fb[:,it,:,0,nxs,nxs]*dGdz[0][nxs,nxs,:,:,:]+fa[:,it,:,0,nxs,nxs]*dGdz[1][nxs,nxs,:,:,:],axis=1)
             dfadGu = np.sum(fa[:,it,:,:,nxs,nxs]*dGdu[0][nxs,nxs,:,:,:,:]-fb[:,it,:,:,nxs,nxs]*dGdu[1][nxs,nxs,:,:,:,:],axis=1)
             dfbdGu = np.sum(fb[:,it,:,:,nxs,nxs]*dGdu[0][nxs,nxs,:,:,:,:]+fa[:,it,:,:,nxs,nxs]*dGdu[1][nxs,nxs,:,:,:,:],axis=1)
         else:
@@ -1277,6 +1275,7 @@ def SStructureFactorDerv(refDict,im,G,hfx,pfx,SGData,SSGData,calcControls,parmDi
                            
             dFdGf[iref] = 2.*(SA*dfadGf[0]+SB*dfbdGf[1])      #array(nRef,natom,nwave,2)
             dFdGx[iref] = 2.*(SA*dfadGx[0]+SB*dfbdGx[1])      #array(nRef,natom,nwave,6)
+            dFdGz[iref] = 2.*(SA*dfadGz[0]+SB*dfbdGz[1])      #array(nRef,natom,5)
             dFdGu[iref] = 2.*(SA*dfadGu[0]+SB*dfbdGu[1])      #array(nRef,natom,nwave,12)
 #            dFdfr[iref] = 2.*(fas[0]*dfadfr[0]+fas[1]*dfadfr[1])*Mdata/len(Uniq)+   \
 #                2.*(fbs[0]*dfbdfr[0]-fbs[1]*dfbdfr[1])*Mdata/len(Uniq)
@@ -1302,7 +1301,7 @@ def SStructureFactorDerv(refDict,im,G,hfx,pfx,SGData,SSGData,calcControls,parmDi
 
                 dFdGf[iref] = [2.*TwMask[it]*(SA[it]*dfadGf[1]+SB[it]*dfbdGf[1]) for it in range(nTwin)]
                 dFdGx[iref] = [2.*TwMask[it]*(SA[it]*dfadGx[1]+SB[it]*dfbdGx[1]) for it in range(nTwin)]
-                dFdGz[iref] = [2.*TwMask[it]*(SA[it]*dfadGx[1]+SB[it]*dfbdGx[1]) for it in range(nTwin)]
+                dFdGz[iref] = [2.*TwMask[it]*(SA[it]*dfadGz[1]+SB[it]*dfbdGz[1]) for it in range(nTwin)]
                 dFdGu[iref] = [2.*TwMask[it]*(SA[it]*dfadGu[1]+SB[it]*dfbdGu[1]) for it in range(nTwin)]                
             else:   #these are good for no twin single crystals
                 dFdfr[iref] = 2.*(SA*dfadfr[0]+SA*dfadfr[1]+SB*dfbdfr[0]+SB*dfbdfr[1])*Mdata/len(Uniq) #array(nRef,nAtom)
@@ -1318,10 +1317,9 @@ def SStructureFactorDerv(refDict,im,G,hfx,pfx,SGData,SSGData,calcControls,parmDi
         dFdbab[iref] = 2.*fas[0]*np.array([np.sum(dfadba*dBabdA),np.sum(-dfadba*parmDict[phfx+'BabA']*SQfactor*dBabdA)]).T+ \
             2.*fbs[0]*np.array([np.sum(dfbdba*dBabdA),np.sum(-dfbdba*parmDict[phfx+'BabA']*SQfactor*dBabdA)]).T
         #loop over atoms - each dict entry is list of derivatives for all the reflections
-#        GSASIIpath.IPyBreak()
         if not iref%100 :
             print ' %d derivative time %.4f\r'%(iref,time.time()-time0),
-    for i in range(len(Mdata)):     #loop over atoms  
+    for i in range(len(Mdata)):     #loop over atoms
         dFdvDict[pfx+'Afrac:'+str(i)] = dFdfr.T[i]
         dFdvDict[pfx+'dAx:'+str(i)] = dFdx.T[0][i]
         dFdvDict[pfx+'dAy:'+str(i)] = dFdx.T[1][i]
@@ -1336,18 +1334,21 @@ def SStructureFactorDerv(refDict,im,G,hfx,pfx,SGData,SSGData,calcControls,parmDi
         for j in range(FSSdata.shape[1]):        #loop over waves Fzero & Fwid?
             dFdvDict[pfx+'Fsin:'+str(i)+':'+str(j)] = dFdGf.T[0][j][i]
             dFdvDict[pfx+'Fcos:'+str(i)+':'+str(j)] = dFdGf.T[1][j][i]
-        for j in range(XSSdata.shape[1]):       #loop over waves 
-            dFdvDict[pfx+'Xsin:'+str(i)+':'+str(j)] = dFdGx.T[0][j][i]
-            dFdvDict[pfx+'Ysin:'+str(i)+':'+str(j)] = dFdGx.T[1][j][i]
-            dFdvDict[pfx+'Zsin:'+str(i)+':'+str(j)] = dFdGx.T[2][j][i]
-            dFdvDict[pfx+'Xcos:'+str(i)+':'+str(j)] = dFdGx.T[3][j][i]
-            dFdvDict[pfx+'Ycos:'+str(i)+':'+str(j)] = dFdGx.T[4][j][i]
-            dFdvDict[pfx+'Zcos:'+str(i)+':'+str(j)] = dFdGx.T[5][j][i]
-        dFdvDict[pfx+'Tmin:'+str(i)+':0'] = dFdGz.T[0][i]
-        dFdvDict[pfx+'Tmax:'+str(i)+':0'] = dFdGz.T[0][i]
-        dFdvDict[pfx+'Xmax:'+str(i)+':0'] = dFdGz.T[0][i]
-        dFdvDict[pfx+'Ymax:'+str(i)+':0'] = dFdGz.T[0][i]
-        dFdvDict[pfx+'Zmax:'+str(i)+':0'] = dFdGz.T[0][i]
+        nx = 0
+        if waveTypes[i] in ['Block','ZigZag']:
+            nx = 1 
+            dFdvDict[pfx+'Tmin:'+str(i)+':0'] = dFdGz.T[0][i]   #ZigZag/Block waves (if any)
+            dFdvDict[pfx+'Tmax:'+str(i)+':0'] = dFdGz.T[1][i]
+            dFdvDict[pfx+'Xmax:'+str(i)+':0'] = dFdGz.T[2][i]
+            dFdvDict[pfx+'Ymax:'+str(i)+':0'] = dFdGz.T[3][i]
+            dFdvDict[pfx+'Zmax:'+str(i)+':0'] = dFdGz.T[4][i]
+        for j in range(XSSdata.shape[1]-nx):       #loop over waves 
+            dFdvDict[pfx+'Xsin:'+str(i)+':'+str(j+nx)] = dFdGx.T[0][j][i]
+            dFdvDict[pfx+'Ysin:'+str(i)+':'+str(j+nx)] = dFdGx.T[1][j][i]
+            dFdvDict[pfx+'Zsin:'+str(i)+':'+str(j+nx)] = dFdGx.T[2][j][i]
+            dFdvDict[pfx+'Xcos:'+str(i)+':'+str(j+nx)] = dFdGx.T[3][j][i]
+            dFdvDict[pfx+'Ycos:'+str(i)+':'+str(j+nx)] = dFdGx.T[4][j][i]
+            dFdvDict[pfx+'Zcos:'+str(i)+':'+str(j+nx)] = dFdGx.T[5][j][i]
         for j in range(USSdata.shape[1]):       #loop over waves
             dFdvDict[pfx+'U11sin:'+str(i)+':'+str(j)] = dFdGu.T[0][j][i]
             dFdvDict[pfx+'U22sin:'+str(i)+':'+str(j)] = dFdGu.T[1][j][i]
@@ -1362,6 +1363,7 @@ def SStructureFactorDerv(refDict,im,G,hfx,pfx,SGData,SSGData,calcControls,parmDi
             dFdvDict[pfx+'U13cos:'+str(i)+':'+str(j)] = 2.*dFdGu.T[10][j][i]
             dFdvDict[pfx+'U23cos:'+str(i)+':'+str(j)] = 2.*dFdGu.T[11][j][i]
             
+#    GSASIIpath.IPyBreak()
     dFdvDict[phfx+'BabA'] = dFdbab.T[0]
     dFdvDict[phfx+'BabU'] = dFdbab.T[1]
     return dFdvDict
@@ -2643,7 +2645,7 @@ def getPowderProfileDerv(parmDict,x,varylist,Histogram,Phases,rigidbodyDict,calc
                     else:
                         try:
                             aname = name.split(pfx)[1][:2]
-                            if aname not in ['Af','dA','AU','RB','Xs','Xc','Ys','Yc','Zs','Zc','U1','U2','U3']: continue # skip anything not an atom or rigid body param
+                            if aname not in ['Af','dA','AU','RB','Xs','Xc','Ys','Yc','Zs','Zc','Tm','Xm','Ym','Zm','U1','U2','U3']: continue # skip anything not an atom or rigid body param
                         except IndexError:
                             continue
                     if name in varylist:
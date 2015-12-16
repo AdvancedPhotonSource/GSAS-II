9cff343f87a8a8a49b8f0bb66275ebfede6d04f3
Barbara Frosik <bfrosik@anl.gov>
2015-12-16 21:20:26 +0000

    Refactored the reader for better performance
    git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@2095 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

--- imports/G2img_HDF5.py
+++ imports/G2img_HDF5.py
@@ -15,25 +15,22 @@ more than a single image.
 
 '''
 
-import numpy as np
+import h5py
 import GSASIIIO as G2IO
 import GSASIIpath
 GSASIIpath.SetVersionNumber("$Revision: $")
 
-class HDF5_Reader(G2IO.ImportImage):
+class Hdf5_Reader(G2IO.ImportImage):
     '''Routine to read a HD5 image, typically from APS Sector 6.
     B. Frosik/SDM. Initial version.
-        
+                   Refactored.        
     '''
     dsetlist = []
+    buffer = {}
+    init = False
 
     def __init__(self):
-        # check if HDF5 library is present, if not importer cannot be used
-        try:
-            import h5py
-        except:
-            self.UseReader = False
-            print 'HDF5 Reader skipped because h5py library not installed'
+        print 'start'
         super(self.__class__,self).__init__( # fancy way to self-reference
                                              extensionlist=('.hdf5','.hd5','.h5','.hdf'),
                                              strictExtension=True,
@@ -42,9 +39,8 @@ class HDF5_Reader(G2IO.ImportImage):
                                              )
 
     def ContentsValidator(self, filepointer):
-        '''test by using the HDF5 open
-        ''' 
-        import h5py
+        '''no test at this time
+        '''
         try:
             # the following does not work, filepointer is not a filename
             f = h5py.File(filepointer.name, 'r')
@@ -56,29 +52,45 @@ class HDF5_Reader(G2IO.ImportImage):
     def Reader(self,filename,filepointer, ParentFrame=None, **kwarg):
         '''Read using HDF5 file reader, :func:`ReadData`
         '''
-        import h5py
         imagenum = kwarg.get('blocknum')
-        if imagenum is None: imagenum = 1
-        f = None
-        try:
-            f = h5py.File(filename, 'r')
-            #print f.keys()
-            self.visit(f)
-            self.Comments,self.Data,self.Npix,self.Image,more = self.read_set(f,imagenum=imagenum)
-            self.LoadImage(ParentFrame,filename,imagenum)
-            self.repeatcount = imagenum
-            self.repeat = more
-            print('Read image #'+str(imagenum)+' from file '+filename)
-            return True
-        except IOError:
-            print 'cannot open file ', filename
+        #self.buffer = kwarg.get('buffer')
+        
+        if self.init is False:
+            self.init = True
+            self.readDataset(filename)
+
+        len = self.buffer['len']
+        if len is 1:
+            image = self.buffer['image']
+        else:
+            images = self.buffer['image']
+            image = images[imagenum-1]
+
+        self.Comments,self.Data,self.Npix,self.Image = self.read_set(image,imagenum=imagenum)
+        if self.Npix == 0 or not self.Comments:
             return False
-        finally:
-            if f is not None:
-               f.close()
-            
-    def visit(self, f):         
-        import h5py
+        self.LoadImage(ParentFrame,filename,imagenum)
+        self.repeatcount = imagenum 
+        self.repeat = imagenum < len
+        #print('Read image #'+str(imagenum)+' from file '+filename)
+        return True
+        
+    def readDataset(self,filename):
+            imagenum = 1
+            try:
+                f = h5py.File(filename, 'r')
+                self.visit(f, imagenum)
+            except IOError:
+                print 'cannot open file ', filename
+                return False
+            finally:
+                if f is not None:
+                   f.close()
+        
+         
+    def visit(self, f, imagenum):
+        #GSASIIpath.IPyBreak()   
+        print 'visiting'      
         def func(name, dset):
             datakeyword = 'data'
             if isinstance(dset, h5py.Dataset):
@@ -87,23 +99,24 @@ class HDF5_Reader(G2IO.ImportImage):
                 if lastindex is not -1:
                     index = len(dset.name)-len(datakeyword)
                     if index == lastindex:
-                        self.image = np.array(f[dset.name],dtype=np.int32)
+                        if self.buffer is not None: 
+                            if len(dset.shape) > 2:
+                                    image = dset[0,...]
+                                    self.buffer['image'] = dset[0,...]
+                                    self.buffer['len'] = image.shape[0]
+                            else:
+                                self.buffer['image'] = dset[()]
+                                self.buffer['len'] = 1
+                            self.buffer['name'] = dset.name
+                        #GSASIIpath.IPyBreak()
         f.visititems(func)
         
-    def read_set(self,f,imagenum=1):
-         more = False
+    def read_set(self,image,imagenum=1):
+         #more = False
          head = ['raw data']
-         #if multiple frames the array size is 1 x number_frames x 2048 x 2048
-         num_dim = len(self.image.shape)
-         #GSASIIpath.IPyBreak()
-         if num_dim > 2:
-             image = self.image[0,imagenum-1]
-             more = self.image.shape[1] > imagenum
-         else:
-             image = self.image
          x_dim = image.shape[0]
          y_dim = image.shape[1]
          sizexy = [x_dim,y_dim]
          Npix = sizexy[0]*sizexy[1]             
          data = {'pixelSize':[200,200],'wavelength':0.15,'distance':250.0,'center':[204.8,204.8],'size':sizexy}
-         return head,data,Npix,image,more
+         return head,data,Npix,image

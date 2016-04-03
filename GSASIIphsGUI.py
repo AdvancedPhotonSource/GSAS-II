remove selected area image multiplier - not necessary fix image scaling issues for selected area simulations fix issues with absent pydiffaxlib & DIFFaX.exe on some platforms
git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@2195 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

GSASIIphsGUI.py
@@ -2430,6 +2430,7 @@ def UpdatePhaseData(G2frame,Item,data,oldPage):
             labels = Layers['Sadp']['Plane']
             lmax = float(Layers['Sadp']['Lmax'])
             XY = 2*lmax*np.mgrid[0:256:256j,0:256:256j]/256.-lmax
+            G2frame.Cmax = 1.0
             G2plt.PlotXYZ(G2frame,XY,Layers['Sadp']['Img'].T,labelX=labels[:-1],
                 labelY=labels[-1],newPlot=False,Title=Layers['Sadp']['Plane'])
             
@@ -2924,11 +2925,10 @@ def UpdatePhaseData(G2frame,Item,data,oldPage):
         ctrls = ''
         dlg = G2gd.DIFFaXcontrols(G2frame,ctrls)
         if dlg.ShowModal() == wx.ID_OK:
-            ctrls,plane,lmax,mult,x,x,x = dlg.GetSelection()
+            ctrls,plane,lmax,x,x,x = dlg.GetSelection()
             data['Layers']['Sadp'] = {}
             data['Layers']['Sadp']['Plane'] = plane
             data['Layers']['Sadp']['Lmax'] = lmax
-            data['Layers']['Sadp']['Mult'] = mult
         else:
             return
         if ctrls == '0\n0\n3\n':    #powder pattern
@@ -2980,7 +2980,6 @@ def UpdatePhaseData(G2frame,Item,data,oldPage):
             data['Layers']['Sadp'] = {}
             data['Layers']['Sadp']['Plane'] = plane
             data['Layers']['Sadp']['Lmax'] = lmax
-            data['Layers']['Sadp']['Mult'] = mult
             data['Layers']['Multi'] = [parm,parmRange,parmStep]
         else:
             return

GSASIIpwd.py
@@ -39,7 +39,7 @@ import pypowder as pyd
 try:
     import pydiffax as pyx
 except ImportError:
-    print 'the pydiffax library could not be loaded'
+    print 'pydiffax is not available for this platform - under develpment'
 
     
 # trig functions in degrees
@@ -1915,7 +1915,10 @@ def StackSim(Layers,ctrls,HistName='',scale=0.,background={},limits=[],inst={},p
             return       
     df.close()    
     time0 = time.time()
-    subp.call(DIFFaX)
+    try:
+        subp.call(DIFFaX)
+    except OSError:
+        print 'DIFFax.exe is not available for this platform - under development'
     print 'DIFFaX time = %.2fs'%(time.time()-time0)
     if os.path.exists('GSASII-DIFFaX.spc'):
         Xpat = np.loadtxt('GSASII-DIFFaX.spc').T
@@ -1978,7 +1981,7 @@ def CalcStackingSADP(Layers):
         laueId = -1
     planeId = ['h0l','0kl','hhl','h-hl'].index(Layers['Sadp']['Plane'])+1
     lmax = int(Layers['Sadp']['Lmax'])
-    mult = int(Layers['Sadp']['Mult'])
+    mult = 1
 # Sequences
     StkType = ['recursive','explicit'].index(Layers['Stacking'][0])
     try:
@@ -2045,8 +2048,6 @@ def CalcStackingSADP(Layers):
     time0 = time.time()
     hkLim,Incr,Nblk = pyx.pygetsadp(controls,Nspec,spec)
     Sapd = np.zeros((256,256))
-    maxInt = np.max(spec[1:])
-    Scale = mult*32767./maxInt
     iB = 0
     for i in range(hkLim):
         iF = iB+Nblk
@@ -2063,8 +2064,6 @@ def CalcStackingSADP(Layers):
                 Sapd[:,p1] = spec[iB:iF]
             Sapd[:,p2] = spec[iB:iF]
         iB += Nblk
-    Sapd *= Scale
-    Sapd = np.where(Sapd<32767.,Sapd,32767.)
     Layers['Sadp']['Img'] = Sapd
     print 'GETSAD time = %.2fs'%(time.time()-time0)
 #    GSASIIpath.IPyBreak()
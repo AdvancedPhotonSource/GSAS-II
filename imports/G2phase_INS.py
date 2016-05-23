89020a06e6c21a3adb53b60e4afcec238c568d55
Robert Von Dreele <vondreele@anl.gov>
2016-05-23 16:53:59 +0000

    more changes to import Shelx INS,RES files
    git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@2287 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

--- imports/G2phase_INS.py
+++ imports/G2phase_INS.py
@@ -29,10 +29,10 @@ class PhaseReaderClass(G2IO.ImportPhase):
     'Opens a .INS file and pulls out a selected phase'
     def __init__(self):
         super(self.__class__,self).__init__( # fancy way to say ImportPhase.__init__
-            extensionlist=('.ins','.INS'),
+            extensionlist=('.ins','.INS','.res','.RES'),
             strictExtension=True,
-            formatName = 'SHELX ins',
-            longFormatName = 'SHELX input (*.ins) file import'
+            formatName = 'SHELX ins,res',
+            longFormatName = 'SHELX input (*.ins, *.res) file import'
             )
         
     def ContentsValidator(self, filepointer):
@@ -60,6 +60,13 @@ class PhaseReaderClass(G2IO.ImportPhase):
     def ReadINSPhase(self,filename,parent=None):
         '''Read a phase from a INS file.
         '''
+        Shelx = ['TITL','CELL','ZERR','LATT','SYMM','SFAC','DISP','UNIT','LAUE','EADP',
+            'MORE','TIME','END ','HKLF','OMIT','SHEL','BASF','TWIN','EXTI','SWAT',
+            'HOPE','MERG','SPEC','RESI','RTAB','MPLA','HFIX','MOVE','ANIS','AFIX',
+            'FRAG','FEND','EXYZ','EDAP','EQIV','CONN','PART','BIND','FREE','DFIX','DANG',
+            'BUMP','SAME','SADI','CHIV','FLAT','DELU','SIMU','DEFS','ISOR','NCSY',
+            'SUMP','L.S.','CGLS','BLOC','DAMP','STIR','WGHT','FVAR','BOND','CONF','MPLA',
+            'HTAB','LIST','ACTA','SIZE','TEMP','WPDB','FMAP','GRID','PLAN','MOLE']
         EightPiSq = 8.*math.pi**2
         self.errors = 'Error opening file'
         file = open(filename, 'Ur')
@@ -80,11 +87,14 @@ class PhaseReaderClass(G2IO.ImportPhase):
             self.errors = 'Error reading at line '+str(line)
             Atom = []
             Aindx = [ch in numbs for ch in S[:4]]   #False for all letters
-            if 'TITL' in S[:4]:
+            if 'TITL' in S[:4].upper():
                 Title = S[4:72].strip()
-            elif 'CELL' in S[:4]:
-                abc = S[12:40].split()
-                angles = S[40:64].split()
+            elif not S.strip():
+                pass
+            elif 'CELL' in S[:4].upper():
+                cellRec = S.split()
+                abc = cellRec[2:5]
+                angles = cellRec[5:8]
                 cell=[float(abc[0]),float(abc[1]),float(abc[2]),
                     float(angles[0]),float(angles[1]),float(angles[2])]
                 Volume = G2lat.calc_V(G2lat.cell2A(cell))
@@ -93,41 +103,45 @@ class PhaseReaderClass(G2IO.ImportPhase):
                 SGData = G2IO.SGData # P 1
                 self.warnings += '\nThe space group is not given in an ins file and has been set to "P 1".'
                 self.warnings += "\nChange this in phase's General tab; NB: it might be in the Phase name."
-            elif S[:4] in 'SFAC':
+            elif S[:4].upper() in 'SFAC':
                 aTypes = S[4:].split()
                 if 'H' in aTypes:
                     self.warnings += '\nHydrogen atoms found; consider replacing them with stereochemically tied ones.'
                     self.warnings += "\nDo 'Edit/Insert H atoms' in this phase's Atoms tab after deleting the old ones."
             elif S[0] == 'Q':
                 pass
-            elif np.any(Aindx) or S[:4].strip() in aTypes:   #this will find an atom record!
-                try:
-                    iNum = Aindx.index(True)
-                except ValueError:
-                    iNum = 4
-                Atype = S[:iNum].strip()
-                Aname = S[:4]
-                x,y,z = S[9:45].split()
+            elif '\x1a' in S[:4]:
+                pass
+            elif S[:3].upper() == 'REM':
+                pass
+            elif S[:4].upper() not in Shelx:   #this will find an atom record!
+                AtRec = S.split()
+                Atype = aTypes[int(AtRec[1])-1]
+                Aname = AtRec[0]
+                Afrac = abs(float(AtRec[5]))%10.
+                x,y,z = AtRec[2:5]
                 XYZ = np.array([float(x),float(y),float(z)])
                 XYZ = np.where(np.abs(XYZ)<0.00001,0,XYZ)
                 SytSym,Mult = G2spc.SytSym(XYZ,SGData)
                 if '=' not in S:
                     IA = 'I'
-                    Uiso = float(S[57:68])
+                    Uiso = float(AtRec[6])
                     if Uiso < 0.:
                         Uiso = 0.025
                     Uij = [0. for i in range(6)]
                 else:
                     IA = 'A'
                     Uiso = 0.
-                    Ustr = S[57:78].split()
+                    Ustr = AtRec[6:8]
                     S = file.readline()
                     if '!' in S:
                         S = S.split('!')[0]
+                    AtRec = S.split()
                     line += 1
-                    Ustr += S[6:51].split()
+                    Ustr += AtRec
                     Uij = [float(Ustr[i]) for i in range(6)]
-                Atom = [Aname,Atype,'',XYZ[0],XYZ[1],XYZ[2],1.0,SytSym,Mult,IA,Uiso]
+                    Uij = Uij[0:3]+[Uij[5],Uij[4],Uij[3]]
+                Atom = [Aname,Atype,'',XYZ[0],XYZ[1],XYZ[2],Afrac,SytSym,Mult,IA,Uiso]
                 Atom += Uij
                 Atom.append(ran.randint(0,sys.maxint))
                 Atoms.append(Atom)

protect a dlg in DoIndexPeaks fix a lost G2frame.LimitsTable - need to go back to tree instead add depVarList to testDeriv file more work on mag derivs. modify testDeriv to show depVar derivatives
git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@2500 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

GSASIIstrMath.py
@@ -1294,20 +1294,20 @@ def StructureFactorDervMag(refDict,G,hfx,pfx,SGData,calcControls,parmDict):
         #sum below is over Uniq
         dfadfr = np.sum(fam/occ,axis=2)        #array(Mxyz,refBlk,nAtom) Fdata != 0 avoids /0. problem 
         dfadx = np.sum(twopi*Uniq[nxs,:,:,nxs,:]*famx[:,:,:,:,nxs],axis=2)
-        dfadmx = np.sum(TMcorr[nxs,nxs,:,nxs,:]*cosm[nxs,nxs,:,:,:]*(dqdm[:,:,:,nxs,nxs]+Q[nxs,:,:,:,:]),axis=-2)
+        dmx = dqdm[:,:,:,nxs,nxs]*Mag[nxs,nxs,nxs,:,:]+Q[nxs,:,:,:,:]*Gdata[:,nxs,nxs,:,:]
+        dfadmx = np.sum(TMcorr[nxs,nxs,:,nxs,:]*cosm[nxs,nxs,:,:,:]*dmx,axis=-2)
         dfadmx = np.reshape(dfadmx,(3,iFin-iBeg,-1,3))
         dfadui = np.sum(-SQfactor[:,nxs,nxs]*fam,axis=2) #array(Ops,refBlk,nAtoms)
         dfadua = np.sum(-Hij[nxs,:,:,nxs,:]*fam[:,:,:,:,nxs],axis=2)
         # array(3,refBlk,nAtom,3) & array(3,refBlk,nAtom,6)
         dfbdfr = np.sum(fbm/occ,axis=2)        #array(mxyz,refBlk,nAtom) Fdata != 0 avoids /0. problem 
         dfbdx = np.sum(twopi*Uniq[nxs,:,:,nxs,:]*fbmx[:,:,:,:,nxs],axis=2)
-        dfbdmx = np.sum(TMcorr[nxs,nxs,:,nxs,:]*sinm[nxs,nxs,:,:,:]*(dqdm[:,:,:,nxs,nxs]+Q[nxs,:,:,:,:]),axis=-2)
+        dfbdmx = np.sum(TMcorr[nxs,nxs,:,nxs,:]*sinm[nxs,nxs,:,:,:]*dmx,axis=-2)
         dfbdmx = np.reshape(dfbdmx,(3,iFin-iBeg,-1,3))
         dfbdui = np.sum(-SQfactor[:,nxs,nxs]*fbm,axis=2) #array(Ops,refBlk,nAtoms)
         dfbdua = np.sum(-Hij[nxs,:,:,nxs,:]*fbm[:,:,:,:,nxs],axis=2)
         dFdfr[iBeg:iFin] = np.sum(2.*(fams[:,:,nxs]*dfadfr+fbms[:,:,nxs]*dfbdfr)*Mdata/(2*Nops*Ncen),axis=0)
         dFdx[iBeg:iFin] = np.sum(2.*(fams[:,:,nxs,nxs]*dfadx+fbms[:,:,nxs,nxs]*dfbdx),axis=0)
-#        GSASIIpath.IPyBreak()
         dFdMx[iBeg:iFin] = np.sum(2.*(fams[:,:,nxs,nxs]*dfadmx+fbms[:,:,nxs,nxs]*dfbdmx),axis=0)
         dFdui[iBeg:iFin] = 2.*np.sum(fams[:,:,nxs]*dfadui+fbms[:,:,nxs]*dfbdui,axis=0)
         dFdua[iBeg:iFin] = 2.*np.sum(fams[:,:,nxs,nxs]*dfadua+fbms[:,:,nxs,nxs]*dfbdua,axis=0)
@@ -1329,6 +1329,7 @@ def StructureFactorDervMag(refDict,G,hfx,pfx,SGData,calcControls,parmDict):
         dFdvDict[pfx+'AU12:'+str(i)] = 2.*dFdua.T[3][i]
         dFdvDict[pfx+'AU13:'+str(i)] = 2.*dFdua.T[4][i]
         dFdvDict[pfx+'AU23:'+str(i)] = 2.*dFdua.T[5][i]
+#    GSASIIpath.IPyBreak()
     return dFdvDict
     
 #def StructureFactorDervTw(refDict,G,hfx,pfx,SGData,calcControls,parmDict):
@@ -3844,10 +3845,13 @@ def getPowderProfileDerv(parmDict,x,varylist,Histogram,Phases,rigidbodyDict,calc
                         if Ka2 and iFin2-iBeg2:
                             depDerivDict[phfx+name][iBeg2:iFin2] += parmDict[phfx+'Scale']*dFdvDict[phfx+name][iref]*dervDict2['int']/refl[9+im]                  
             if not Phase['General'].get('doPawley'):
-                #do atom derivatives -  for RB,F,X & U so far              
-                corr = dervDict['int']/refl[9+im]
-                if Ka2 and iFin2-iBeg2:
-                    corr2 = dervDict2['int']/refl[9+im]
+                #do atom derivatives -  for RB,F,X & U so far
+                corr = 0.
+                corr2 = 0.
+                if refl[9+im]:             
+                    corr = dervDict['int']/refl[9+im]
+                    if Ka2 and iFin2-iBeg2:
+                        corr2 = dervDict2['int']/refl[9+im]
                 for name in varylist+dependentVars:
                     if '::RBV;' in name:
                         pass

testDeriv.py
@@ -94,18 +94,19 @@ class testDeriv(wx.Frame):
                 file = open(testFile,'rb')
                 self.values = cPickle.load(file)
                 self.HistoPhases = cPickle.load(file)
-                (self.constrDict,self.fixedList) = cPickle.load(file)
+                (self.constrDict,self.fixedList,self.depVarList) = cPickle.load(file)
                 self.parmDict = cPickle.load(file)
                 self.varylist = cPickle.load(file)
                 self.calcControls = cPickle.load(file)
                 self.pawleyLookup = cPickle.load(file)
-                self.use = [False for i in range(len(self.varylist))]
-                self.delt = [max(abs(self.parmDict[name])*0.001,1e-6) for name in self.varylist]
+                self.use = [False for i in range(len(self.varylist+self.depVarList))]
+                self.delt = [max(abs(self.parmDict[name])*0.001,1e-6) for name in self.varylist+self.depVarList]
                 file.close()
                 groups,parmlist = G2mv.GroupConstraints(self.constrDict)
                 G2mv.GenerateConstraints(groups,parmlist,self.varylist,self.constrDict,self.fixedList,self.parmDict)
                 self.UpdateControls(event)
                 print G2mv.VarRemapShow(self.varylist)
+                print 'Dependent Vary List:',self.depVarList
         finally:
             dlg.Destroy()
             
@@ -117,6 +118,7 @@ class testDeriv(wx.Frame):
             self.use[item] = Obj.GetValue()
             
         def OnDelValue(event):
+            event.Skip()
             Obj = event.GetEventObject()
             item = ObjInd[Obj.GetId()]
             try:
@@ -129,10 +131,11 @@ class testDeriv(wx.Frame):
         self.testDerivPanel.DestroyChildren()
         ObjInd = {}
         varylist = self.varylist
+        depVarList = self.depVarList
         use = self.use
         delt = self.delt
         mainSizer = wx.FlexGridSizer(0,8,5,5)
-        for id,[ck,name,d] in enumerate(zip(use,varylist,delt)):
+        for id,[ck,name,d] in enumerate(zip(use,varylist+depVarList,delt)):
             useVal = wx.CheckBox(self.testDerivPanel,label=name)
             useVal.SetValue(ck)
             ObjInd[useVal.GetId()] = id
@@ -163,28 +166,40 @@ class testDeriv(wx.Frame):
         def test2(name,delt):
             
             Title = 'derivatives test for '+name
+            varyList = self.varylist+self.depVarList
             hplot = self.plotNB.add(Title).gca()
             dMdV = G2stMth.dervRefine(self.values,self.HistoPhases,self.parmDict,
-                self.varylist,self.calcControls,self.pawleyLookup,None)
-            hplot.plot(dMdV[self.varylist.index(name)],'b',label='analytic deriv')
-            if name in self.varylist:
-                print 'parameter:',name,self.values[self.varylist.index(name)],delt
-                self.values[self.varylist.index(name)] -= delt
+                varyList,self.calcControls,self.pawleyLookup,None)
+            hplot.plot(dMdV[varyList.index(name)],'b',label='analytic deriv')
+            if name in varyList:
+                mmin = np.min(dMdV[varyList.index(name)])
+                mmax = np.max(dMdV[varyList.index(name)])
+                print 'parameter:',name,self.parmDict[name],delt,mmin,mmax
+                if name in self.varylist:
+                    self.values[self.varylist.index(name)] -= delt
+                else:
+                    self.parmDict[name] -= delt
                 M0 = G2stMth.errRefine(self.values,self.HistoPhases,self.parmDict,
-                    self.varylist,self.calcControls,self.pawleyLookup,None)
-                self.values[self.varylist.index(name)] += 2.*delt
+                    varyList,self.calcControls,self.pawleyLookup,None)
+                if name in self.varylist:
+                    self.values[self.varylist.index(name)] += 2.*delt
+                else:
+                    self.parmDict[name] += 2.*delt
                 M1 = G2stMth.errRefine(self.values,self.HistoPhases,self.parmDict,
-                    self.varylist,self.calcControls,self.pawleyLookup,None)
-                self.values[self.varylist.index(name)] -= delt    
+                    varyList,self.calcControls,self.pawleyLookup,None)
+                if name in self.varylist:
+                    self.values[self.varylist.index(name)] -= delt
+                else:
+                    self.parmDict[name] -= delt    
                 Mn = (M1-M0)/(2.*delt)
                 hplot.plot(Mn,'r',label='numeric deriv')
-                hplot.plot(dMdV[self.varylist.index(name)]-Mn,'g',label='diff')
+                hplot.plot(dMdV[varyList.index(name)]-Mn,'g',label='diff')
+#            GSASIIpath.IPyBreak()
             hplot.legend(loc='best')            
             
         while self.plotNB.nb.GetPageCount():
             self.plotNB.nb.DeletePage(0)
-        test1()
-        for use,name,delt in zip(self.use,self.varylist,self.delt):
+        for use,name,delt in zip(self.use,self.varylist+self.depVarList,self.delt):
             if use:
                 test2(name,delt)
         
@@ -203,4 +218,5 @@ def main():
     application.MainLoop()
     
 if __name__ == '__main__':
+    GSASIIpath.InvokeDebugOpts()
     main()
Speed up image integration on Mac by reducing ProgressBar calls from 3*nBlk*nBlk+3 to Nblk+3 Allow PDF setup to include chemical formula from tif macrofile fix contour bug for G(R), etc. fix copy error for masks
git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@2563 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

GSASIIimage.py
@@ -899,18 +899,18 @@ def ImageIntegrate(image,data,masks,blkSize=128,dlg=None,returnN=False):
             # next is most expensive step!
             TA,tam = Make2ThetaAzimuthMap(data,masks,(iBeg,iFin),(jBeg,jFin),times)           #2-theta & azimuth arrays & create position mask
             Nup += 1
-            if dlg:
-                pause = dlg.Update(Nup)
-                if not pause[0]: CancelPressed = True
+#            if dlg and jBlk == 0:                    #update progress bar on each row
+#                pause = dlg.Update(Nup)
+#                if not pause[0]: CancelPressed = True
             Block = image[iBeg:iFin,jBeg:jFin]
             t0 = time.time()
             tax,tay,taz,tad,tabs = Fill2ThetaAzimuthMap(masks,TA,tam,Block)    #and apply masks
             del TA; del tam
             times[2] += time.time()-t0
             Nup += 1
-            if dlg:
-                pause = dlg.Update(Nup)
-                if not pause[0]: CancelPressed = True
+#            if dlg and jBlk == 0:                    #update progress bar on each row
+#                pause = dlg.Update(Nup)
+#                if not pause[0]: CancelPressed = True
             tax = np.where(tax > LRazm[1],tax-360.,tax)                 #put azm inside limits if possible
             tax = np.where(tax < LRazm[0],tax+360.,tax)
             if data.get('SampleAbs',[0.0,''])[1]:
@@ -931,7 +931,7 @@ def ImageIntegrate(image,data,masks,blkSize=128,dlg=None,returnN=False):
             times[3] += time.time()-t0
             Nup += 1
             del tax; del tay; del taz; del tad; del tabs
-            if dlg:
+            if dlg and jBlk == 0:                    #update progress bar on each row
                 pause = dlg.Update(Nup)
                 if not pause[0]: CancelPressed = True
     t0 = time.time()

GSASIIplot.py
@@ -2356,7 +2356,7 @@ def PlotISFG(G2frame,newPlot=False,type=''):
             G2frame.Contour = not G2frame.Contour
             if G2frame.Contour:
                 G2frame.SinglePlot = False
-                Pattern[0]['Offset'] = [0.,0.]
+                Page.Offset = [0.,0.]
         elif event.key == 's':
             if G2frame.Contour:
                 choice = [m for m in mpl.cm.datad.keys() if not m.endswith("_r")]
@@ -2452,8 +2452,6 @@ def PlotISFG(G2frame,newPlot=False,type=''):
                     
     PDFdata = G2frame.PatternTree.GetItemPyData(G2gd.GetPatternTreeItemId(G2frame,PatternId, 'PDF Controls'))
     numbDen = G2pwd.GetNumDensity(PDFdata['ElList'],PDFdata['Form Vol'])
-    Xb = [0.,10.]
-    Yb = [0.,-40.*np.pi*numbDen]
     Ymax = 0.01
     lenX = 0
     for Pattern in PlotList:
@@ -2487,6 +2485,8 @@ def PlotISFG(G2frame,newPlot=False,type=''):
             else:
                 Plot.plot(X,Y,colors[N%6],picker=False)
             if type == 'G(R)':
+                Xb = [0.,10.]
+                Yb = [0.,-40.*np.pi*numbDen]
                 Plot.plot(Xb,Yb,color='k',dashes=(5,5))
             elif type == 'F(Q)':
                 Plot.axhline(0.,color=wx.BLACK)
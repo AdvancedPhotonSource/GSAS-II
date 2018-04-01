fix column averaging in seq results table
git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@3331 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

GSASIIdataGUI.py
@@ -5764,7 +5764,7 @@ def UpdateSeqResults(G2frame,data,prevSize=None):
     def OnAveSelSeq(event):
         'average the selected columns from menu command'
         cols = sorted(G2frame.dataDisplay.GetSelectedCols()) # ignore selection order
-        useCol =  not np.array(G2frame.SeqTable.GetColValues(0),dtype=bool)
+        useCol =  ~np.array(G2frame.SeqTable.GetColValues(0),dtype=bool)
         if cols:
             for col in cols:
                 items = GetColumnInfo(col)[1]
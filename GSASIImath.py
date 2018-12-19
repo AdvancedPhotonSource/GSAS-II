name fix for single crystal 1D plots remove ZigZag & Block from modulation derivatives - now done numerically fix ZigZag modulation function & proper limits for Tmin & Tmax in GUI
git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@3768 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

GSASIImath.py
@@ -1413,6 +1413,7 @@ def ModulationTw(H,HP,nWaves,Fmod,Xmod,Umod,glTau,glWt):
     
 def makeWavesDerv(ngl,waveTypes,FSSdata,XSSdata,USSdata,MSSdata,Mast):
     '''
+    Only for Fourier waves for fraction, position & adp (probably not used for magnetism)
     FSSdata: array 2 x atoms x waves    (sin,cos terms)
     XSSdata: array 2x3 x atoms X waves (sin,cos terms)
     USSdata: array 2x6 x atoms X waves (sin,cos terms)
@@ -1435,16 +1436,16 @@ def makeWavesDerv(ngl,waveTypes,FSSdata,XSSdata,USSdata,MSSdata,Mast):
     ZtauXx = np.zeros((Ax.shape[0],3,ngl))               #atoms x XYZmax x ngl
     for iatm in range(Ax.shape[0]):
         nx = 0
-        if 'ZigZag' in waveTypes[iatm]:
-            nx = 1
-            Tmm = Ax[iatm][0][:2]                        
-            XYZmax = np.array([Ax[iatm][0][2],Bx[iatm][0][0],Bx[iatm][0][1]])            
-            ZtauXt[iatm],ZtauXx[iatm] = posZigZagDerv(glTau,Tmm,XYZmax)
-        elif 'Block' in waveTypes[iatm]:
-            nx = 1
-            Tmm = Ax[iatm][0][:2]                        
-            XYZmax = np.array([Ax[iatm][0][2],Bx[iatm][0][0],Bx[iatm][0][1]])            
-            ZtauXt[iatm],ZtauXx[iatm] = posBlockDerv(glTau,Tmm,XYZmax)
+#        if 'ZigZag' in waveTypes[iatm]:
+#            nx = 1
+#            Tmm = Ax[iatm][0][:2]                        
+#            XYZmax = np.array([Ax[iatm][0][2],Bx[iatm][0][0],Bx[iatm][0][1]])            
+#            ZtauXt[iatm],ZtauXx[iatm] = posZigZagDerv(glTau,Tmm,XYZmax)
+#        elif 'Block' in waveTypes[iatm]:
+#            nx = 1
+#            Tmm = Ax[iatm][0][:2]                        
+#            XYZmax = np.array([Ax[iatm][0][2],Bx[iatm][0][0],Bx[iatm][0][1]])            
+#            ZtauXt[iatm],ZtauXx[iatm] = posBlockDerv(glTau,Tmm,XYZmax)
         tauX = np.arange(1.,nWaves[1]+1-nx)[:,nxs]*glTau  #Xwaves x ngl
         if nx:    
             StauX[iatm][:-nx] = np.ones_like(Ax)[iatm,nx:,:,nxs]*np.sin(twopi*tauX)[nxs,:,nxs,:]   #atoms X waves X 3(xyz) X ngl
@@ -1452,7 +1453,6 @@ def makeWavesDerv(ngl,waveTypes,FSSdata,XSSdata,USSdata,MSSdata,Mast):
         else:
             StauX[iatm] = np.ones_like(Ax)[iatm,:,:,nxs]*np.sin(twopi*tauX)[nxs,:,nxs,:]   #atoms X waves X 3(xyz) X ngl
             CtauX[iatm] = np.ones_like(Bx)[iatm,:,:,nxs]*np.cos(twopi*tauX)[nxs,:,nxs,:]   #ditto
-#    GSASIIpath.IPyBreak()
     if nWaves[0]:
         tauF = np.arange(1.,nWaves[0]+1)[:,nxs]*glTau  #Fwaves x ngl
         StauF = np.ones_like(Af)[:,:,nxs]*np.sin(twopi*tauF)[nxs,:,:] #also dFmod/dAf
@@ -1608,30 +1608,30 @@ def posZigZag(T,Tmm,Xmax):
     DT = Tmm[1]-Tmm[0]
     Su = 2.*Xmax/DT
     Sd = 2.*Xmax/(1.-DT)
-    A = np.array([np.where(Tmm[0] < t%1. <= Tmm[1],-Xmax+Su*((t-Tmm[0])%1.),Xmax-Sd*((t-Tmm[1])%1.)) for t in T])
+    A = np.array([np.where(  0.< (t-Tmm[0])%1. <= DT, -Xmax+Su*((t-Tmm[0])%1.), Xmax-Sd*((t-Tmm[1])%1.)) for t in T])
     return A
     
-def posZigZagDerv(T,Tmm,Xmax):
-    DT = Tmm[1]-Tmm[0]
-    Su = 2.*Xmax/DT
-    Sd = 2.*Xmax/(1.-DT)
-    dAdT = np.zeros((2,3,len(T)))
-    dAdT[0] = np.array([np.where(Tmm[0] < t <= Tmm[1],Su*(t-Tmm[0]-1)/DT,-Sd*(t-Tmm[1])/(1.-DT)) for t in T]).T
-    dAdT[1] = np.array([np.where(Tmm[0] < t <= Tmm[1],-Su*(t-Tmm[0])/DT,Sd*(t-Tmm[1])/(1.-DT)) for t in T]).T
-    dAdX = np.ones(3)[:,nxs]*np.array([np.where(Tmm[0] < t%1. <= Tmm[1],-1.+2.*(t-Tmm[0])/DT,1.-2.*(t-Tmm[1])%1./DT) for t in T])
-    return dAdT,dAdX
+#def posZigZagDerv(T,Tmm,Xmax):
+#    DT = Tmm[1]-Tmm[0]
+#    Su = 2.*Xmax/DT
+#    Sd = 2.*Xmax/(1.-DT)
+#    dAdT = np.zeros((2,3,len(T)))
+#    dAdT[0] = np.array([np.where(Tmm[0] < t <= Tmm[1],Su*(t-Tmm[0]-1)/DT,-Sd*(t-Tmm[1])/(1.-DT)) for t in T]).T
+#    dAdT[1] = np.array([np.where(Tmm[0] < t <= Tmm[1],-Su*(t-Tmm[0])/DT,Sd*(t-Tmm[1])/(1.-DT)) for t in T]).T
+#    dAdX = np.ones(3)[:,nxs]*np.array([np.where(Tmm[0] < t%1. <= Tmm[1],-1.+2.*(t-Tmm[0])/DT,1.-2.*(t-Tmm[1])%1./DT) for t in T])
+#    return dAdT,dAdX
 
 def posBlock(T,Tmm,Xmax):
     A = np.array([np.where(Tmm[0] < t%1. <= Tmm[1],-Xmax,Xmax) for t in T])
     return A
     
-def posBlockDerv(T,Tmm,Xmax):
-    dAdT = np.zeros((2,3,len(T)))
-    ind = np.searchsorted(T,Tmm)
-    dAdT[0,:,ind[0]] = -Xmax/len(T)
-    dAdT[1,:,ind[1]] = Xmax/len(T)
-    dAdX = np.ones(3)[:,nxs]*np.array([np.where(Tmm[0] < t <= Tmm[1],-1.,1.) for t in T])  #OK
-    return dAdT,dAdX
+#def posBlockDerv(T,Tmm,Xmax):
+#    dAdT = np.zeros((2,3,len(T)))
+#    ind = np.searchsorted(T,Tmm)
+#    dAdT[0,:,ind[0]] = -Xmax/len(T)
+#    dAdT[1,:,ind[1]] = Xmax/len(T)
+#    dAdX = np.ones(3)[:,nxs]*np.array([np.where(Tmm[0] < t <= Tmm[1],-1.,1.) for t in T])  #OK
+#    return dAdT,dAdX
     
 def fracCrenel(tau,Toff,Twid):
     Tau = (tau-Toff)%1.

GSASIIphsGUI.py
@@ -5020,7 +5020,7 @@ def UpdatePhaseData(G2frame,Item,data):
                     item,iwave = Indx[Obj.GetId()]
                     del atm[-1]['SS1'][item][iwave+1]
                     if len(atm[-1]['SS1'][item]) == 1:
-                        atm[-1]['SS1'][item][0] = ''
+                        atm[-1]['SS1'][item][0] = 'Fourier'
                     wx.CallAfter(RepaintAtomInfo,G2frame.waveData.GetScrollPos(wx.VERTICAL))
                     
                 def OnWavePlot(invalid,value,tc):
@@ -5087,7 +5087,11 @@ def UpdatePhaseData(G2frame,Item,data):
                             val = wave[0][ival]
                             if np.any(CSI[0][ival]):
                                 minmax = [-0.2,0.2]
-                                if waveTyp in ['ZigZag','Block'] and ival < 2: minmax = [0.,2.]
+                                if waveTyp in ['ZigZag','Block'] and not iwave and ival < 2:
+                                    if not ival:
+                                        minmax = [0.,2.]
+                                    else:
+                                        minmax = [wave[0][0],1.0+wave[0][0]]
                                 waveVal = G2G.ValidatedTxtCtrl(waveData,wave[0],ival,nDig=(10,5),min=minmax[0],max=minmax[1],OnLeave=OnWavePlot)
                             else:
                                 waveVal = wx.TextCtrl(waveData,value='%.5f'%(val),style=wx.TE_READONLY)

GSASIIplot.py
@@ -1077,6 +1077,7 @@ def Plot1DSngl(G2frame,newPlot=False,hklRef=None,Super=0,Title=False):
         to F, F**2, etc. as requested
     '''
     global xylim,X
+    Name = G2frame.GPXtree.GetItemText(G2frame.PatternId)
     def OnKeyPress(event):
         if event.key == 'q':
             Page.qaxis = not Page.qaxis
@@ -1116,7 +1117,7 @@ def Plot1DSngl(G2frame,newPlot=False,hklRef=None,Super=0,Title=False):
             try:
                 G2frame.G2plotNB.status.SetStatusText('d =%9.3f F^2 =%9.3f'%(Xpos,ypos),1)                   
             except TypeError:
-                G2frame.G2plotNB.status.SetStatusText('Select '+Title+' pattern first',1)
+                G2frame.G2plotNB.status.SetStatusText('Select '+Title+Name+' pattern first',1)
             Page.SetToolTipString(s)
                 
     def Draw():
@@ -1171,7 +1172,7 @@ def Plot1DSngl(G2frame,newPlot=False,hklRef=None,Super=0,Title=False):
         else:
             Page.canvas.draw()
 
-    new,plotNum,Page,Plot,lim = G2frame.G2plotNB.FindPlotTab(Title,'mpl')
+    new,plotNum,Page,Plot,lim = G2frame.G2plotNB.FindPlotTab(Title+Name,'mpl')
     Page.Offset = [0,0]
     if not new:
         if not newPlot:
@@ -5678,7 +5679,7 @@ def ModulationPlot(G2frame,data,atom,ax,off=0):
         for i,spos in enumerate(Spos[1:]):
             if waveType in ['ZigZag','Block'] and not i:
                 Tminmax = spos[0][:2]
-                XYZmax = np.array(spos[0][2:])
+                XYZmax = np.array(spos[0][2:5])
                 if waveType == 'Block':
                     wave = G2mth.posBlock(tau,Tminmax,XYZmax).T
                 elif waveType == 'ZigZag':
@@ -5686,7 +5687,7 @@ def ModulationPlot(G2frame,data,atom,ax,off=0):
             else:
                 scof.append(spos[0][:3])
                 ccof.append(spos[0][3:])
-        wave += G2mth.posFourier(tau,np.array(scof),np.array(ccof))
+                wave += G2mth.posFourier(tau,np.array(scof),np.array(ccof))
     if mapData['Flip']:
         Title = 'Charge flip'
     else:

GSASIIspc.py
@@ -2587,7 +2587,7 @@ def GetSSfxuinel(waveType,Stype,nH,XYZ,SGData,SSGData,debug=False):
         DT = Tmm[1]-Tmm[0]
         slopeUp = 2.*XYZmax/DT
         slopeDn = 2.*XYZmax/(1.-DT)
-        A = np.array([np.where(Tmm[0] < t%1. <= Tmm[1],-XYZmax+slopeUp*((t-Tmm[0])%1.),XYZmax-slopeDn*((t-Tmm[1])%1.)) for t in tau])
+        A = np.array([np.where(0. < t-(Tmm[0])%1. <= DT,-XYZmax+slopeUp*((t-Tmm[0])%1.),XYZmax-slopeDn*((t-Tmm[1])%1.)) for t in tau])
         return A
 
     def posBlock(tau,Tmm,XYZmax):
@@ -2653,7 +2653,6 @@ def GetSSfxuinel(waveType,Stype,nH,XYZ,SGData,SSGData,debug=False):
         return CSI,dF,dFTP
         
     def DoXYZ():
-        delt4 = np.ones(4)*0.001
         delt5 = np.ones(5)*0.001
         delt6 = np.eye(6)*0.001
         if 'Fourier' in waveType:
fix super indexing problem in transposeHKLF fix reflection generation for incommensurate mag case in G2lattice & G2pwd clean up non Fourier modulation calcs & remove analytic derivative stuff (now numeric) fix uij derivative bug work on incommensurate magnetic sturcture factors - not working yet clean up of testDeriv - better choices for delt & force reflection regeneration before each derivative test
git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@3774 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

GSASIIlattice.py
@@ -1343,6 +1343,9 @@ def GenPfHKLs(nMax,SGData,A):
 
 def GenSSHLaue(dmin,SGData,SSGData,Vec,maxH,A):
     'needs a doc string'
+    ifMag = False
+    if 'MagSpGrp' in SGData:
+        ifMag = True
     HKLs = []
     vec = np.array(Vec)
     vstar = np.sqrt(calc_rDsq(vec,A))     #find extra needed for -n SS reflections
@@ -1361,7 +1364,7 @@ def GenSSHLaue(dmin,SGData,SSGData,Vec,maxH,A):
                 d = 1./np.sqrt(calc_rDsq(H,A))
                 if d >= dmin:
                     HKLM = np.array([h,k,l,dH])
-                    if G2spc.checkSSLaue([h,k,l,dH],SGData,SSGData) and G2spc.checkSSextc(HKLM,SSGData):
+                    if (G2spc.checkSSLaue([h,k,l,dH],SGData,SSGData) and G2spc.checkSSextc(HKLM,SSGData)) or ifMag:
                         HKLs.append([h,k,l,dH,d])    
     return HKLs
     

GSASIIphsGUI.py
@@ -1341,6 +1341,8 @@ def UpdatePhaseData(G2frame,Item,data):
                                     wType = waveType
                             atom[-1]['SS1'][parm] = [wType,]+list(atom[-1]['SS1'][parm])
                     del atom[-1]['SS1']['waveType']
+        else:
+            generalData['Super'] = 0
         if 'Modulated' not in generalData:
             generalData['Modulated'] = False
         if 'HydIds' not in generalData:
@@ -1596,7 +1598,7 @@ def UpdatePhaseData(G2frame,Item,data):
                                 generalData['SuperSg'] = SetDefaultSSsymbol()
                             generalData['SSGData'] = G2spc.SSpcGroup(generalData['SGData'],generalData['SuperSg'])[1]
                             if 'SuperVec' not in generalData:
-                                generalData['Super'] = 1
+                                generalData['Super'] = True
                                 generalData['SuperVec'] = [[0.,0.,0.],False,4]
                                 generalData['SSGData'] = {}
                             if '4DmapData' not in generalData:
@@ -5088,6 +5090,8 @@ def UpdatePhaseData(G2frame,Item,data):
                             val = wave[0][ival]
                             if np.any(CSI[0][ival]):
                                 minmax = [-0.2,0.2]
+                                if Stype == 'Smag':
+                                    minmax = [-20.,20.]
                                 if waveTyp in ['ZigZag','Block','Crenel'] and not iwave and ival < 2:
                                     if not ival:
                                         minmax = [0.,2.]

GSASIIpwd.py
@@ -1063,6 +1063,9 @@ def getHKLMpeak(dmin,Inst,SGData,SSGData,Vec,maxH,A):
     HKL = G2lat.GenHLaue(dvec,SGData,A)        
     SSdH = [vec*h for h in range(-maxH,maxH+1)]
     SSdH = dict(zip(range(-maxH,maxH+1),SSdH))
+    ifMag = False
+    if 'MagSpGrp' in SGData:
+        ifMag = True
     for h,k,l,d in HKL:
         ext = G2spc.GenHKLf([h,k,l],SGData)[0]
         if not ext and d >= dmin:
@@ -1074,7 +1077,7 @@ def getHKLMpeak(dmin,Inst,SGData,SSGData,Vec,maxH,A):
                 d = float(1/np.sqrt(G2lat.calc_rDsq(H,A)))
                 if d >= dmin:
                     HKLM = np.array([h,k,l,dH])
-                    if G2spc.checkSSextc(HKLM,SSGData):
+                    if G2spc.checkSSextc(HKLM,SSGData) or ifMag:
                         HKLs.append([h,k,l,dH,d,G2lat.Dsp2pos(Inst,d),-1])    
     return G2lat.sortHKLd(HKLs,True,True,True)
 

GSASIIstrMath.py
@@ -849,8 +849,8 @@ def StructureFactorDerv2(refDict,G,hfx,pfx,SGData,calcControls,parmDict):
         occ = Mdata*Fdata/len(SGT)
         biso = -SQfactor*Uisodata[:,nxs]
         Tiso = np.repeat(np.where(biso<1.,np.exp(biso),1.0),len(SGT),axis=1).T
-        HbH = np.sum(Uniq.T*np.swapaxes(np.inner(bij,Uniq),2,-1),axis=1)
-        Tuij = np.where(HbH<1.,np.exp(-HbH),1.0).T
+        HbH = -np.sum(Uniq.T*np.swapaxes(np.inner(bij,Uniq),2,-1),axis=1)
+        Tuij = np.where(HbH<1.,np.exp(HbH),1.0).T
         Tcorr = np.reshape(Tiso,Tuij.shape)*Tuij*Mdata*Fdata/len(SGMT)
         Hij = np.array([Mast*np.multiply.outer(U,U) for U in np.reshape(Uniq,(-1,3))])      #Nref*Nops,3,3
         Hij = np.reshape(np.array([G2lat.UijtoU6(uij) for uij in Hij]),(-1,len(SGT),6))     #Nref,Nops,6
@@ -1496,7 +1496,9 @@ def SStructureFactor(refDict,G,hfx,pfx,SGData,SSGData,calcControls,parmDict):
                 FP,FPP = G2el.BlenResTOF(Tdata,BLtables,refl.T[12])
             FP = np.repeat(FP.T,Uniq.shape[1],axis=0)
             FPP = np.repeat(FPP.T,Uniq.shape[1],axis=0)
-        Bab = np.repeat(parmDict[phfx+'BabA']*np.exp(-parmDict[phfx+'BabU']*SQfactor),Uniq.shape[1])
+        Bab = 0.
+        if phfx+'BabA' in parmDict:
+            Bab = np.repeat(parmDict[phfx+'BabA']*np.exp(-parmDict[phfx+'BabU']*SQfactor),Uniq.shape[1])
         Tindx = np.array([refDict['FF']['El'].index(El) for El in Tdata])
         FF = np.repeat(refDict['FF']['FF'][iBeg:iFin].T[Tindx].T,Uniq.shape[1],axis=0)
         phase = twopi*(np.inner(Uniq[:,:,:3],(dXdata.T+Xdata.T))-Phi[:,:,nxs])
@@ -1507,6 +1509,9 @@ def SStructureFactor(refDict,G,hfx,pfx,SGData,SSGData,calcControls,parmDict):
         HbH = -np.sum(UniqP[:,:,nxs,:]*np.inner(UniqP[:,:,:],bij),axis=-1)  #use hklt proj to hkl
         Tuij = np.where(HbH<1.,np.exp(HbH),1.0)
         Tcorr = np.reshape(Tiso,Tuij.shape)*Tuij*Mdata*Fdata/Uniq.shape[1]  #refBlk x ops x atoms
+        GfpuA = G2mth.Modulation(Uniq,UniqP,nWaves,Fmod,Xmod,Umod,Mmod,glTau,glWt) #2 x refBlk x sym X atoms
+        fams = np.zeros(32)
+        fbms = np.zeros(32)
 
         if 'N' in calcControls[hfx+'histType'] and parmDict[pfx+'isMag']:       #TODO: mag math here??
             MF = refDict['FF']['MF'][iBeg:iFin].T[Tindx].T   #Nref,Natm
@@ -1515,42 +1520,36 @@ def SStructureFactor(refDict,G,hfx,pfx,SGData,SSGData,calcControls,parmDict):
                 mphase = np.hstack((phase,-phase))
             else:
                 mphase = phase 
-            mphase = np.array([mphase+twopi*np.inner(cen,H)[:,nxs,nxs] for cen in SGData['SGCen']])
+            mphase = np.array([mphase+twopi*np.inner(cen,HP.T)[:,nxs,nxs] for cen in SGData['SGCen']])
             mphase = np.concatenate(mphase,axis=1)              #Nref,full Nop,Natm
             sinm = np.sin(mphase)                               #ditto - match magstrfc.for
             cosm = np.cos(mphase)                               #ditto
-            HM = np.inner(Bmat,H)                             #put into cartesian space
+            HM = np.inner(Bmat,HP.T)                             #put into cartesian space
             HM = HM/np.sqrt(np.sum(HM**2,axis=0))               #Gdata = MAGS & HM = UVEC in magstrfc.for both OK
             eDotK = np.sum(HM[:,:,nxs,nxs]*Gdata[:,nxs,:,:],axis=0)
             Q = HM[:,:,nxs,nxs]*eDotK[nxs,:,:,:]-Gdata[:,nxs,:,:] #xyz,Nref,Nop,Natm = BPM in magstrfc.for OK
             fam = Q*TMcorr[nxs,:,nxs,:]*cosm[nxs,:,:,:]*Mag[nxs,nxs,:,:]    #ditto
             fbm = Q*TMcorr[nxs,:,nxs,:]*sinm[nxs,:,:,:]*Mag[nxs,nxs,:,:]    #ditto
-            fams = np.sum(np.sum(fam,axis=-1),axis=-1)                          #xyz,Nref
-            fbms = np.sum(np.sum(fbm,axis=-1),axis=-1)                          #ditto
-            refl.T[9] = np.sum(fams**2,axis=0)+np.sum(fbms**2,axis=0)
-            refl.T[7] = np.copy(refl.T[9])                
-            refl.T[10] = 0.0    #atan2d(fbs[0],fas[0]) - what is phase for mag refl?
+            fagm = fam*GfpuA[0]-fbm*GfpuA[1]   #real; 2 x refBlk x sym x atoms
+            fbgm = fbm*GfpuA[0]+fam*GfpuA[1]
+            fams = np.sum(np.sum(fagm,axis=-1),axis=-1)                          #xyz,Nref
+            fbms = np.sum(np.sum(fbgm,axis=-1),axis=-1)                          #ditto
 
 
+        if 'T' in calcControls[hfx+'histType']:
+            fa = np.array([np.reshape(((FF+FP).T-Bab).T,cosp.shape)*cosp*Tcorr,-np.reshape(Flack*FPP,sinp.shape)*sinp*Tcorr])
+            fb = np.array([np.reshape(Flack*FPP,cosp.shape)*cosp*Tcorr,np.reshape(((FF+FP).T-Bab).T,sinp.shape)*sinp*Tcorr])
         else:
-            if 'T' in calcControls[hfx+'histType']:
-                fa = np.array([np.reshape(((FF+FP).T-Bab).T,cosp.shape)*cosp*Tcorr,-np.reshape(Flack*FPP,sinp.shape)*sinp*Tcorr])
-                fb = np.array([np.reshape(Flack*FPP,cosp.shape)*cosp*Tcorr,np.reshape(((FF+FP).T-Bab).T,sinp.shape)*sinp*Tcorr])
-            else:
-                fa = np.array([np.reshape(((FF+FP).T-Bab).T,cosp.shape)*cosp*Tcorr,-Flack*FPP*sinp*Tcorr])
-                fb = np.array([Flack*FPP*cosp*Tcorr,np.reshape(((FF+FP).T-Bab).T,sinp.shape)*sinp*Tcorr])
-            GfpuA = G2mth.Modulation(Uniq,UniqP,nWaves,Fmod,Xmod,Umod,Mmod,glTau,glWt) #2 x refBlk x sym X atoms
-            fag = fa*GfpuA[0]-fb*GfpuA[1]   #real; 2 x refBlk x sym x atoms
-            fbg = fb*GfpuA[0]+fa*GfpuA[1]
-            fas = np.sum(np.sum(fag,axis=-1),axis=-1)   #2 x refBlk; sum sym & atoms
-            fbs = np.sum(np.sum(fbg,axis=-1),axis=-1)
-            if 'P' in calcControls[hfx+'histType']:
-                refl.T[10] = np.sum(fas,axis=0)**2+np.sum(fbs,axis=0)**2    #square of sums
-                refl.T[11] = atan2d(fbs[0],fas[0])  #ignore f' & f"
-            else:
-                refl.T[10] = np.sum(fas,axis=0)**2+np.sum(fbs,axis=0)**2    #square of sums
-                refl.T[8] = np.copy(refl.T[10])                
-                refl.T[11] = atan2d(fbs[0],fas[0])  #ignore f' & f"
+            fa = np.array([np.reshape(((FF+FP).T-Bab).T,cosp.shape)*cosp*Tcorr,-Flack*FPP*sinp*Tcorr])
+            fb = np.array([Flack*FPP*cosp*Tcorr,np.reshape(((FF+FP).T-Bab).T,sinp.shape)*sinp*Tcorr])
+        fag = fa*GfpuA[0]-fb*GfpuA[1]   #real; 2 x refBlk x sym x atoms
+        fbg = fb*GfpuA[0]+fa*GfpuA[1]
+        fas = np.sum(np.sum(fag,axis=-1),axis=-1)   #2 x refBlk; sum sym & atoms
+        fbs = np.sum(np.sum(fbg,axis=-1),axis=-1)
+        refl.T[10] = np.sum(fas,axis=0)**2+np.sum(fbs,axis=0)**2+np.sum(fams,axis=0)**2+np.sum(fbms,axis=0)**2    #square of sums
+        refl.T[11] = atan2d(fbs[0],fas[0])  #ignore f' & f"
+        if 'P' not in calcControls[hfx+'histType']:
+            refl.T[8] = np.copy(refl.T[10])                
         iBeg += blkSize
 #    print ('nRef %d time %.4f\r'%(nRef,time.time()-time0))
     return copy.deepcopy(refDict['RefList'])
@@ -1772,8 +1771,10 @@ def SStructureFactorDerv(refDict,im,G,hfx,pfx,SGData,SSGData,calcControls,parmDi
         HP = H[:3]+modQ*H[3:]            #projected hklm to hkl
         SQ = 1./(2.*refl[4+im])**2             # or (sin(theta)/lambda)**2
         SQfactor = 8.0*SQ*np.pi**2
-        dBabdA = np.exp(-parmDict[phfx+'BabU']*SQfactor)
-        Bab = parmDict[phfx+'BabA']*dBabdA
+        Bab = 0.0
+        if phfx+'BabA' in parmDict:
+            dBabdA = np.exp(-parmDict[phfx+'BabU']*SQfactor)
+            Bab = parmDict[phfx+'BabA']*dBabdA
         Tindx = np.array([refDict['FF']['El'].index(El) for El in Tdata])
         FF = refDict['FF']['FF'][iref].T[Tindx]
         Uniq = np.inner(H,SSGMT)
@@ -1863,8 +1864,9 @@ def SStructureFactorDerv(refDict,im,G,hfx,pfx,SGData,SSGData,calcControls,parmDi
             dFdGf[iref] = 2.*(SA*dfadGf[0]+SB*dfbdGf[1])      #array(nRef,natom,nwave,2)
             dFdGx[iref] = 2.*(SA*dfadGx[0]+SB*dfbdGx[1])      #array(nRef,natom,nwave,6)
             dFdGu[iref] = 2.*(SA*dfadGu[0]+SB*dfbdGu[1])      #array(nRef,natom,nwave,12)
-        dFdbab[iref] = 2.*fas[0]*np.array([np.sum(dfadba*dBabdA),np.sum(-dfadba*parmDict[phfx+'BabA']*SQfactor*dBabdA)]).T+ \
-            2.*fbs[0]*np.array([np.sum(dfbdba*dBabdA),np.sum(-dfbdba*parmDict[phfx+'BabA']*SQfactor*dBabdA)]).T
+        if phfx+'BabA' in parmDict:
+            dFdbab[iref] = 2.*fas[0]*np.array([np.sum(dfadba*dBabdA),np.sum(-dfadba*parmDict[phfx+'BabA']*SQfactor*dBabdA)]).T+ \
+                2.*fbs[0]*np.array([np.sum(dfbdba*dBabdA),np.sum(-dfbdba*parmDict[phfx+'BabA']*SQfactor*dBabdA)]).T
         #loop over atoms - each dict entry is list of derivatives for all the reflections
         if not iref%100 :
             print (' %d derivative time %.4f\r'%(iref,time.time()-time0),end='')

testDeriv.py
@@ -43,8 +43,8 @@ ptx.pyqlmninit()            #initialize fortran arrays for spherical harmonics
 def create(parent):
     return testDeriv(parent)
     
-[wxID_FILEEXIT, wxID_FILEOPEN, wxID_MAKEPLOTS,
-] = [wx.NewId() for _init_coll_File_Items in range(3)]
+[wxID_FILEEXIT, wxID_FILEOPEN, wxID_MAKEPLOTS, wxID_CLEARSEL,
+] = [wx.NewId() for _init_coll_File_Items in range(4)]
 
 def FileDlgFixExt(dlg,file):            #this is needed to fix a problem in linux wx.FileDialog
     ext = dlg.GetWildcard().split('|')[2*dlg.GetFilterIndex()+1].strip('*')
@@ -61,10 +61,12 @@ class testDeriv(wx.Frame):
         self.File = wx.Menu(title='')
         self.File.Append(wxID_FILEOPEN,'Open testDeriv file','Open testDeriv')
         self.File.Append(wxID_MAKEPLOTS,'Make plots','Make derivative plots')
+        self.File.Append(wxID_CLEARSEL,'Clear selections')
         self.File.Append(wxID_FILEEXIT,'Exit','Exit from testDeriv')
-        self.Bind(wx.EVT_MENU, self.OnTestRead, id=wxID_FILEOPEN)
+        self.Bind(wx.EVT_MENU,self.OnTestRead, id=wxID_FILEOPEN)
         self.Bind(wx.EVT_MENU,self.OnMakePlots,id=wxID_MAKEPLOTS)
-        self.Bind(wx.EVT_MENU, self.OnFileExit, id=wxID_FILEEXIT)
+        self.Bind(wx.EVT_MENU,self.ClearSelect,id=wxID_CLEARSEL)
+        self.Bind(wx.EVT_MENU,self.OnFileExit, id=wxID_FILEEXIT)
         self.testDerivMenu.Append(menu=self.File, title='File')
         self.SetMenuBar(self.testDerivMenu)
         self.testDerivPanel = wx.ScrolledWindow(self)        
@@ -94,6 +96,10 @@ class testDeriv(wx.Frame):
             self.dataFrame.Clear() 
             self.dataFrame.Destroy()
         self.Close()
+        
+    def ClearSelect(self,event):
+        self.use = [False for i in range(len(self.names))]
+        self.UpdateControls(event)
 
     def OnTestRead(self,event):
         dlg = wx.FileDialog(self, 'Open *.testDeriv file',defaultFile='*.testDeriv',
@@ -127,8 +133,12 @@ class testDeriv(wx.Frame):
             self.varylist = cPickle.load(file,encoding='Latin-1')
             self.calcControls = cPickle.load(file,encoding='Latin-1')
             self.pawleyLookup = cPickle.load(file,encoding='Latin-1')
-        self.use = [False for i in range(len(self.varylist+self.depVarList))]
-        self.delt = [max(abs(self.parmDict[name])*0.0001,1e-6) for name in self.varylist+self.depVarList]
+        self.names = self.varylist+self.depVarList
+        self.use = [False for i in range(len(self.names))]
+        self.delt = [max(abs(self.parmDict[name])*0.0001,1e-6) for name in self.names]
+        for iname,name in enumerate(self.names):
+            if name.split(':')[-1] in ['Shift','DisplaceX','DisplaceY',]:
+                self.delt[iname] = 0.1
         file.close()
         msg = G2mv.EvaluateMultipliers(self.constrDict,self.parmDict)
         if msg:
@@ -158,12 +168,11 @@ class testDeriv(wx.Frame):
         
         self.testDerivPanel.DestroyChildren()
         ObjInd = {}
-        varylist = self.varylist
-        depVarList = self.depVarList
+        names = self.names
         use = self.use
         delt = self.delt
         mainSizer = wx.FlexGridSizer(0,8,5,5)
-        for id,[ck,name,d] in enumerate(zip(use,varylist+depVarList,delt)):
+        for id,[ck,name,d] in enumerate(zip(use,names,delt)):
             useVal = wx.CheckBox(self.testDerivPanel,label=name)
             useVal.SetValue(ck)
             ObjInd[useVal.GetId()] = id
@@ -201,13 +210,17 @@ class testDeriv(wx.Frame):
             
         def test2(name,delt,doProfile):
             Title = 'derivatives test for '+name
-            varyList = self.varylist+self.depVarList
+            names = self.names
             hplot = self.plotNB.add(Title).gca()
             if doProfile:
                 pr = cProfile.Profile()
                 pr.enable()
+            #regenerate minimization fxn
+            G2stMth.errRefine(self.values,self.HistoPhases,
+                self.parmDict,self.varylist,self.calcControls,
+                self.pawleyLookup,None)
             dMdV = G2stMth.dervRefine(self.values,self.HistoPhases,self.parmDict,
-                varyList,self.calcControls,self.pawleyLookup,None)
+                names,self.calcControls,self.pawleyLookup,None)
             if doProfile:
                 pr.disable()
                 s = StringIO.StringIO()
@@ -216,18 +229,18 @@ class testDeriv(wx.Frame):
                 ps.print_stats("GSASII",.5)
                 print('Profiler of '+name+' derivative calculation; top 50% of routines:')
                 print(s.getvalue())
-            M2 = dMdV[varyList.index(name)]
+            M2 = dMdV[names.index(name)]
             hplot.plot(M2,'b',label='analytic deriv')
-            mmin = np.min(dMdV[varyList.index(name)])
-            mmax = np.max(dMdV[varyList.index(name)])
+            mmin = np.min(dMdV[names.index(name)])
+            mmax = np.max(dMdV[names.index(name)])
             print('parameter:',name,self.parmDict[name],delt,mmin,mmax)
             if name in self.varylist:
                 self.values[self.varylist.index(name)] -= delt
                 M0 = G2stMth.errRefine(self.values,self.HistoPhases,self.parmDict,
-                    varyList,self.calcControls,self.pawleyLookup,None)
+                    names,self.calcControls,self.pawleyLookup,None)
                 self.values[self.varylist.index(name)] += 2.*delt
                 M1 = G2stMth.errRefine(self.values,self.HistoPhases,self.parmDict,
-                    varyList,self.calcControls,self.pawleyLookup,None)
+                    names,self.calcControls,self.pawleyLookup,None)
                 self.values[self.varylist.index(name)] -= delt
             elif name in self.depVarList:   #in depVarList
                 if 'dA' in name:
@@ -235,10 +248,10 @@ class testDeriv(wx.Frame):
                     delt *= -1
                 self.parmDict[name] -= delt
                 M0 = G2stMth.errRefine(self.values,self.HistoPhases,self.parmDict,
-                    varyList,self.calcControls,self.pawleyLookup,None)
+                    names,self.calcControls,self.pawleyLookup,None)
                 self.parmDict[name] += 2.*delt
                 M1 = G2stMth.errRefine(self.values,self.HistoPhases,self.parmDict,
-                    varyList,self.calcControls,self.pawleyLookup,None)
+                    names,self.calcControls,self.pawleyLookup,None)
                 self.parmDict[name] -= delt    
             Mn = (M1-M0)/(2.*abs(delt))
             hplot.plot(Mn,'r',label='numeric deriv')
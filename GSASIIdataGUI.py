correct calculation of FWHM for TOF data & fix the alp values in the exported peaks table a fix for incommensurate mag str fctr calcs
git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@3778 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

GSASIIdataGUI.py
@@ -4007,9 +4007,9 @@ class GSASII(wx.Frame):
                                 sig = np.sqrt(peak[8])
                                 gam = peak[10]
                                 esddsp = G2lat.Pos2dsp(Inst,esds['pos'])
-                                FWHM = G2pwd.getgamFW(gam,sig)      #to get delta-TOF from Gam(peak)
+                                FWHM = G2pwd.getgamFW(gam,sig) +(peak[4]+peak[6])*np.log(2.)/(peak[4]*peak[6])     #to get delta-TOF from Gam(peak)
                                 file.write("%10.2f %10.5f %10.5f %12.2f %10.3f %10.3f %10.3f %10.3f %10.3f\n" % \
-                                    (peak[0],dsp,esddsp,peak[2],np.sqrt(max(0.0001,peak[4])),peak[6],peak[8],peak[10],FWHM))
+                                    (peak[0],dsp,esddsp,peak[2],peak[4],peak[6],peak[8],peak[10],FWHM))
                             else:               #CW
                                 #get esds from sigDict for each peak & put in output - esds for sig & gam from UVWXY?
                                 esds = {'pos':0.,'int':0.,'sig':0.,'gam':0.}

GSASIIpwd.py
@@ -652,7 +652,7 @@ def getWidthsTOF(pos,alp,bet,sig,gam):
     return widths,fmin,fmax
     
 def getFWHM(pos,Inst):
-    '''Compute total FWHM from Thompson, Cox & Hastings (1987), J. Appl. Cryst. 20, 79-83
+    '''Compute total FWHM from Thompson, Cox & Hastings (1987) , J. Appl. Cryst. 20, 79-83
     via getgamFW(g,s).
     
     :param pos: float peak position in deg 2-theta or tof in musec
@@ -665,15 +665,19 @@ def getFWHM(pos,Inst):
     sigTOF = lambda dsp,S0,S1,S2,Sq: np.sqrt(S0+S1*dsp**2+S2*dsp**4+Sq*dsp)
     gam = lambda Th,X,Y,Z: Z+X/cosd(Th)+Y*tand(Th)
     gamTOF = lambda dsp,X,Y,Z: Z+X*dsp+Y*dsp**2
+    alpTOF = lambda dsp,alp: alp/dsp
+    betTOF = lambda dsp,bet0,bet1,betq: bet0+bet1/dsp**4+betq/dsp**2
     if 'C' in Inst['Type'][0]:
         s = sig(pos/2.,Inst['U'][1],Inst['V'][1],Inst['W'][1])
         g = gam(pos/2.,Inst['X'][1],Inst['Y'][1],Inst['Z'][1])
         return getgamFW(g,s)/100.  #returns FWHM in deg
     else:
         dsp = pos/Inst['difC'][0]
+        alp = alpTOF(dsp,Inst['alpha'][0])
+        bet = betTOF(dsp,Inst['beta-0'][0],Inst['beta-1'][0],Inst['beta-q'][0])
         s = sigTOF(dsp,Inst['sig-0'][1],Inst['sig-1'][1],Inst['sig-2'][1],Inst['sig-q'][1])
         g = gamTOF(dsp,Inst['X'][1],Inst['Y'][1],Inst['Z'][1])
-        return getgamFW(g,s)
+        return getgamFW(g,s)+np.log(2.0)*(alp+bet)/(alp*bet)
     
 def getgamFW(g,s):
     '''Compute total FWHM from Thompson, Cox & Hastings (1987), J. Appl. Cryst. 20, 79-83

GSASIIstrMath.py
@@ -1488,7 +1488,7 @@ def SStructureFactor(refDict,G,hfx,pfx,SGData,SSGData,calcControls,parmDict):
         Uniq = np.inner(H.T,SSGMT)
         UniqP = np.inner(HP.T,SGMT)
         Phi = np.inner(H.T,SSGT)
-        if SGInv:   #if centro - expand HKL sets
+        if SGInv and not SGData['SGFixed']:   #if centro - expand HKL sets
             Uniq = np.hstack((Uniq,-Uniq))
             Phi = np.hstack((Phi,-Phi))
             UniqP = np.hstack((UniqP,-UniqP))
@@ -1519,11 +1519,7 @@ def SStructureFactor(refDict,G,hfx,pfx,SGData,SSGData,calcControls,parmDict):
         if 'N' in calcControls[hfx+'histType'] and parmDict[pfx+'isMag']:       #TODO: mag math here??
             MF = refDict['FF']['MF'][iBeg:iFin].T[Tindx].T   #Nref,Natm
             TMcorr = 0.539*(np.reshape(Tiso,Tuij.shape)*Tuij)[:,0,:]*Fdata*Mdata*MF/(2*Nops)     #Nref,Natm
-            if SGData['SGInv'] and not SGData['SGFixed']:
-                mphase = np.hstack((phase,-phase))
-            else:
-                mphase = phase 
-            mphase = np.array([mphase+twopi*np.inner(cen,HP.T)[:,nxs,nxs] for cen in SGData['SGCen']])
+            mphase = np.array([phase+twopi*np.inner(cen,HP.T)[:,nxs,nxs] for cen in SGData['SGCen']])
             mphase = np.concatenate(mphase,axis=1)              #Nref,full Nop,Natm
             sinm = np.sin(mphase)                               #ditto - match magstrfc.for
             cosm = np.cos(mphase)                               #ditto
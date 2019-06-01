f2593f99b70e667d5207756ad99752a9ecd63012
Robert Von Dreele <vondreele@anl.gov>
2019-06-01 15:36:04 +0000

    reinstate texture model as possible with single histograms - skip the "Kaduk" test
    git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@4012 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

--- GSASIIphsGUI.py
+++ GSASIIphsGUI.py
@@ -6753,22 +6753,23 @@ def UpdatePhaseData(G2frame,Item,data):
             # of any type with differing setting angles from the 1st.
             instArray = {}
             if int(Obj.GetValue()) > 0:
-                for h in data['Histograms']:
-                    PatternId = G2gd.GetGPXtreeItemId(G2frame,G2frame.root,h)
-                    Inst = G2frame.GPXtree.GetItemPyData(G2gd.GetGPXtreeItemId(G2frame,PatternId,'Instrument Parameters'))[0]
-                    Sample = G2frame.GPXtree.GetItemPyData(G2gd.GetGPXtreeItemId(G2frame,PatternId,'Sample Parameters'))
-                    if Inst['Type'][1] in instArray:
-                        if instArray[Inst['Type'][1]] != [Sample['Chi'],Sample['Phi'],Sample['Omega']]:
-                            textureData['Order'] = int(Obj.GetValue())
-                            break
+                if textureData['Model'] != 'cylindrical':
+                    for h in data['Histograms']:
+                        PatternId = G2gd.GetGPXtreeItemId(G2frame,G2frame.root,h)
+                        Inst = G2frame.GPXtree.GetItemPyData(G2gd.GetGPXtreeItemId(G2frame,PatternId,'Instrument Parameters'))[0]
+                        Sample = G2frame.GPXtree.GetItemPyData(G2gd.GetGPXtreeItemId(G2frame,PatternId,'Sample Parameters'))
+                        if Inst['Type'][1] in instArray:
+                            if instArray[Inst['Type'][1]] != [Sample['Chi'],Sample['Phi'],Sample['Omega']]:
+                                textureData['Order'] = int(Obj.GetValue())
+                                break
+                        else:
+                            instArray[Inst['Type'][1]] = [Sample['Chi'],Sample['Phi'],Sample['Omega']]
                     else:
-                        instArray[Inst['Type'][1]] = [Sample['Chi'],Sample['Phi'],Sample['Omega']]
+                        textureData['Order'] = 0
+                        wx.MessageBox('Incorrect use of Texture. Use preferred orientation (on data tab) unless you have multiple histograms taken with different orientations',
+                                caption='Texture Error',style=wx.ICON_EXCLAMATION)
                 else:
                     textureData['Order'] = 0
-                    wx.MessageBox('Incorrect use of Texture. Use preferred orientation (on data tab) unless you have multiple histograms taken with different orientations',
-                            caption='Texture Error',style=wx.ICON_EXCLAMATION)
-            else:
-                textureData['Order'] = 0
             textureData['SH Coeff'][1] = SetSHCoef()
             wx.CallLater(100,UpdateTexture)
             wx.CallAfter(G2plt.PlotTexture,G2frame,data)

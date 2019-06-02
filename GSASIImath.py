fix print bug in fourier map calculation
git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@4014 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

GSASIImath.py
@@ -3255,7 +3255,7 @@ def Fourier4DMap(data,reflDict):
     mapData['rho'] = np.real(rho)
     mapData['rhoMax'] = max(np.max(mapData['rho']),-np.min(mapData['rho']))
     mapData['minmax'] = [np.max(mapData['rho']),np.min(mapData['rho'])]
-    print ('Fourier map time: %.4f no. elements: %d dimensions: %s'%(time.time()-time0,Fhkl.sizestr(Fhkl.shape)))
+    print ('Fourier map time: %.4f no. elements: %d dimensions: %s'%(time.time()-time0,Fhkl.size,str(Fhkl.shape)))
 
 # map printing for testing purposes
 def printRho(SGLaue,rho,rhoMax):                          
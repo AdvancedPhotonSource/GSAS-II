9f4e4dc4483189dbb577f958b174bd686a0c5cf2
Robert Von Dreele <vondreele@anl.gov>
2020-03-06 07:30:53 +0000

    git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@4344 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

--- GSASIIstrMath.py
+++ GSASIIstrMath.py
@@ -1607,8 +1607,8 @@ def SStructureFactor(refDict,G,hfx,pfx,SGData,SSGData,calcControls,parmDict):
             MF = refDict['FF']['MF'][iBeg:iFin].T[Tindx].T   #Nref,Natm
             TMcorr = 0.539*(np.reshape(Tiso,Tuij.shape)*Tuij)[:,0,:]*Mdata*Fdata*MF/(2*Nops)     #Nref,Natm
                       
-            HM = np.inner(uBmat,HP.T)                            #put into cartesian space X||H,Z||H*L
-            eM = (HM/np.sqrt(np.sum(HM**2,axis=0))).T               # normalize  HP  Nref,hkl=Unit vectors || Q
+            HM = np.inner(uBmat.T,HP.T)                            #put into cartesian space X||H,Z||H*L
+            eM = (HM/np.sqrt(np.sum(HM**2,axis=0))).T              # normalize  HP  Nref,hkl=Unit vectors || Q
 #for fixed moments --> m=0 reflections 
             fam0 = 0.
             fbm0 = 0.

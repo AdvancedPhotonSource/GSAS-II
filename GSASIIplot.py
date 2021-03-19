Add new button to Covariance page - Show last shift/esd bar plot Add PlotNamedFloatBarGraph to G2plot to do it. Fix a missing default on get in ReportProblems in G2strMain
git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@4857 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

GSASIIplot.py
@@ -5704,7 +5704,8 @@ def PlotStrain(G2frame,data,newPlot=False):
         
 ##### PlotBarGraph ################################################################################
 def PlotBarGraph(G2frame,Xarray,Xname='',Yname='Number',Title='',PlotName=None,ifBinned=False,maxBins=None):
-    'Needs a description'
+    ''' does a vertical bar graph
+    '''
     
     def OnPageChanged(event):
         PlotText = G2frame.G2plotNB.nb.GetPageText(G2frame.G2plotNB.nb.GetSelection())
@@ -5745,6 +5746,42 @@ def PlotBarGraph(G2frame,Xarray,Xname='',Yname='Number',Title='',PlotName=None,i
     Plot.bar(Dbins,Bins,width=wid,align='edge',facecolor='red',edgecolor='black')
     Page.canvas.draw()
 
+##### PlotNamedFloatBarGraph ################################################################################
+def PlotNamedFloatHBarGraph(G2frame,Xvals,Ynames,Xlabel='Value',Ylabel='',Title='',PlotName=None):
+    ''' does a horizintal bar graph
+    '''
+    
+    def OnPageChanged(event):
+        PlotText = G2frame.G2plotNB.nb.GetPageText(G2frame.G2plotNB.nb.GetSelection())
+        if PlotText == PlotName:
+            PlotNamedFloatHBarGraph(G2frame,Xvals,Ynames,Xlabel,Ylabel,Title,PlotText)
+    
+    def OnMotion(event):
+        if event.ydata is None:
+            return
+        ypos = int(event.ydata+.5)
+        if ypos and ypos < len(Ynames):                                        #avoid out of frame mouse position
+            SetCursor(Page)
+            try:
+                G2frame.G2plotNB.status.SetStatusText('X =%s %s = %9.3g'%(Ynames[ypos],Xlabel,Xvals[ypos]),1)                   
+            except TypeError:
+                G2frame.G2plotNB.status.SetStatusText('Select %s first'%PlotName,1)
+
+    if PlotName is None or not len(Ynames):
+        return
+    new,plotNum,Page,Plot,lim = G2frame.G2plotNB.FindPlotTab(PlotName,'mpl')
+    if new:
+        G2frame.G2plotNB.Bind(wx.aui.EVT_AUINOTEBOOK_PAGE_CHANGED,OnPageChanged)
+        Page.canvas.mpl_connect('motion_notify_event', OnMotion)
+    Yvals = np.arange(len(Ynames))
+    colors = ['blue' if x >= 0. else 'red' for x in Xvals]
+    Page.Choice = None
+    Plot.set_title(Title)
+    Plot.set_xlabel(Xlabel,fontsize=14)
+    Plot.set_ylabel(Ylabel,fontsize=14)
+    Plot.barh(Yvals,Xvals,height=0.8,align='center',color=colors,edgecolor='black',tick_label=Ynames)
+    Page.canvas.draw()
+    
 ##### PlotSASDSizeDist ################################################################################
 def PlotSASDSizeDist(G2frame):
     'Needs a description'
@@ -6774,6 +6811,7 @@ def PlotCovariance(G2frame,Data):
                           vmin=-1.,vmax=1.)
         imgAx = Img.axes
         ytics = imgAx.get_yticks()
+        imgAx.set_yticks(ytics[:-1])
         ylabs = [Page.varyList[int(i)] for i in ytics[:-1]]
         imgAx.set_yticklabels(ylabs)
         Page.figure.colorbar(Img)

GSASIIstrMain.py
@@ -81,7 +81,7 @@ def ReportProblems(result,Rvals,varyList):
                 else:
                     msg += ', {}'.format(varyList[val])
         if m: G2fil.G2Print(m, mode='warn')
-    SVD0 = result[2].get('SVD0')
+    SVD0 = result[2].get('SVD0',0)
     if SVD0 == 1: 
         msg += 'Warning: Soft (SVD) singularity in the Hessian'
     elif SVD0 > 0: 
@@ -252,15 +252,10 @@ def RefineCore(Controls,Histograms,Phases,restraintDict,rigidbodyDict,parmDict,v
         try:
             covMatrix = result[1]*Rvals['GOF']**2
             sig = np.sqrt(np.diag(covMatrix))
-            Lastshft = result[2].get('Xvec',None)
-            if Lastshft is None:
-                Rvals['Max shft/sig'] = None
-            else:
-                Rvals['Max shft/sig'] = np.max(np.nan_to_num(Lastshft/sig))
+            Lastshft = result[0]-values
+            Rvals['Max shft/sig'] = np.max(np.nan_to_num(Lastshft/sig))
             if np.any(np.isnan(sig)) or not sig.shape:
                 G2fil.G2Print ('*** Least squares aborted - some invalid esds possible ***',mode='error')
-#            table = dict(zip(varyList,zip(values,result[0],(result[0]-values)/sig)))
-#            for item in table: print item,table[item]               #useful debug - are things shifting?
             # report on refinement issues. Result in Rvals['msg']
             ReportProblems(result,Rvals,varyList)
             break                   #refinement succeeded - finish up!
@@ -303,7 +298,7 @@ def RefineCore(Controls,Histograms,Phases,restraintDict,rigidbodyDict,parmDict,v
         G2stMth.GetFobsSq(Histograms,Phases,parmDict,calcControls)
     if chisq0 is not None:
         Rvals['GOF0'] = np.sqrt(chisq0/(Histograms['Nobs']-len(varyList)))
-    return IfOK,Rvals,result,covMatrix,sig
+    return IfOK,Rvals,result,covMatrix,sig,Lastshft
 
 def Refine(GPXfile,dlg=None,makeBack=True,refPlotUpdate=None):
     '''Global refinement -- refines to minimize against all histograms. 
@@ -397,7 +392,7 @@ def Refine(GPXfile,dlg=None,makeBack=True,refPlotUpdate=None):
     Rvals = {}
     try:
         covData = {}
-        IfOK,Rvals,result,covMatrix,sig = RefineCore(Controls,Histograms,Phases,restraintDict,
+        IfOK,Rvals,result,covMatrix,sig,Lastshft = RefineCore(Controls,Histograms,Phases,restraintDict,
             rigidbodyDict,parmDict,varyList,calcControls,pawleyLookup,ifSeq,printFile,dlg,
             refPlotUpdate=refPlotUpdate)
         if IfOK:
@@ -405,7 +400,7 @@ def Refine(GPXfile,dlg=None,makeBack=True,refPlotUpdate=None):
             newCellDict = G2stMth.GetNewCellParms(parmDict,varyList)
             newAtomDict = G2stMth.ApplyXYZshifts(parmDict,varyList)
             covData = {'variables':result[0],'varyList':varyList,'sig':sig,'Rvals':Rvals,
-                       'varyListStart':varyListStart,
+                       'varyListStart':varyListStart,'Lastshft':Lastshft,
                        'covMatrix':covMatrix,'title':GPXfile,'newAtomDict':newAtomDict,
                        'newCellDict':newCellDict,'freshCOV':True}
             # add the uncertainties into the esd dictionary (sigDict)
@@ -817,7 +812,7 @@ def SeqRefine(GPXfile,dlg,refPlotUpdate=None):
                ' The following refined variables have previously been frozen due to exceeding limits:\n\t{}\n'
                .format(s))
         try:
-            IfOK,Rvals,result,covMatrix,sig = RefineCore(Controls,Histo,Phases,restraintDict,
+            IfOK,Rvals,result,covMatrix,sig,Lastshft = RefineCore(Controls,Histo,Phases,restraintDict,
                 rigidbodyDict,parmDict,varyList,calcControls,pawleyLookup,ifSeq,printFile,dlg,
                 refPlotUpdate=refPlotUpdate)
             try:
@@ -859,7 +854,7 @@ def SeqRefine(GPXfile,dlg,refPlotUpdate=None):
             newAtomDict = copy.deepcopy(G2stMth.ApplyXYZshifts(parmDict,varyList))
             histRefData = {
                 'variables':result[0],'varyList':varyList,'sig':sig,'Rvals':Rvals,
-                'varyListStart':varyListStart,
+                'varyListStart':varyListStart,'Lastshft':Lastshft,
                 'covMatrix':covMatrix,'title':histogram,'newAtomDict':newAtomDict,
                 'newCellDict':newCellDict,'depParmDict':depParmDict,
                 'constraintInfo':constraintInfo,
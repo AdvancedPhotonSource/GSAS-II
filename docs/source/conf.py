5c95a1285d4818f8f8ff2e55490ffa3397cd7a49
Brian Toby <toby@anl.gov>
2023-05-08 23:20:18 +0000

    more sphinx work
    git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@5570 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -155,8 +155,6 @@ html_css_files = [
     'theme_overrides.css',
 ]
 
-
-
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
 #html_last_updated_fmt = '%b %d, %Y'

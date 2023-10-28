ac4b1c7c0a37fdd3d67dd58a7922691718fca52e
Robert Von Dreele <vondreele@anl.gov>
2023-10-28 23:09:16 +0000

    Do image manipulations in GetImageZ as float32; use np.rint to convert to int32
    git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@5688 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

--- GSASIIimgGUI.py
+++ GSASIIimgGUI.py
@@ -82,7 +82,7 @@ def GetImageZ(G2frame,data,newRange=False):
     Npix,imagefile,imagetag = G2IO.GetCheckImageFile(G2frame,G2frame.Image)
     if imagefile is None: return []
     formatName = data.get('formatName','')
-    sumImg = np.array(G2IO.GetImageData(G2frame,imagefile,True,ImageTag=imagetag,FormatName=formatName),dtype='int32')
+    sumImg = np.array(G2IO.GetImageData(G2frame,imagefile,True,ImageTag=imagetag,FormatName=formatName),dtype='float32')
     if sumImg is None:
         return []
     darkImg = False
@@ -95,9 +95,9 @@ def GetImageZ(G2frame,data,newRange=False):
                 dformatName = Ddata.get('formatName','')
                 Npix,darkfile,imagetag = G2IO.GetCheckImageFile(G2frame,Did)
 #                darkImage = G2IO.GetImageData(G2frame,darkfile,True,ImageTag=imagetag,FormatName=dformatName)
-                darkImage = np.array(G2IO.GetImageData(G2frame,darkfile,True,ImageTag=imagetag,FormatName=dformatName),dtype='int32')
+                darkImage = np.array(G2IO.GetImageData(G2frame,darkfile,True,ImageTag=imagetag,FormatName=dformatName),dtype='float32')
                 if darkImg is not None:                
-                    sumImg += np.array(darkImage*darkScale,dtype='int32')
+                    sumImg += np.array(darkImage*darkScale,dtype='float32')
             else:
                 print('Warning: resetting dark image (not found: {})'.format(
                     darkImg))
@@ -110,11 +110,11 @@ def GetImageZ(G2frame,data,newRange=False):
                 Npix,backfile,imagetag = G2IO.GetCheckImageFile(G2frame,Bid)
                 Bdata = G2frame.GPXtree.GetItemPyData(G2gd.GetGPXtreeItemId(G2frame,Bid,'Image Controls'))
                 bformatName = Bdata.get('formatName','')
-                backImage = np.array(G2IO.GetImageData(G2frame,backfile,True,ImageTag=imagetag,FormatName=bformatName),dtype='int32')
+                backImage = np.array(G2IO.GetImageData(G2frame,backfile,True,ImageTag=imagetag,FormatName=bformatName),dtype='float32')
                 if darkImg and backImage is not None:
-                    backImage += np.array(darkImage*darkScale/backScale,dtype='int32')
+                    backImage += np.array(darkImage*darkScale/backScale,dtype='float32')
                 if backImage is not None:
-                    sumImg += np.array(backImage*backScale,dtype='int32')
+                    sumImg += np.array(backImage*backScale,dtype='float32')
     if 'Gain map' in data:
         gainMap = data['Gain map']
         if gainMap:
@@ -130,7 +130,7 @@ def GetImageZ(G2frame,data,newRange=False):
     Imin = np.min(sumImg)
     if 'range' not in data or newRange:
         data['range'] = [(Imin,Imax),[Imin,Imax]]
-    return np.asarray(sumImg,dtype='int32')
+    return np.asarray(np.rint(sumImg),dtype='int32')
 
 def UpdateImageData(G2frame,data):
     

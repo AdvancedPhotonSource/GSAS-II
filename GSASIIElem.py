correct sign for b" (should be + not -)
git-svn-id: https://subversion.xray.aps.anl.gov/pyGSAS/trunk@5740 2b3fe19a-dc8f-cc80-9b5f-a4fb0fcdb5be

GSASIIElem.py
@@ -615,7 +615,7 @@ def BlenResCW(Els,BLtables,wave):
             D1 = T1**2+gam**2/4.
             D2 = T2**2+gam**2/4.
             FP[i] = Re*(T0/D0+A*T1/D1+B*T2/D2)+BL['BW-LS'][0]
-            FPP[i] = -Im*(1/D0+A/D1+B/D2)
+            FPP[i] = Im*(1/D0+A/D1+B/D2)
         else:
             FPP[i] = BL['SL'][1]    #for Li, B, etc.
     return FP,FPP
@@ -637,7 +637,7 @@ def BlenResTOF(Els,BLtables,wave):
             D1 = T1**2+gam**2/4.
             D2 = T2**2+gam**2/4.
             FP[i] = Re*(T0/D0+A*T1/D1+B*T2/D2)+BL[i]['BW-LS'][0]
-            FPP[i] = -Im*(1/D0+A/D1+B/D2)
+            FPP[i] = Im*(1/D0+A/D1+B/D2)
         else:
             FPP[i] = np.ones(len(wave))*BL[i]['SL'][1]    #for Li, B, etc.
     return FP,FPP
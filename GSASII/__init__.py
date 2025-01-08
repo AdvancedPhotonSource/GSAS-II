2f25679945f3e1d921a5d3b05b77739008adde18
Thomas A Caswell <tcaswell@bnl.gov>
2025-01-08 00:54:07 -0500

    ENH: add checker to validate sys.path is not mutated

---
+++ GSASII/__init__.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+sys.path.append('.')
+
+
+def make_path_watcher():
+    import sys
+    import traceback
+
+    init_path = tuple(sys.path)
+    def no_path_hacking(event_name, args):
+        if event_name != 'import':
+            return
+        module, filename, path, meta_path, path_hooks = args
+        if path is not None and tuple(path) != init_path:
+            for line in traceback.format_stack():
+                print(line.strip())
+            print(set(path) - set(init_path))
+            sys.exit(1)
+    return no_path_hacking
+
+if not os.environ.get("GSASII_YOLO_PATH", ''):
+    sys.addaudithook(make_path_watcher())
+
+del sys, make_path_watcher, os

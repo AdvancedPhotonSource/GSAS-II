a9dfcd3d197752ccfbf4e54b6583c18cba2886b4
BHT <toby@anl.gov>
2025-02-10 07:17:25 -0600

    self-test path fixes

--- tests/test_elm.py
+++ tests/test_elm.py
@@ -1,5 +1,17 @@
+import os
+import sys
+import importlib.util
+if importlib.util.find_spec('GSASII') is None: # hack path if GSASII not installed into Python
+    home = os.path.dirname(__file__)
+    sys.path.append(os.path.dirname(home))
+
 from GSASII import GSASIIElem
 
 def test_get_xsection():
     xsection = GSASIIElem.GetXsectionCoeff('Fe')
     assert len(xsection) > 0
+
+if __name__ == '__main__':
+    # run self-tests
+    test_get_xsection()
+    print ("OK")
--- tests/test_lattice.py
+++ tests/test_lattice.py
@@ -5,7 +5,7 @@ import os
 import sys
 import numpy as np
 
-import importlib
+import importlib.util
 if importlib.util.find_spec('GSASII') is None: # hack path if GSASII not installed into Python
     home = os.path.dirname(__file__)
     sys.path.append(os.path.dirname(home))

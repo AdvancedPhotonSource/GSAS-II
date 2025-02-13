0eaa58f770a7ad129fbc8217041fa07fc6b7840c
BHT <toby@anl.gov>
2025-02-12 18:26:41 -0600

    minor cleanup

--- GSASII/imports/G2img_CheMin.py
+++ GSASII/imports/G2img_CheMin.py
@@ -5,7 +5,7 @@
 from __future__ import division, print_function
 from .. import GSASIIobj as G2obj
 from .. import GSASIIpath
-from .. import GSASIIfil as G2fil
+from .. import GSASIIfiles as G2fil
 try:
     import imageio
 except ImportError:

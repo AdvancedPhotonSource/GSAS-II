2872d9710f1c72032bbc71d15d13c8e4f5816ba5
BHT <toby@anl.gov>
2025-02-18 17:33:02 -0600

    Python 3.13 is fine

--- GSASII/GSASIIdataGUI.py
+++ GSASII/GSASIIdataGUI.py
@@ -361,7 +361,7 @@ versionDict['badVersionWarn'] = {'numpy':['1.16.0'],
 'versions of modules that are known to have bugs'
 versionDict['tooNewWarn'] = {} 
 'module versions newer than what we have tested & where problems are suspected'
-versionDict['tooNewUntested'] = {'Python':'3.12','wx': '4.2.3'}
+versionDict['tooNewUntested'] = {'Python':'3.14','wx': '4.2.3'}
 'module versions newer than what we have tested but no problems are suspected'
 
 def ShowVersions():

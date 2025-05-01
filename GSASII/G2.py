AdvancedPhotonSource/GSAS-II

commit 8ec4d386946014c0b195c9e3e6305097cf0928f3

LeBail & Pawley: Pawley turns of LeBail; Reset LeBail intensities asked only once with better message; LeBail turns off PO, extinction, Babinet; addresses #43 & #47

GSASII/GSASIIdataGUI.py
@@ -5450,11 +5450,11 @@ If you continue from this point, it is quite likely that all intensity computati
             return
         Controls = self.GPXtree.GetItemPyData(GetGPXtreeItemId(self,self.root, 'Controls'))
         if Controls.get('newLeBail',False):
-            dlgtxt = '''Do Le Bail refinement of intensities first?
+            dlgtxt = '''Reset Le Bail structure factors?
             
-    If Yes, resets starting structure factors; recommended after major parameter changes.
-    If No, then previous structure factors are used.'''
-            dlgb = wx.MessageDialog(self,dlgtxt,'Le Bail Refinement',style=wx.YES_NO)            
+    Yes: all structure factors are reset to start at unity; Le Bail-only fitting will be used before any least-squares cycles.\n
+    No: least-squares starts with previously set structure factors.'''
+            dlgb = wx.MessageDialog(self,dlgtxt,'Le Bail Mode',style=wx.YES_NO)
             result = wx.ID_NO
             try:
                 result = dlgb.ShowModal()
@@ -5478,7 +5478,7 @@ If you continue from this point, it is quite likely that all intensity computati
         else:
             refPlotUpdate = None
         try:
-            OK,Rvals = G2stMn.Refine(self.GSASprojectfile,dlg,refPlotUpdate=refPlotUpdate,newLeBail=Controls.get('newLeBail',False))
+            OK,Rvals = G2stMn.Refine(self.GSASprojectfile,dlg,refPlotUpdate=refPlotUpdate)
         finally:
             dlg.Update(101.) # forces the Auto_Hide; needed after move w/Win & wx3.0
             dlg.Destroy()
@@ -5528,7 +5528,10 @@ If you continue from this point, it is quite likely that all intensity computati
                 dlg.Destroy()
         else:
             self.ErrorDialog('Refinement error',Rvals['msg'])
-            
+        # a fit has been done, no need to reset intensities again
+        Controls = self.GPXtree.GetItemPyData(GetGPXtreeItemId(self,self.root, 'Controls'))
+        Controls['newLeBail'] = False
+
     def OnLeBail(self,event):
         '''Do a 1 cycle LeBail refinement with no other variables; usually done upon initialization of a LeBail refinement
         either single or sequentially
@@ -5544,7 +5547,6 @@ If you continue from this point, it is quite likely that all intensity computati
             rChi2initial = '?'
         
         if GSASIIpath.GetConfigValue('G2RefinementWindow'):            
-#            Controls = self.GPXtree.GetItemPyData(GetGPXtreeItemId(self,self.root, 'Controls'))
             if (self.testSeqRefineMode()):
                 l = len(self.testSeqRefineMode())
             else:
@@ -5577,7 +5579,7 @@ If you continue from this point, it is quite likely that all intensity computati
                 text += txt
                 rtext += txt
             text += '\nLoad new result & continue refinement?'
-            dlg2 = wx.MessageDialog(self,text,'LeBail fit: Rwp={:.3f}'.format(Rwp),wx.OK|wx.CANCEL)
+            dlg2 = wx.MessageDialog(self,text,'LeBail-only fit: Rwp={:.3f}'.format(Rwp),wx.OK|wx.CANCEL)
             dlg2.CenterOnParent()
             try:
                 if dlg2.ShowModal() == wx.ID_OK:
@@ -5656,7 +5658,7 @@ If you continue from this point, it is quite likely that all intensity computati
         self.GPXtree.SaveExposedItems()             # save the exposed/hidden tree items
         
         try:
-            OK,Rvals = G2stMn.Refine(self.GSASprojectfile,dlg,refPlotUpdate=None,newLeBail=False)
+            OK,Rvals = G2stMn.Refine(self.GSASprojectfile,dlg,refPlotUpdate=None)
         except Exception as msg:
             print('Refinement failed with message',msg)
             Controls['deriv type'] = saveDervtype

GSASII/GSASIIphsGUI.py
@@ -2369,6 +2369,20 @@ def UpdatePhaseData(G2frame,Item,data):
                        
         def PawleySizer():
             # find d-space range in used histograms
+            def enablePawley(*args):
+                for c in PawleyCtrlsList:
+                    c.Enable(generalData['doPawley'])
+                # If Pawley is on, turn off Le Bail settings (since they will
+                # be hidden)
+                if generalData['doPawley']:
+                    Controls = G2frame.GPXtree.GetItemPyData(G2gd.GetGPXtreeItemId(G2frame,G2frame.GPXtree.root, 'Controls'))
+                    Controls['newLeBail'] = False
+                    for h in data['Histograms']:
+                        data['Histograms'][h]['LeBail'] = False
+                    G2G.G2MessageBox(G2frame,title='Note:',
+                            msg='Use Pawley Create in Operations menu of Pawley'+
+                            ' Reflections tab to complete the Pawley setup')
+                
             dmin,dmax,nhist,lbl = getPawleydRange(G2frame,data)
             
             pawleySizer = wx.BoxSizer(wx.HORIZONTAL)
@@ -2381,24 +2395,30 @@ def UpdatePhaseData(G2frame,Item,data):
             generalData['Pawley dmax'] = min(generalData['Pawley dmax'],dmax)
             generalData['Pawley dmin'] = max(generalData['Pawley dmin'],dmin)
 
+            PawleyCtrlsList = []
             pawlRef = G2G.G2CheckBoxFrontLbl(General,' Do Pawley refinement?',
-                                         generalData,'doPawley')
+                                         generalData,'doPawley',enablePawley)
             pawleySizer.Add(pawlRef,0,WACV)
             pawleySizer.Add(wx.StaticText(General,label='  dmin: '),0,WACV)
             pawlMin = G2G.ValidatedTxtCtrl(General,generalData,'Pawley dmin',size=(75,-1),
                 xmin=dmin,xmax=20.,nDig=(10,5))
+            PawleyCtrlsList.append(pawlMin)
             pawleySizer.Add(pawlMin,0,WACV)
             pawleySizer.Add(wx.StaticText(General,label='  dmax: '),0,WACV)
             pawlMax = G2G.ValidatedTxtCtrl(General,generalData,'Pawley dmax',size=(75,-1),
                 xmin=2.0,xmax=dmax,nDig=(10,5))
+            PawleyCtrlsList.append(pawlMax)
             pawleySizer.Add(pawlMax,0,WACV)
             pawleySizer.Add(wx.StaticText(General,label=' Pawley neg. wt.: '),0,WACV)
             pawlNegWt = G2G.ValidatedTxtCtrl(General,generalData,'Pawley neg wt',size=(65,-1),
                 xmin=0.,xmax=1.,nDig=(10,3,'g'))
+            PawleyCtrlsList.append(pawlNegWt)
             pawleySizer.Add(pawlNegWt,0,WACV)
             pawleyOuter = wx.BoxSizer(wx.VERTICAL)
             pawleyOuter.Add(pawleySizer)
             pawleyOuter.Add(wx.StaticText(General,label=lbl),0,wx.LEFT,120)
+            for c in PawleyCtrlsList:
+                c.Enable(generalData['doPawley'])
             return pawleyOuter
             
         def MapSizer():
@@ -12805,7 +12825,7 @@ u''' The 2nd column below shows the last saved mode values. The 3rd && 4th colum
                         Inst = G2frame.GPXtree.GetItemPyData(G2gd.GetGPXtreeItemId(G2frame,Id,'Instrument Parameters'))[0]
                         data['Histograms'][histoName] = {
                             'Histogram':histoName,'Show':False,
-                            'LeBail':False,'newLeBail':True,
+                            'LeBail':False,'newLeBail':False,
                             'Scale':[1.0,False],'Pref.Ori.':['MD',1.0,False,[0,0,1],0,{},['',],0.1],'Type':Inst['Type'][0],
                             'Size':['isotropic',[1.,1.,1.],[False,False,False],[0,0,1],
                                 [1.,1.,1.,0.,0.,0.],6*[False,]],
@@ -15492,13 +15512,16 @@ of the crystal structure.
         SetPhaseWindow(PawleyRefList,mainSizer)
                     
     def OnPawleySet(event):
-        '''Set Pawley parameters and optionally recompute
+        '''Open dialog to set Pawley parameters and optionally recompute reflections. 
+        This is called from the Phase/Pawley Reflections "Pawley Settings" 
+        menu command. These settings are also available on the Phase/General tab.
         '''
         def DisablePawleyOpts(*args):
-            for c in controlsList:
+            'dis-/enable Pawley options'
+            for c in PawleyCtrlsList:
                 c.Enable(generalData['doPawley'])
-
-        controlsList = []
+                
+        PawleyCtrlsList = []
         dmin,dmax,nhist,lbl = getPawleydRange(G2frame,data)
         generalData = data['General']
         prevPawleySetting = generalData['doPawley']
@@ -15526,7 +15549,7 @@ of the crystal structure.
         pawlVal = G2G.ValidatedTxtCtrl(genDlg,generalData,'Pawley dmin',
             xmin=dmin,xmax=20.,nDig=(10,5),typeHint=float)
 #            xmin=dmin,xmax=20.,nDig=(10,5),typeHint=float,OnLeave=d2Q)
-        controlsList.append(pawlVal)
+        PawleyCtrlsList.append(pawlVal)
         pawleySizer.Add(pawlVal,0,WACV)
         #pawleySizer.Add(wx.StaticText(genDlg,label='   Qmax: '),0,WACV)
         #temp = {'Qmax':2 * math.pi / generalData['Pawley dmin']}
@@ -15535,7 +15558,7 @@ of the crystal structure.
         #    pawlVal.SetValue(generalData['Pawley dmin'])        
         #pawlQVal = G2G.ValidatedTxtCtrl(genDlg,temp,'Qmax',
         #    xmin=0.314,xmax=25.,nDig=(10,5),typeHint=float,OnLeave=Q2D)
-        #controlsList.append(pawlQVal)
+        #PawleyCtrlsList.append(pawlQVal)
         #pawleySizer.Add(pawlQVal,0,WACV)
         mainSizer.Add(pawleySizer)
 
@@ -15543,7 +15566,7 @@ of the crystal structure.
         pawleySizer.Add(wx.StaticText(genDlg,label='   Pawley dmax: '),0,WACV)
         pawlVal = G2G.ValidatedTxtCtrl(genDlg,generalData,'Pawley dmax',
             xmin=2.,xmax=dmax,nDig=(10,5),typeHint=float)
-        controlsList.append(pawlVal)
+        PawleyCtrlsList.append(pawlVal)
         pawleySizer.Add(pawlVal,0,WACV)
         mainSizer.Add(pawleySizer)
         
@@ -15552,7 +15575,7 @@ of the crystal structure.
         pawlNegWt = G2G.ValidatedTxtCtrl(genDlg,generalData,'Pawley neg wt',
             xmin=0.,xmax=1.,nDig=(10,4),typeHint=float)
         pawleySizer.Add(pawlNegWt,0,WACV)
-        controlsList.append(pawlNegWt)
+        PawleyCtrlsList.append(pawlNegWt)
         mainSizer.Add(pawleySizer)
 
         # make OK button
@@ -15574,7 +15597,13 @@ of the crystal structure.
         res = genDlg.ShowModal()
         genDlg.Destroy()
         if res == wx.ID_NO: return
-
+        # If Pawley is on, turn off Le Bail settings (since they will
+        # be hidden)
+        if generalData['doPawley']:
+            Controls = G2frame.GPXtree.GetItemPyData(G2gd.GetGPXtreeItemId(G2frame,G2frame.GPXtree.root, 'Controls'))
+            Controls['newLeBail'] = False
+            for h in data['Histograms']:
+                data['Histograms'][h]['LeBail'] = False
         # ask to generate the reflections if the extraction setting or dmin has changed
         if generalData['doPawley'] and res == wx.ID_OK and (
                 not prevPawleySetting or startDmin != generalData['Pawley dmin']):
@@ -16634,7 +16663,9 @@ of the crystal structure.
     # make sure that the phase menu bars get created before selecting
     # any (this will only be true on the first call to UpdatePhaseData)
     if callable(G2frame.dataWindow.DataGeneral):
+        wx.BeginBusyCursor()        
         G2frame.dataWindow.DataGeneral()
+        wx.EndBusyCursor()
 
     #patch
     if 'RBModels' not in data:

GSASII/GSASIIstrMain.py
@@ -433,7 +433,7 @@ def RefineCore(Controls,Histograms,Phases,restraintDict,rigidbodyDict,parmDict,v
         Rvals['GOF0'] = np.sqrt(chisq0/(Histograms['Nobs']-len(varyList)))
     return IfOK,Rvals,result,covMatrix,sig,Lastshft
 
-def Refine(GPXfile,dlg=None,makeBack=True,refPlotUpdate=None,newLeBail=False,allDerivs=False):
+def Refine(GPXfile,dlg=None,makeBack=True,refPlotUpdate=None,allDerivs=False):
     '''Global refinement -- refines to minimize against all histograms. 
     This can be called in one of three ways, from :meth:`GSASIIdataGUI.GSASII.OnRefine` in an 
     interactive refinement, where dlg will be a wx.ProgressDialog, or non-interactively from 
@@ -453,7 +453,7 @@ def Refine(GPXfile,dlg=None,makeBack=True,refPlotUpdate=None,newLeBail=False,all
     parmDict = {}
     G2mv.InitVars()
     Controls = G2stIO.GetControls(GPXfile)
-    Controls['newLeBail'] = newLeBail
+    Controls['newLeBail'] = Controls.get('newLeBail',False)
     G2stIO.ShowControls(Controls,printFile)
     calcControls = {}
     calcControls.update(Controls)

GSASII/GSASIIddataGUI.py
@@ -855,11 +855,16 @@ def UpdateDData(G2frame,DData,data,hist='',Scroll=0):
             wx.CallLater(100,RepaintHistogramInfo,DData.GetScrollPos(wx.VERTICAL))
 
         def OnLeBail(event):
-            if not UseList[G2frame.hist]['LeBail']:
+            '''Toggle the LeBail flag (Phase['Histograms'][hist]['LeBail'] and
+            when turned on, set the Controls['newLeBail'] flag to indicate that
+            the a new Le Bail extraction will be performed at the next 
+            refinement
+            '''
+            UseList[G2frame.hist]['LeBail'] = not UseList[G2frame.hist]['LeBail']
+            if UseList[G2frame.hist]['LeBail']:
                 Controls['newLeBail'] = True
             else:
                 Controls['newLeBail'] = False
-            UseList[G2frame.hist]['LeBail'] = not UseList[G2frame.hist]['LeBail']
             wx.CallLater(100,RepaintHistogramInfo,DData.GetScrollPos(wx.VERTICAL))
             
         def OnResetSize(event):
@@ -1047,6 +1052,13 @@ def UpdateDData(G2frame,DData,data,hist='',Scroll=0):
                 bottomSizer.Add(ExtSizer('PWDR'),0,wx.TOP|wx.BOTTOM,5)
                 if generalData['Type'] != 'magnetic': 
                     bottomSizer.Add(BabSizer(),0,wx.BOTTOM,5)
+            else:
+                # turn off preferred orientation fitting in LeBail mode (hidden)
+                UseList[G2frame.hist]['Pref.Ori.'][2] = False
+                for bab in ['A','U']:
+                    UseList[G2frame.hist]['Babinet']['Bab'+bab][1] = False
+                # TODO: should turn off all Extinction refinement flags
+                UseList[G2frame.hist]['Extinction'][1] = False
         elif G2frame.hist[:4] == 'HKLF':
             bottomSizer.Add(ExtSizer('HKLF'))  #,0,wx.BOTTOM,5)
             bottomSizer.Add(BabSizer())  #,0,wx.BOTTOM,5)

commit 956a87045286dd921efb7828869fa26837e1b782

Fix LeBail error introduced in 8ec4d386; fix 0 cycle big; clean up some startup messages; remove newLeBail from HAP (not used there)

GSASII/G2.py
@@ -23,7 +23,7 @@ except ModuleNotFoundError:
 
 if __name__ == '__main__':
     if pkginfo is None:  # hack path if GSASII not installed into Python
-        print('GSAS-II not installed in Python; Hacking sys.path')
+        print('Adding GSAS-II location to Python system path')
         sys.path.insert(0,os.path.dirname(os.path.dirname(__file__)))
     from GSASII.GSASIIGUI import main
     main()

GSASII/GSASIIdataGUI.py
@@ -5427,18 +5427,23 @@ If you continue from this point, it is quite likely that all intensity computati
             print ('\nError message(s):\n',errmsg)
             self.ErrorDialog('Error in constraints',errmsg)
             return
-        Controls = self.GPXtree.GetItemPyData(GetGPXtreeItemId(self,self.root, 'Controls'))
         if Controls.get('newLeBail',False):
             dlgtxt = '''Reset Le Bail structure factors?
 
-    Yes: all structure factors are reset to start at unity; Le Bail-only fitting will be used before any least-squares cycles.\n
-    No: least-squares starts with previously set structure factors.'''
-            dlgb = wx.MessageDialog(self,dlgtxt,'Le Bail Mode',style=wx.YES_NO)
-            result = wx.ID_NO
-            try:
-                result = dlgb.ShowModal()
-            finally:
-                dlgb.Destroy()
+Yes: all structure factors are reset to start at unity; Le Bail-only fitting will be applied before any least-squares cycles.\n
+No: least-squares fitting starts with previously fit structure factors.'''
+            #dlgb = wx.MessageDialog(self,dlgtxt,'Le Bail Mode',style=wx.YES_NO)
+            #result = wx.ID_NO
+            # try:
+            #     result = dlgb.ShowModal()
+            # finally:
+            #     dlgb.Destroy()
+            result = G2G.ShowScrolledInfo(self,dlgtxt,header='Le Bail Mode',
+                                            width=400,height=200,
+                                           buttonlist=[
+        ('Yes', lambda event: event.GetEventObject().GetParent().EndModal(wx.ID_YES)),
+        ('No', lambda event: event.GetEventObject().GetParent().EndModal(wx.ID_NO))
+                                               ])
             if result == wx.ID_YES:
                 res = self.OnLeBail(event)
                 if res: return
@@ -5457,7 +5462,7 @@ If you continue from this point, it is quite likely that all intensity computati
         else:
             refPlotUpdate = None
         try:
-            OK,Rvals = G2stMn.Refine(self.GSASprojectfile,dlg,refPlotUpdate=refPlotUpdate)
+            OK,Rvals = G2stMn.Refine(self.GSASprojectfile,dlg,refPlotUpdate=refPlotUpdate,newLeBail=Controls.get('newLeBail',False))
         finally:
             dlg.Update(101.) # forces the Auto_Hide; needed after move w/Win & wx3.0
             dlg.Destroy()
@@ -5509,8 +5514,7 @@ If you continue from this point, it is quite likely that all intensity computati
                 dlg.Destroy()
         else:
             self.ErrorDialog('Refinement error',Rvals['msg'])
-        # a fit has been done, no need to reset intensities again
-        Controls = self.GPXtree.GetItemPyData(GetGPXtreeItemId(self,self.root, 'Controls'))
+        # if a LeBail fit has been done, no need to ask about reseting intensities again
         Controls['newLeBail'] = False
 
     def OnLeBail(self,event):

GSASII/GSASIImiscGUI.py
@@ -1360,6 +1360,9 @@ def mkParmDictfromTree(G2frame,sigDict=None):
 
 def LogCellChanges(G2frame):
     '''Log varied cell parameters into the data tree notebook'''
+    Controls = G2frame.GPXtree.GetItemPyData(
+        G2gd.GetGPXtreeItemId(G2frame,G2frame.root, 'Controls'))
+    if Controls['max cyc'] == 0: return # no fit so no change
     covData = G2frame.GPXtree.GetItemPyData(
             G2gd.GetGPXtreeItemId(G2frame,G2frame.root,'Covariance'))
     parmDict = mkParmDictfromTree(G2frame)

GSASII/GSASIIpath.py
@@ -1342,12 +1342,12 @@ def LoadConfig(printInfo=True):
                 if isinstance(config.__dict__[i],types.ModuleType): continue
                 configDict.update({i:str(config.__dict__[i])})
         except ImportError as err:
-            print("Error importing config.py file\n",err)
+            print("New install: start without a config.py file")
             return
         except Exception as err:
             print("Error reading config.py file\n",err)
             return
-        print(f"Contents of {config.__file__} to be written...")
+        print(f"Contents of {config.__file__} to be written from config.py...")
         WriteConfig(configDict)
 
     import configparser
@@ -2157,7 +2157,7 @@ to update/regress repository from git repository:
         try:
             importlib.util.find_spec('GSASII.GSASIIGUI')
         except ModuleNotFoundError:
-            print('GSAS-II not installed in Python; Hacking sys.path')
+            print('Adding GSAS-II location to Python system path')
             sys.path.insert(0,os.path.dirname(os.path.dirname(__file__)))
 
         # now restart GSAS-II with the new version

GSASII/GSASIIphsGUI.py
@@ -12836,7 +12836,7 @@ u''' The 2nd column below shows the last saved mode values. The 3rd && 4th colum
         if 'HKLF' in sourceDict['Histogram']:
             copyNames = ['Extinction','Babinet','Flack','Twins']
         else:  #PWDR
-            copyNames = ['Pref.Ori.','Size','Mustrain','HStrain','Extinction','Babinet','LeBail','newLeBail','Layer Disp']
+            copyNames = ['Pref.Ori.','Size','Mustrain','HStrain','Extinction','Babinet','LeBail','Layer Disp']
         copyNames += ['Scale','Fix FXU','FixedSeqVars']
         copyDict = {}
         for name in copyNames:
@@ -12946,7 +12946,7 @@ u''' The 2nd column below shows the last saved mode values. The 3rd && 4th colum
         if 'HKLF' in sourceDict['Histogram']:
             copyNames = ['Extinction','Babinet','Flack','Twins']
         else:  #PWDR
-            copyNames = ['Pref.Ori.','Size','Mustrain','HStrain','Extinction','Babinet','LeBail','newLeBail','Layer Disp']
+            copyNames = ['Pref.Ori.','Size','Mustrain','HStrain','Extinction','Babinet','LeBail','Layer Disp']
         copyNames += ['Scale','Fix FXU','FixedSeqVars']
         dlg = G2G.G2MultiChoiceDialog(G2frame,'Select which parameters to copy',
             'Select phase data parameters', copyNames)
@@ -13027,7 +13027,7 @@ u''' The 2nd column below shows the last saved mode values. The 3rd && 4th colum
         if 'HKLF' in histNam:
             copyNames = ['Extinction','Babinet','Flack','Twins']
         else:  #PWDR
-            copyNames = ['Pref.Ori.','Size','Mustrain','HStrain','Extinction','Babinet','LeBail','newLeBail','Layer Disp']
+            copyNames = ['Pref.Ori.','Size','Mustrain','HStrain','Extinction','Babinet','LeBail','Layer Disp']
         copyNames += ['Scale','Fix FXU','FixedSeqVars']
         dlg = G2G.G2MultiChoiceDialog(G2frame,'Select which parameters to copy',
             'Select phase data parameters', copyNames)
@@ -13073,7 +13073,7 @@ u''' The 2nd column below shows the last saved mode values. The 3rd && 4th colum
                         Inst = G2frame.GPXtree.GetItemPyData(G2gd.GetGPXtreeItemId(G2frame,Id,'Instrument Parameters'))[0]
                         data['Histograms'][histoName] = {
                             'Histogram':histoName,'Show':False,
-                            'LeBail':False,'newLeBail':False,
+                            'LeBail':False,
                             'Scale':[1.0,False],'Pref.Ori.':['MD',1.0,False,[0,0,1],0,{},['',],0.1],'Type':Inst['Type'][0],
                             'Size':['isotropic',[1.,1.,1.],[False,False,False],[0,0,1],
                                 [1.,1.,1.,0.,0.,0.],6*[False,]],

GSASII/GSASIIstrMain.py
@@ -443,7 +443,7 @@ def RefineCore(Controls,Histograms,Phases,restraintDict,rigidbodyDict,parmDict,v
         Rvals['GOF0'] = np.sqrt(chisq0/(Histograms['Nobs']-len(varyList)))
     return IfOK,Rvals,result,covMatrix,sig,Lastshft
 
-def Refine(GPXfile,dlg=None,makeBack=True,refPlotUpdate=None,allDerivs=False):
+def Refine(GPXfile,dlg=None,makeBack=True,refPlotUpdate=None,newLeBail=False,allDerivs=False):
     '''Global refinement -- refines to minimize against all histograms.
     This can be called in one of three ways, from :meth:`GSASIIdataGUI.GSASII.OnRefine` in an
     interactive refinement, where dlg will be a wx.ProgressDialog, or non-interactively from
@@ -463,7 +463,7 @@ def Refine(GPXfile,dlg=None,makeBack=True,refPlotUpdate=None,allDerivs=False):
     parmDict = {}
     G2mv.InitVars()
     Controls = G2stIO.GetControls(GPXfile)
-    Controls['newLeBail'] = Controls.get('newLeBail',False)
+    Controls['newLeBail'] = newLeBail  # override value from file
     G2stIO.ShowControls(Controls,printFile)
     calcControls = {}
     calcControls.update(Controls)
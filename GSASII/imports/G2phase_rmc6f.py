5f5b528305ec5854f69dc02e0bf2d616c96b3df9
BHT <toby@anl.gov>
2025-07-01 07:41:24 -0500

    fix RMC import error

--- GSASII/imports/G2phase_rmc6f.py
+++ GSASII/imports/G2phase_rmc6f.py
@@ -8,7 +8,11 @@ import numpy as np
 import random as ran
 from .. import GSASIIobj as G2obj
 from .. import GSASIIlattice as G2lat
-
+try:
+    from .. import GSASIIctrlGUI as G2G
+except ImportError:
+    G2G = None
+    
 class PhaseReaderClass(G2obj.ImportPhase):
     'Opens a .rmc6f file and pulls out the phase'
     def __init__(self):
@@ -43,8 +47,8 @@ class PhaseReaderClass(G2obj.ImportPhase):
         fp = open(filename, 'r')
         Phase = {}
         Title = os.path.split(filename)
-        import GSASIIctrlGUI as G2G
-        G2G.SaveGPXdirectory(Title[0])
+        if G2G is not None: 
+            G2G.SaveGPXdirectory(Title[0])
         Title = os.path.splitext(Title[1])[0]
         Atoms = []
         S = fp.readline()

AdvancedPhotonSource/GSAS-II

commit 12ec78f05c703a59d58111f7b308b537fd9746f2

use correct form factor for normalization of micro-EM charge flipping

GSASII/GSASIIpwdGUI.py
@@ -3404,7 +3404,8 @@ def UpdateInstrumentGrid(G2frame,data):
     RefObj = {}
     Inst2 = G2frame.GPXtree.GetItemPyData(G2gd.GetGPXtreeItemId(G2frame,
             G2frame.PatternId,'Instrument Parameters'))[1]
-    G2gd.SetDataMenuBar(G2frame)
+    RefData = G2frame.GPXtree.GetItemPyData(G2frame.PatternId)[1]
+    RefData['Type'] = data['Type'][0]
     #patch
     if 'P' in insVal['Type']:                   #powder data
         if 'C' in insVal['Type']:               #constant wavelength

GSASII/GSASIIElem.py
@@ -915,7 +915,7 @@ def SetupGeneral(data, dirname):
     F000N = 0.
     F000E = 0.
     ElTypes = [StripValence(elem) for elem in generalData['AtomTypes']]
-    EFFtables = GetEFFtable(ElTypes) # broken for charged species
+    EFFtables = GetEFFtable(ElTypes) 
     for i,elem in enumerate(generalData['AtomTypes']):
         F000X += generalData['NoAtoms'][elem]*generalData['Z']
         isotope = generalData['Isotope'][elem]

GSASII/GSASIImath.py
@@ -4246,16 +4246,26 @@ def ChargeFlip(data,reflDict,pgbar):
     :returns: type name: description
 
     '''
+    print('Charge flip data type: ',reflDict['Type'])
     generalData = data['General']
     mapData = generalData['Map']
     flipData = generalData['Flip']
     FFtable = {}
-    if 'None' not in flipData['Norm element']:
+    if 'None' not in flipData['Norm element'] and 'N' not in reflDict['Type']:
         normElem = flipData['Norm element'].upper()
-        FFs = G2el.GetFormFactorCoeff(normElem.split('+')[0].split('-')[0])
+        if 'X' in reflDict['Type']:
+            FFs = G2el.GetFormFactorCoeff(normElem.split('+')[0].split('-')[0])
+        else: 
+            FFs = G2el.GetEFormFactorCoeff(normElem)
         for ff in FFs:
             if ff['Symbol'] == normElem:
                 FFtable.update(ff)
+        if 'X' in reflDict['Type']:
+            print('%s normalizing form factor: fa: %s, fb: %s, fc: %s'%(FFtable['Symbol'],
+                str(FFtable['fa']),str(FFtable['fb']),str(FFtable['fc'])))
+        else:
+            print('%s normalizing form factor: fa: %s, fb: %s'%(FFtable['Symbol'],
+                str(FFtable['fa']),str(FFtable['fb'])))
     dmin = flipData['GridStep']*2.
     SGData = generalData['SGData']
     SGMT = np.array([ops[0].T for ops in SGData['SGOps']])

GSASII/GSASIIphsGUI.py
@@ -2515,8 +2515,8 @@ def UpdatePhaseData(G2frame,Item,data):
             line2Sizer.Add(wx.StaticText(General,label=' Map grid step: '),0,WACV)
             flipRes = G2G.ValidatedTxtCtrl(General,Flip,'GridStep',nDig=(10,2),xmin=0.10,xmax=2.)
             line2Sizer.Add(flipRes,0,WACV)
-            line2Sizer.Add(wx.StaticText(General,label=' k-Factor (0.1-1.2): '),0,WACV)
-            kFactor = G2G.ValidatedTxtCtrl(General,Flip,'k-factor',nDig=(10,3),xmin=0.1,xmax=1.2)
+            line2Sizer.Add(wx.StaticText(General,label=' k-Factor (0.01-1.2): '),0,WACV)
+            kFactor = G2G.ValidatedTxtCtrl(General,Flip,'k-factor',nDig=(10,3),xmin=0.01,xmax=1.2)
             line2Sizer.Add(kFactor,0,WACV)
             line2Sizer.Add(wx.StaticText(General,label=' k-Max (>=10.0): '),0,WACV)
             kMax = G2G.ValidatedTxtCtrl(General,Flip,'k-Max',nDig=(10,1),xmin=10.)

commit 5c9a48b26e9712045bf2ce66264609d9a88e4a0e

quick fix to bug introduced in 12ec78f

GSASII/GSASIIpwdGUI.py
@@ -3404,8 +3404,11 @@ def UpdateInstrumentGrid(G2frame,data):
     RefObj = {}
     Inst2 = G2frame.GPXtree.GetItemPyData(G2gd.GetGPXtreeItemId(G2frame,
             G2frame.PatternId,'Instrument Parameters'))[1]
-    RefData = G2frame.GPXtree.GetItemPyData(G2frame.PatternId)[1]
-    RefData['Type'] = data['Type'][0]
+    G2gd.SetDataMenuBar(G2frame)  # this was removed in 12ec78f Why? (BHT)
+    #RefData = G2frame.GPXtree.GetItemPyData(G2frame.PatternId)[1]
+    #RefData['Type'] = data['Type'][0]
+    # The above was added but RefData would need to be ...PatternId)[0]
+    # for this to work. Not sure what is going on, so I am commenting the new code (BHT)
     #patch
     if 'P' in insVal['Type']:                   #powder data
         if 'C' in insVal['Type']:               #constant wavelength
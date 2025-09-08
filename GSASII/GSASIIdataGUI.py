95ee3540554cb7f85d23801fe29b83ab7af0f151
vondreele <vondreele@anl.gov>
2025-09-08 10:04:54 -0500

    more tweaks of extinction check plot

--- GSASII/GSASIIdataGUI.py
+++ GSASII/GSASIIdataGUI.py
@@ -8288,9 +8288,9 @@ def UpdatePWHKPlot(G2frame,kind,item):
             Fc = np.sqrt(np.array([xy[iFc+Super] for xy in refList if test(xy)]))
             Sig = np.array([xy[iSig+Super] for xy in refList if test(xy)])/wtFctr      #sig(fo^2)/wtFactor (1/GOF)
             XE = [[xy[iFc+Super],xy[iExt+Super]] for xy in refList if test(xy)]
-            XE = np.array([[np.sqrt(xe[0]),1./xe[1]] for xe in XE]).T
-            G2plt.PlotXY(G2frame,[[Fc,2.*Fo*(Fo-Fc)/Sig],],XY2=[XE,],labelX='|Fc|',labelY=GkDelta+'F/sig, 1/ExtC',newPlot=False,
-               Title='Extinction check',lines=False,points2=True,names=['|Fo|-|Fc|',],names2=['1/ExtC',])
+            XE = np.array([[np.sqrt(xe[0]),1./xe[1]-1.] for xe in XE]).T
+            G2plt.PlotXY(G2frame,[[Fc,2.*Fo*(Fo-Fc)/Sig],],XY2=[XE,],labelX='Fc',labelY=GkDelta+'F/sig, 1/ExtC-1',newPlot=False,
+               Title='Extinction check',lines=False,points2=True,names=[GkDelta+'F/sig',],names2=['1/ExtC-1',])
             
         G2plt.PlotDeltSig(G2frame,kind)
         if kind in ['HKLF',]:

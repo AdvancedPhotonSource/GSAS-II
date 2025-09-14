"fix" to multiple scattering extinction derivatives.

GSASII/GSASIIstrMath.py
@@ -2733,9 +2733,9 @@ def SCExtinction(ref,im,phfx,hfx,pfx,calcControls,parmDict,varyList):
         PB = np.exp(-parmDict[phfx+'Mb']*FPone**2)
         PC = np.exp(-parmDict[phfx+'Mc']*FPone**3)
         extCor = (PA+ + PB + PC)/3.            
-        dervDict[phfx+'Ma'] = -PA*FPone/3.
-        dervDict[phfx+'Mb'] = -PB*FPone**2/3.
-        dervDict[phfx+'Mc'] = -PC*FPone**3/3.
+        dervDict[phfx+'Ma'] = -4.*PA*FPone**2
+        dervDict[phfx+'Mb'] = -PB*FPone**4
+        dervDict[phfx+'Mc'] = -PC*FPone**6
         return extCor,dervDict
         
     if calcControls[phfx+'EType'] != 'None':
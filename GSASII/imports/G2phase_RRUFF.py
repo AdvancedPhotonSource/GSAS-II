f7da067753356e123a05d4a8f7d77a24ac55b179
BHT <toby@anl.gov>
2025-09-15 18:50:23 -0500

    commenting out nearly everything

--- GSASII/imports/G2phase_RRUFF.py
+++ GSASII/imports/G2phase_RRUFF.py
@@ -1,16 +1,16 @@
 '''Reads structure files downloaded from the RRUFF database either as ASCII
 text files or .rtf files that somehow are generated
 '''
-import sys
-import numpy as np
-import random as ran
-#from .. import GSASIIobj as G2obj
+# import sys
+# import numpy as np
+# import random as ran
+# #from .. import GSASIIobj as G2obj
 
-class RRUFFReader(G2obj.ImportPhase):
-    '''A fairly quickly-written importer to pull out the phase info from a 
-    RRUFF database (https://rruff.info) text file. 
-    '''
-    pass
+# class RRUFFReader(G2obj.ImportPhase):
+#     '''A fairly quickly-written importer to pull out the phase info from a 
+#     RRUFF database (https://rruff.info) text file. 
+#     '''
+#     pass
     # def __init__(self):
     #     super(self.__class__,self).__init__( # fancy way to say ImportPhase.__init__
     #         extensionlist=('.txt'),

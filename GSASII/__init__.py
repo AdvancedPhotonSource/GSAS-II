c71cec613d0691750e02e3ebe6db0c96779088e3
BHT <toby@anl.gov>
2025-10-13 03:20:47 -0500

    remove missed bit of legacy path hacking

--- GSASII/__init__.py
+++ GSASII/__init__.py
@@ -15,12 +15,13 @@ def make_path_watcher():
             return
         module, filename, path, meta_path, path_hooks = args
         if path is not None and tuple(path) != init_path:
-            lines = list(traceback.format_stack())
-            for line in lines:
-                print(line.strip())
-            print(set(path) - set(init_path))
+            print('Oops, path was hacked with...',set(path) - set(init_path))
+            raise PathHackingException()
+            #lines = list(traceback.format_stack()) # raises recursive exception in "pixi run ui"
+            #for line in lines:
+            #    print(line.strip())
             #sys.exit(1)
-            raise PathHackingException(lines[-2].strip())
+            #raise PathHackingException(lines[-2].strip())
     return no_path_hacking
 
 if os.environ.get("GSASII_NOPATHHACKING", ''):

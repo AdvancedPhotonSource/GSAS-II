5d187d81aec8e9a5fea5e7425afab0185a532ced
vondreele <vondreele@anl.gov>
2026-03-15 07:48:19 -0500

    fix the squeeze for twins

--- GSASII/GSASIIstrMath.py
+++ GSASII/GSASIIstrMath.py
@@ -1182,7 +1182,8 @@ def StructureFactor2(refDict,G,hfx,pfx,SGData,calcControls,parmDict):
         refl = refDict['RefList'][iBeg:iFin]    #array(blkSize,nItems)
         H = refl.T[:3]                          #array(blkSize,3)
         H = np.inner(H.T,TwinLaw)
-        H = np.squeeze(H,axis=1)   #maybe array(blkSize,nTwins,3) or (blkSize,3)
+        if TwinLaw.shape[0] == 1:            
+            H = np.squeeze(H,axis=1)   #maybe array(blkSize,nTwins,3) or (blkSize,3)
         TwMask = np.any(H,axis=-1)
         if TwinLaw.shape[0] > 1 and TwDict: #need np.inner(TwinLaw[?],TwDict[iref][i])*TwinInv[i]
             for ir in range(blkSize):

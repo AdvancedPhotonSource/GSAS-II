40584678e4d10cf7ceda37e0f2865e1db2e1ebf2
Brian Toby <briantoby@users.noreply.github.com>
2026-03-23 17:00:43 -0500

    Revert "Fix empty index in PDF docs for both developer and scripting manuals …" (#318)
    This reverts commit a6eb29bc20248f4bcb9b55868fae89fb70e3456b.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -253,9 +253,6 @@ latex_elements = {
 
 #this allows \AA to be used in equations 
 'preamble': '\\global\\renewcommand{\\AA}{\\text{\\r{A}}}',
-
-# Suppress auto-generated printindex at end; index is printed via indices.rst
-'printindex': '',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples

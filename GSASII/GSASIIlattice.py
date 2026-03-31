Add derivative calc to KslCalc - currently not used fix spin RB geometry in G2strMath

GSASII/GSASIIlattice.py
@@ -2999,7 +2999,7 @@ def SHarmcal(SytSym,SHFln,psi,gam):
             SHVal += (SHFln[term][0]*Ksl)
     return SHVal
 
-def KslCalc(trm,psi,gam):
+def KslCalc(trm,psi,gam,ifDerv=False):
     '''Compute one angular part term in spherical harmonics
 
     :param str trm:sp. harm term name in the form of 'C(l,m)' or 'C(l,m)c' for cubic
@@ -3008,11 +3008,22 @@ def KslCalc(trm,psi,gam):
 
     :returns array Ksl: spherical harmonics angular part for psi,gam pairs
     '''
+    delt = 0.0001
     l,m = eval(trm.strip('C').strip('c'))
     if 'c' in trm:
-        return CubicSHarm(l,m,psi,gam)
+        if ifDerv:
+            dYdp = (CubicSHarm(l,m,psi+delt,gam)-CubicSHarm(l,m,psi-delt,gam))/(2.0*delt)
+            dYdg = (CubicSHarm(l,m,psi,gam+delt)-CubicSHarm(l,m,psi,gam-delt))/(2.0*delt)
+            return CubicSHarm(l,m,psi,gam),dYdp,dYdg
+        else:
+            return CubicSHarm(l,m,psi,gam)
     else:
-        return SphHarmAng(l,m,1.0,psi,gam)
+        if ifDerv:
+            dYdp = (SphHarmAng(l,m,1.0,psi+delt,gam)-SphHarmAng(l,m,1.0,psi-delt,gam))/(2.0*delt)
+            dYdg = (SphHarmAng(l,m,1.0,psi,gam+delt)-SphHarmAng(l,m,1.0,psi,gam-delt))/(2.0*delt)
+            return SphHarmAng(l,m,1.0,psi,gam),dYdp,dYdg
+        else:
+            return SphHarmAng(l,m,1.0,psi,gam)
 
 def SphHarmAng(L,M,P,Th,Ph):
     ''' Compute spherical harmonics values using scipy.special.sph_harm

GSASII/GSASIIplot.py
@@ -7412,7 +7412,7 @@ def PlotStructure(G2frame,data,firstCall=False,pageCallback=None):
                         symAxis = np.array(SpnData.get('symAxis',[0,0,1]))
                         Npsi,Ngam = 90,45 
                         QA = G2mth.invQ(SpnData['Orient'][0])       #rotate about chosen axis
-                        QB = G2mth.make2Quat(symAxis,np.array([0,0,1.]))[0]     #position obj polar axis
+                        QB = G2mth.make2Quat(np.array([0,0,1.]),symAxis)[0]     #position obj polar axis
                         QP = G2mth.AVdeg2Q(360./Npsi,np.array([0,0,1.])) #this shifts by 1 azimuth pixel
                         Q = G2mth.prodQQ(QB,QA)
                         Q = G2mth.prodQQ(Q,QP)

GSASII/GSASIIstrMath.py
@@ -449,7 +449,7 @@ def MakeSpHarmFF(HKL,Amat,Bmat,SHCdict,Tdata,hType,FFtables,ORBtables,BLtables,F
 
     def MakePolar(Orient,QB):
         QA = G2mth.invQ(Orient)       #rotates about chosen axis
-        Q = G2mth.prodQQ(QA,QB)     #might be switched? QB,QA is order for plotting
+        Q = G2mth.prodQQ(QB,QA)     #might be switched? QB,QA is order for plotting
         M = np.inner(G2mth.Q2Mat(Q),Bmat)
         return G2lat.H2ThPh2(hkl,M)[1:]
 